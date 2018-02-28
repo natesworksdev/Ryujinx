ae41b750ee735c234e31ff978be98113d6e49608
AcK77 <Acoustik666@gmail.com>
2018-02-28 17:12:49 +0100

    More stubs
    - Fix SvcGetInfo loops by gdkchan.
    - Implement stub for Sfdnsres service.
    - Add more stubs for Bsd service.

---
+++ Ryujinx.Core/OsHle/Services/Sfdnsres/ServiceSfdnsres.cs
@@ -0,0 +1,27 @@
+using Ryujinx.Core.OsHle.Ipc;
+using System.Collections.Generic;
+
+namespace Ryujinx.Core.OsHle.IpcServices.Sfdnsres
+{
+    class ServiceSfdnsres : IIpcService
+    {
+        private Dictionary<int, ServiceProcessRequest> m_Commands;
+
+        public IReadOnlyDictionary<int, ServiceProcessRequest> Commands => m_Commands;
+
+        public ServiceSfdnsres()
+        {
+            m_Commands = new Dictionary<int, ServiceProcessRequest>()
+            {
+                //{ 0, GetLoadState }
+            };
+        }
+
+        public static long GetLoadState(ServiceCtx Context)
+        {
+            Context.ResponseData.Write(1);
+
+            return 0;
+        }
+    }
+}
