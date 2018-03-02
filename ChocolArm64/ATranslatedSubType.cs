331c355313ee7b0ad4c3c99ff67769fa05403d5d
gdkchan <gab.dark.100@gmail.com>
2018-03-02 15:27:30 -0300

    Remove unnecessary usings

--- ChocolArm64/ATranslatedSubType.cs
+++ ChocolArm64/ATranslatedSubType.cs
@@ -1,11 +1,3 @@
-using ChocolArm64.Memory;
-using ChocolArm64.State;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Reflection;
-using System.Reflection.Emit;
-
 namespace ChocolArm64
 {
     enum ATranslatedSubType
