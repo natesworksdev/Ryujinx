d237bca9a8aff2bf673cee8930ec0914769aae06
gdkchan <gab.dark.100@gmail.com>
2018-03-28 16:40:51 -0300

    Some improvements to macro stuff

--- Ryujinx.Graphics/Gpu/NvGpuEngine3d.cs
+++ Ryujinx.Graphics/Gpu/NvGpuEngine3d.cs
@@ -11,57 +11,156 @@ namespace Ryujinx.Graphics.Gpu
 
         private Dictionary<int, NvGpuMethod> Methods;
 
+        private struct ConstBuffer
+        {
+            public bool Enabled;
+            public long Position;
+            public int  Size;
+        }
+
+        private ConstBuffer[] Cbs;
+
         public NvGpuEngine3d(NsGpu Gpu)
         {
             this.Gpu = Gpu;
 
             Registers = new int[0xe00];
 
-            Methods = new Dictionary<int, NvGpuMethod>()
+            Methods = new Dictionary<int, NvGpuMethod>();
+
+            void AddMethod(int Meth, int Count, int Stride, NvGpuMethod Method)
             {
-                { 0x6c3, QueryControl }
-            };
+                while (Count-- > 0)
+                {
+                    Methods.Add(Meth, Method);
+
+                    Meth += Stride;
+                }
+            }
+
+            AddMethod(0x585,  1, 1, VertexEndGl);
+            AddMethod(0x6c3,  1, 1, QueryControl);
+            AddMethod(0x8e4, 16, 1, CbData);
+            AddMethod(0x904,  1, 1, CbBind);
+
+            Cbs = new ConstBuffer[18];
         }
 
         public void CallMethod(AMemory Memory, NsGpuPBEntry PBEntry)
         {
-            if (Methods.TryGetValue(PBEntry.Method, out NvGpuMethod Methd))
+            if (Methods.TryGetValue(PBEntry.Method, out NvGpuMethod Method))
             {
-                Methd(Memory, PBEntry);
+                Method(Memory, PBEntry);
             }
+            else
+            {
+                WriteRegister(PBEntry);
+            }
+        }
 
-            if (PBEntry.Arguments.Count == 1)
-            {
-                Registers[PBEntry.Method] = PBEntry.Arguments[0];
-            }
+        private void VertexEndGl(AMemory Memory, NsGpuPBEntry PBEntry)
+        {
+            int TexCbuf = ReadRegister(NvGpuEngine3dReg.TextureCbIndex);
+
+            int TexHandle = ReadCb(Memory, TexCbuf, 0x20);
         }
 
         private void QueryControl(AMemory Memory, NsGpuPBEntry PBEntry)
         {
-            long Position = MakeAddress(NvGpuEngine3dReg.QueryAddr);
-
-            int Seq  = Registers[(int)NvGpuEngine3dReg.QuerySequence];
-            int Ctrl = Registers[(int)NvGpuEngine3dReg.QueryControl];
-
-            int Mode = Ctrl & 3;
-
-            if (Mode == 0)
+            if (TryGetCpuAddr(NvGpuEngine3dReg.QueryAddr, out long Position))
             {
-                //Write.
-                Position = Gpu.MemoryMgr.GetCpuAddr(Position);
+                int Seq  = Registers[(int)NvGpuEngine3dReg.QuerySequence];
+                int Ctrl = Registers[(int)NvGpuEngine3dReg.QueryControl];
 
-                if (Position != -1)
+                int Mode = Ctrl & 3;
+
+                if (Mode == 0)
                 {
+                    //Write.
                     Memory.WriteInt32(Position, Seq);
                 }
             }
+
+            WriteRegister(PBEntry);
         }
 
-        private long MakeAddress(NvGpuEngine3dReg Reg)
+        private void CbData(AMemory Memory, NsGpuPBEntry PBEntry)
+        {
+            if (TryGetCpuAddr(NvGpuEngine3dReg.CbAddress, out long Position))
+            {
+                int Offset = ReadRegister(NvGpuEngine3dReg.CbOffset);
+
+                foreach (int Arg in PBEntry.Arguments)
+                {
+                    Memory.WriteInt32(Position + Offset, Arg);
+
+                    Offset += 4;
+                }
+
+                WriteRegister(NvGpuEngine3dReg.CbOffset, Offset);
+            }
+        }
+
+        private void CbBind(AMemory Memory, NsGpuPBEntry PBEntry)
+        {
+            int Index = PBEntry.Arguments[0];
+
+            bool Enabled = (Index & 1) != 0;
+
+            Index = (Index >> 4) & 0x1f;
+
+            if (TryGetCpuAddr(NvGpuEngine3dReg.CbAddress, out long Position))
+            {
+                Cbs[Index].Position = Position;
+            }
+
+            Cbs[Index].Enabled = Enabled;
+            Cbs[Index].Size    = ReadRegister(NvGpuEngine3dReg.CbSize);
+        }
+
+        private int ReadCb(AMemory Memory, int Cbuf, int Offset)
+        {
+            long Position = Cbs[Cbuf].Position;
+
+            int Value = Memory.ReadInt32(Position + Offset);
+
+            return Value;
+        }
+
+        private bool TryGetCpuAddr(NvGpuEngine3dReg Reg, out long Position)
+        {
+            Position = MakeInt64From2xInt32(Reg);
+
+            Position = Gpu.GetCpuAddr(Position);
+
+            return Position != -1;
+        }
+
+        private long MakeInt64From2xInt32(NvGpuEngine3dReg Reg)
         {
             return
                 (long)Registers[(int)Reg + 0] << 32 |
                 (uint)Registers[(int)Reg + 1];
+        }
+
+        private void WriteRegister(NsGpuPBEntry PBEntry)
+        {
+            int ArgsCount = PBEntry.Arguments.Count;
+
+            if (ArgsCount > 0)
+            {
+                Registers[PBEntry.Method] = PBEntry.Arguments[ArgsCount - 1];
+            }
+        }
+
+        private int ReadRegister(NvGpuEngine3dReg Reg)
+        {
+            return Registers[(int)Reg];
+        }
+
+        private void WriteRegister(NvGpuEngine3dReg Reg, int Value)
+        {
+            Registers[(int)Reg] = Value;
         }
     }
 }
--- Ryujinx.Graphics/Gpu/NvGpuEngine3dReg.cs
+++ Ryujinx.Graphics/Gpu/NvGpuEngine3dReg.cs
@@ -2,8 +2,12 @@ namespace Ryujinx.Graphics.Gpu
 {
     enum NvGpuEngine3dReg
     {
-        QueryAddr     = 0x6c0,
-        QuerySequence = 0x6c2,
-        QueryControl  = 0x6c3
+        QueryAddr      = 0x6c0,
+        QuerySequence  = 0x6c2,
+        QueryControl   = 0x6c3,
+        CbSize         = 0x8e0,
+        CbAddress      = 0x8e1,
+        CbOffset       = 0x8e3,
+        TextureCbIndex = 0x982
     }
 }
