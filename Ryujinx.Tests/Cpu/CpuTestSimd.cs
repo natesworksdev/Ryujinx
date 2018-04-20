550989c4ef93a5c43def1cb5d3e3f42952f3e39a
LDj3SNuD <35856442+LDj3SNuD@users.noreply.github.com>
2018-04-20 16:48:12 +0200

    Update CpuTestSimd.cs

--- Ryujinx.Tests/Cpu/CpuTestSimd.cs
+++ Ryujinx.Tests/Cpu/CpuTestSimd.cs
@@ -19,9 +19,33 @@ namespace Ryujinx.Tests.Cpu
             AArch64.TakeReset(false);
         }
 
+#region "ValueSource"
+        private static ulong[] _D_()
+        {
+            return new ulong[] { 0x0000000000000000ul, 0x7FFFFFFFFFFFFFFFul,
+                                 0x8000000000000000ul, 0xFFFFFFFFFFFFFFFFul };
+        }
+
+        private static ulong[] _8B4H2S_()
+        {
+            return new ulong[] { 0x0000000000000000ul, 0x7F7F7F7F7F7F7F7Ful,
+                                 0x8080808080808080ul, 0x7FFF7FFF7FFF7FFFul,
+                                 0x8000800080008000ul, 0x7FFFFFFF7FFFFFFFul,
+                                 0x8000000080000000ul, 0xFFFFFFFFFFFFFFFFul };
+        }
+
+        private static ulong[] _16B8H4S2D_()
+        {
+            return new ulong[] { 0x0000000000000000ul, 0x7F7F7F7F7F7F7F7Ful,
+                                 0x8080808080808080ul, 0x7FFF7FFF7FFF7FFFul,
+                                 0x8000800080008000ul, 0x7FFFFFFF7FFFFFFFul,
+                                 0x8000000080000000ul, 0x7FFFFFFFFFFFFFFFul,
+                                 0x8000000000000000ul, 0xFFFFFFFFFFFFFFFFul };
+        }
+#endregion
+
         [Test, Description("ABS <V><d>, <V><n>")]
-        public void Abs_S_D([Values(0x0000000000000000ul, 0x7FFFFFFFFFFFFFFFul,
-                                    0x8000000000000000ul, 0xFFFFFFFFFFFFFFFFul)] [Random(1)] ulong A)
+        public void Abs_S_D([ValueSource("_D_")] [Random(1)] ulong A)
         {
             uint Opcode = 0x5EE0B820; // ABS D0, D1
             Bits Op = new Bits(Opcode);
@@ -38,10 +62,7 @@ namespace Ryujinx.Tests.Cpu
         }
 
         [Test, Description("ABS <Vd>.<T>, <Vn>.<T>")]
-        public void Abs_V_8B_4H_2S([Values(0x0000000000000000ul, 0x7F7F7F7F7F7F7F7Ful,
-                                           0x8080808080808080ul, 0x7FFF7FFF7FFF7FFFul,
-                                           0x8000800080008000ul, 0x7FFFFFFF7FFFFFFFul,
-                                           0x8000000080000000ul, 0xFFFFFFFFFFFFFFFFul)] [Random(1)] ulong A,
+        public void Abs_V_8B_4H_2S([ValueSource("_8B4H2S_")] [Random(1)] ulong A,
                                    [Values(0b00u, 0b01u, 0b10u)] uint size) // <8B, 4H, 2S>
         {
             uint Opcode = 0x0E20B820; // ABS V0.8B, V1.8B
@@ -60,16 +81,8 @@ namespace Ryujinx.Tests.Cpu
         }
 
         [Test, Pairwise, Description("ABS <Vd>.<T>, <Vn>.<T>")]
-        public void Abs_V_16B_8H_4S_2D([Values(0x0000000000000000ul, 0x7F7F7F7F7F7F7F7Ful,
-                                               0x8080808080808080ul, 0x7FFF7FFF7FFF7FFFul,
-                                               0x8000800080008000ul, 0x7FFFFFFF7FFFFFFFul,
-                                               0x8000000080000000ul, 0x7FFFFFFFFFFFFFFFul,
-                                               0x8000000000000000ul, 0xFFFFFFFFFFFFFFFFul)] [Random(1)] ulong A0,
-                                       [Values(0x0000000000000000ul, 0x7F7F7F7F7F7F7F7Ful,
-                                               0x8080808080808080ul, 0x7FFF7FFF7FFF7FFFul,
-                                               0x8000800080008000ul, 0x7FFFFFFF7FFFFFFFul,
-                                               0x8000000080000000ul, 0x7FFFFFFFFFFFFFFFul,
-                                               0x8000000000000000ul, 0xFFFFFFFFFFFFFFFFul)] [Random(1)] ulong A1,
+        public void Abs_V_16B_8H_4S_2D([ValueSource("_16B8H4S2D_")] [Random(1)] ulong A0,
+                                       [ValueSource("_16B8H4S2D_")] [Random(1)] ulong A1,
                                        [Values(0b00u, 0b01u, 0b10u, 0b11u)] uint size) // <16B, 8H, 4S, 2D>
         {
             uint Opcode = 0x4E20B820; // ABS V0.16B, V1.16B
@@ -91,8 +104,7 @@ namespace Ryujinx.Tests.Cpu
         }
 
         [Test, Description("NEG <V><d>, <V><n>")]
-        public void Neg_S_D([Values(0x0000000000000000ul, 0x7FFFFFFFFFFFFFFFul,
-                                    0x8000000000000000ul, 0xFFFFFFFFFFFFFFFFul)] [Random(1)] ulong A)
+        public void Neg_S_D([ValueSource("_D_")] [Random(1)] ulong A)
         {
             uint Opcode = 0x7EE0B820; // NEG D0, D1
             Bits Op = new Bits(Opcode);
@@ -109,10 +121,7 @@ namespace Ryujinx.Tests.Cpu
         }
 
         [Test, Description("NEG <Vd>.<T>, <Vn>.<T>")]
-        public void Neg_V_8B_4H_2S([Values(0x0000000000000000ul, 0x7F7F7F7F7F7F7F7Ful,
-                                           0x8080808080808080ul, 0x7FFF7FFF7FFF7FFFul,
-                                           0x8000800080008000ul, 0x7FFFFFFF7FFFFFFFul,
-                                           0x8000000080000000ul, 0xFFFFFFFFFFFFFFFFul)] [Random(1)] ulong A,
+        public void Neg_V_8B_4H_2S([ValueSource("_8B4H2S_")] [Random(1)] ulong A,
                                    [Values(0b00u, 0b01u, 0b10u)] uint size) // <8B, 4H, 2S>
         {
             uint Opcode = 0x2E20B820; // NEG V0.8B, V1.8B
@@ -131,16 +140,8 @@ namespace Ryujinx.Tests.Cpu
         }
 
         [Test, Pairwise, Description("NEG <Vd>.<T>, <Vn>.<T>")]
-        public void Neg_V_16B_8H_4S_2D([Values(0x0000000000000000ul, 0x7F7F7F7F7F7F7F7Ful,
-                                               0x8080808080808080ul, 0x7FFF7FFF7FFF7FFFul,
-                                               0x8000800080008000ul, 0x7FFFFFFF7FFFFFFFul,
-                                               0x8000000080000000ul, 0x7FFFFFFFFFFFFFFFul,
-                                               0x8000000000000000ul, 0xFFFFFFFFFFFFFFFFul)] [Random(1)] ulong A0,
-                                       [Values(0x0000000000000000ul, 0x7F7F7F7F7F7F7F7Ful,
-                                               0x8080808080808080ul, 0x7FFF7FFF7FFF7FFFul,
-                                               0x8000800080008000ul, 0x7FFFFFFF7FFFFFFFul,
-                                               0x8000000080000000ul, 0x7FFFFFFFFFFFFFFFul,
-                                               0x8000000000000000ul, 0xFFFFFFFFFFFFFFFFul)] [Random(1)] ulong A1,
+        public void Neg_V_16B_8H_4S_2D([ValueSource("_16B8H4S2D_")] [Random(1)] ulong A0,
+                                       [ValueSource("_16B8H4S2D_")] [Random(1)] ulong A1,
                                        [Values(0b00u, 0b01u, 0b10u, 0b11u)] uint size) // <16B, 8H, 4S, 2D>
         {
             uint Opcode = 0x6E20B820; // NEG V0.16B, V1.16B
