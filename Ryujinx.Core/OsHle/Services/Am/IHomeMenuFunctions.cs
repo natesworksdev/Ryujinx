01349942d77cd89de5589692ab40bcaeee9436b6
Ac_K <Acoustik666@gmail.com>
2018-04-21 18:59:30 +0000

    am services implementations
    Implement:
    - IAllSystemAppletProxiesService
    - IApplicationCreator
    - IGlobalStateController
    - IHomeMenuFunctions
      * RequestToGetForeground stub.
      * GetPopFromGeneralChannelEvent stub event.
    - ISystemAppletProxy
      * GetCommonStateGetter
      * GetSelfController
      * GetWindowController
      * GetAudioController
      * GetDisplayController
      * GetLibraryAppletCreator
      * GetHomeMenuFunctions
      * GetGlobalStateController
      * GetApplicationCreator
      * GetDebugFunctions

---
+++ Ryujinx.Core/OsHle/Services/Am/IHomeMenuFunctions.cs
@@ -0,0 +1,46 @@
+using Ryujinx.Core.OsHle.Handles;
+using Ryujinx.Core.OsHle.Ipc;
+using System.Collections.Generic;
+
+namespace Ryujinx.Core.OsHle.Services.Am
+{
+    class IHomeMenuFunctions : IpcService
+    {
+        private Dictionary<int, ServiceProcessRequest> m_Commands;
+
+        public override IReadOnlyDictionary<int, ServiceProcessRequest> Commands => m_Commands;
+
+        private KEvent ChannelEvent;
+
+        public IHomeMenuFunctions()
+        {
+            m_Commands = new Dictionary<int, ServiceProcessRequest>()
+            {
+                { 10, RequestToGetForeground        },
+                { 21, GetPopFromGeneralChannelEvent }
+            };
+
+            ChannelEvent = new KEvent();
+            //TODO: We shouldn't be signaling this here.
+            ChannelEvent.WaitEvent.Set();
+        }
+
+        public long RequestToGetForeground(ServiceCtx Context)
+        {
+            Logging.Stub(LogClass.ServiceAm, "Stubbed");
+
+            return 0;
+        }
+
+        public long GetPopFromGeneralChannelEvent(ServiceCtx Context)
+        {
+            int Handle = Context.Process.HandleTable.OpenHandle(ChannelEvent);
+
+            Context.Response.HandleDesc = IpcHandleDesc.MakeCopy(Handle);
+
+            Logging.Stub(LogClass.ServiceAm, "Stubbed");
+
+            return 0;
+        }
+    }
+}
