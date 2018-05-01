b7600a9cc9902b130efd59daecaec558b367e87c
Ac_K <Acoustik666@gmail.com>
2018-05-02 01:53:28 +0200

    Update IAudioRendererManager.cs

--- Ryujinx.Core/OsHle/Services/Aud/IAudioRendererManager.cs
+++ Ryujinx.Core/OsHle/Services/Aud/IAudioRendererManager.cs
@@ -47,7 +47,7 @@ namespace Ryujinx.Core.OsHle.Services.Aud
             {
                 long Size;
 
-		Size  = UnknownC * 0x400 + 0x50;
+                Size  = UnknownC * 0x400 + 0x50;
                 Size += RoundUp(Unknown8 * 4, 64);
                 Size += (UnknownC + 1) * 0x940;
                 Size += Unknown14 * 0x170;
