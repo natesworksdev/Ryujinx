2252c5ca92fce1f2ed153e951daabd1c48b53a72
Thog <me@thog.eu>
2018-05-04 20:39:32 +0200

    Add ISystemDisplayService::GetDisplayMode & IManagerDisplayService::SetLayerVisibility
    Also add some missing PrintStub calls

--- Ryujinx.Core/OsHle/Services/Vi/ISystemDisplayService.cs
+++ Ryujinx.Core/OsHle/Services/Vi/ISystemDisplayService.cs
@@ -1,5 +1,6 @@
 using Ryujinx.Core.OsHle.Ipc;
 using System.Collections.Generic;
+using Ryujinx.Core.Logging;
 
 namespace Ryujinx.Core.OsHle.Services.Vi
 {
@@ -14,17 +15,29 @@ namespace Ryujinx.Core.OsHle.Services.Vi
             m_Commands = new Dictionary<int, ServiceProcessRequest>()
             {
                 { 2205, SetLayerZ },
-                { 2207, SetLayerVisibility }
+                { 2207, SetLayerVisibility },
+                { 3200, GetDisplayMode }
             };
         }
 
         public static long SetLayerZ(ServiceCtx Context)
         {
+            Context.Ns.Log.PrintStub(LogClass.ServiceVi, "Stubbed.");
             return 0;
         }
 
         public static long SetLayerVisibility(ServiceCtx Context)
         {
+            Context.Ns.Log.PrintStub(LogClass.ServiceVi, "Stubbed.");
+            return 0;
+        }
+
+        public static long GetDisplayMode(ServiceCtx Context)
+        {
+            Context.ResponseData.Write(1280);
+            Context.ResponseData.Write(720);
+            Context.ResponseData.Write(16.0 / 9.0);
+            Context.ResponseData.Write(0L);
             return 0;
         }
     }
