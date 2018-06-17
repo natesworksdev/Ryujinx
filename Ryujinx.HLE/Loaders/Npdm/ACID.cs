d78445504b39260784bfd6785a71fac7c25c50b4
Ac_K <Acoustik666@gmail.com>
2018-06-17 18:47:00 +0200

    Update ACID.cs

--- Ryujinx.HLE/Loaders/Npdm/ACID.cs
+++ Ryujinx.HLE/Loaders/Npdm/ACID.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 namespace Ryujinx.HLE.Loaders.Npdm
 {
-    public class ACID
+    class ACID
     {
         public byte[] RSA2048Signature;
         public byte[] RSA2048Modulus;
@@ -14,16 +14,18 @@ namespace Ryujinx.HLE.Loaders.Npdm
         public string TitleIdRangeMin;
         public string TitleIdRangeMax;
 
-        private int   FSAccessControlOffset;
-        private int   FSAccessControlSize;
-        private int   ServiceAccessControlOffset;
-        private int   ServiceAccessControlSize;
-        private int   KernelAccessControlOffset;
-        private int   KernelAccessControlSize;
+        private int FSAccessControlOffset;
+        private int FSAccessControlSize;
+        private int ServiceAccessControlOffset;
+        private int ServiceAccessControlSize;
+        private int KernelAccessControlOffset;
+        private int KernelAccessControlSize;
 
         public FSAccessControl      FSAccessControl;
         public ServiceAccessControl ServiceAccessControl;
         public KernelAccessControl  KernelAccessControl;
+        
+        public const long ACIDMagic = 'A' << 0 | 'C' << 8 | 'I' << 16 | 'D' << 24;
 
         public ACID(Stream ACIDStream, int Offset)
         {
@@ -34,7 +36,7 @@ namespace Ryujinx.HLE.Loaders.Npdm
             RSA2048Signature = Reader.ReadBytes(0x100);
             RSA2048Modulus   = Reader.ReadBytes(0x100);
 
-            if (EndianSwap.Swap32(Reader.ReadInt32()) != 0x41434944) // ACID
+            if (Reader.ReadInt32() != ACIDMagic)
             {
                 throw new InvalidNpdmException("ACID Stream doesn't contain ACID section!");
             }
