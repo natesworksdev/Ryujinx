f6632f06b3620de1f8305d55d9fb1d28095acb9f
Ac_K <Acoustik666@gmail.com>
2018-06-17 03:28:40 +0000

    Implement NPDM files parser
    (Currently not used in Ryujinx)

---
+++ Ryujinx.HLE/Loaders/Npdm/ACI0.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Ryujinx.HLE.Loaders.Npdm
+{
+    public class ACI0
+    {
+        public string TitleId;
+
+        private int   FSAccessHeaderOffset;
+        private int   FSAccessHeaderSize;
+        private int   ServiceAccessControlOffset;
+        private int   ServiceAccessControlSize;
+        private int   KernelAccessControlOffset;
+        private int   KernelAccessControlSize;
+
+        public FSAccessHeader       FSAccessHeader;
+        public ServiceAccessControl ServiceAccessControl;
+        public KernelAccessControl  KernelAccessControl;
+
+        public ACI0(Stream ACI0Stream, int Offset)
+        {
+            ACI0Stream.Seek(Offset, SeekOrigin.Begin);
+
+            BinaryReader Reader = new BinaryReader(ACI0Stream);
+
+            if (EndianSwap.Swap32(Reader.ReadInt32()) != 0x41434930) // ACI0
+            {
+                throw new InvalidNpdmException("ACI0 Stream doesn't contain ACI0 section!");
+            }
+
+            ACI0Stream.Seek(0x0C, SeekOrigin.Current);
+
+            byte[] TempTitleId = Reader.ReadBytes(8);
+            Array.Reverse(TempTitleId);
+            TitleId = BitConverter.ToString(TempTitleId).Replace("-", "");
+
+            // Reserved (Not currently used, potentially to be used for lowest title ID in future.)
+            ACI0Stream.Seek(0x08, SeekOrigin.Current);
+
+            FSAccessHeaderOffset       = Reader.ReadInt32();
+            FSAccessHeaderSize         = Reader.ReadInt32();
+            ServiceAccessControlOffset = Reader.ReadInt32();
+            ServiceAccessControlSize   = Reader.ReadInt32();
+            KernelAccessControlOffset  = Reader.ReadInt32();
+            KernelAccessControlSize    = Reader.ReadInt32();
+
+            FSAccessHeader       = new FSAccessHeader(ACI0Stream, Offset + FSAccessHeaderOffset, FSAccessHeaderSize);
+            ServiceAccessControl = new ServiceAccessControl(ACI0Stream, Offset + ServiceAccessControlOffset, ServiceAccessControlSize);
+            KernelAccessControl  = new KernelAccessControl(ACI0Stream, Offset + KernelAccessControlOffset, KernelAccessControlSize);
+        }
+    }
+}
---
+++ Ryujinx.HLE/Loaders/Npdm/ServiceAccessControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ryujinx.HLE.Loaders.Npdm
+{
+    public class ServiceAccessControl
+    {
+        public List<Tuple<string, bool>> Services = new List<Tuple<string, bool>>();
+
+        public ServiceAccessControl(Stream ServiceAccessControlStream, int Offset, int Size)
+        {
+            ServiceAccessControlStream.Seek(Offset, SeekOrigin.Begin);
+
+            BinaryReader Reader = new BinaryReader(ServiceAccessControlStream);
+
+            int ByteReaded = 0;
+
+            while (ByteReaded != Size)
+            {
+                byte ControlByte = Reader.ReadByte();
+
+                if (ControlByte == 0x00) break;
+
+                int Length             = ((ControlByte & 0x07)) + 1;
+                bool RegisterAllowed   = ((ControlByte & 0x80) != 0);
+                byte[] TempServiceName = Reader.ReadBytes(Length);
+
+                Services.Add(Tuple.Create(Encoding.ASCII.GetString(TempServiceName, 0, TempServiceName.Length), RegisterAllowed));
+
+                ByteReaded += Length + 1;
+            }
+        }
+    }
+}
