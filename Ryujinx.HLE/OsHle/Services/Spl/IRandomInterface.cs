b30640d74939a93ec5f3e218aa5c89f3210c2f67
Starlet <gpyron@mail.com>
2018-07-03 12:10:35 -0400

    Implement CSRNG (Cryptographically Secure Random Bytes)

---
+++ Ryujinx.HLE/OsHle/Services/Spl/IRandomInterface.cs
@@ -0,0 +1,38 @@
+﻿using Ryujinx.HLE.OsHle.Ipc;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Ryujinx.HLE.OsHle.Services.Spl
+{
+    class IRandomInterface : IpcService
+    {
+        private Dictionary<int, ServiceProcessRequest> m_Commands;
+
+        public override IReadOnlyDictionary<int, ServiceProcessRequest> Commands => m_Commands;
+
+        private RNGCryptoServiceProvider CspRnd;
+
+        public IRandomInterface()
+        {
+            m_Commands = new Dictionary<int, ServiceProcessRequest>()
+            {
+                { 0, GetRandomBytes }
+            };
+
+            CspRnd = new RNGCryptoServiceProvider();
+        }
+
+        public long GetRandomBytes(ServiceCtx Context)
+        {
+            (long Position, long Size) = Context.Request.GetBufferType0x21();
+
+            byte[] BytesBuffer = new byte[Size];
+
+            CspRnd.GetBytes(BytesBuffer);
+
+            Context.Memory.WriteBytes(Position, BytesBuffer);
+
+            return 0;
+        }
+    }
+}
