1e6b8ef2be4e7b79b7c17967599edc10cae71231
greggameplayer <33609333+greggameplayer@users.noreply.github.com>
2018-07-10 19:21:07 +0200

    return correct texture size

--- Ryujinx.HLE/Gpu/Texture/TextureHelper.cs
+++ Ryujinx.HLE/Gpu/Texture/TextureHelper.cs
@@ -35,6 +35,7 @@ namespace Ryujinx.HLE.Gpu.Texture
                     return Texture.Width * Texture.Height * 8;
 
                 case GalTextureFormat.A8B8G8R8:
+                case GalTextureFormat.A2B10G10R10:
                 case GalTextureFormat.R32:
                 case GalTextureFormat.ZF32:
                     return Texture.Width * Texture.Height * 4;
