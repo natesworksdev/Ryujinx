2f811eff99a94ced1b2980270bfe71531b3203ae
Ac_K <Acoustik666@gmail.com>
2018-07-19 05:41:27 +0200

    Implement SvcWaitForAddress 0x34
    Currently needed by Sonic Mania Plus

---
+++ Ryujinx.HLE/OsHle/Kernel/AddressArbiter.cs
@@ -0,0 +1,94 @@
+﻿using ChocolArm64.Memory;
+using ChocolArm64.State;
+using Ryujinx.HLE.OsHle.Handles;
+
+using static Ryujinx.HLE.OsHle.ErrorCode;
+
+namespace Ryujinx.HLE.OsHle.Kernel
+{
+    static class AddressArbiter
+    {
+        static ulong WaitForAddress(Process Process, AThreadState ThreadState, long Address, ulong Timeout)
+        {
+            KThread CurrentThread = Process.GetThread(ThreadState.Tpidr);
+
+            Process.Scheduler.SetReschedule(CurrentThread.ProcessorId);
+
+            CurrentThread.ArbiterWaitAddress = Address;
+            CurrentThread.ArbiterSignaled    = false;
+
+            Process.Scheduler.EnterWait(CurrentThread, NsTimeConverter.GetTimeMs(Timeout));
+
+            if (!CurrentThread.ArbiterSignaled)
+            {
+                return MakeError(ErrorModule.Kernel, KernelErr.Timeout);
+            }
+
+            return 0;
+        }
+
+        public static ulong WaitForAddressIfLessThan(Process      Process, 
+                                                     AThreadState ThreadState, 
+                                                     AMemory      Memory, 
+                                                     long         Address, 
+                                                     int          Value, 
+                                                     ulong        Timeout, 
+                                                     bool         ShouldDecrement)
+        {
+            int CurrentValue = Memory.ReadInt32(Address);
+
+            if (CurrentValue < Value)
+            {
+                if (ShouldDecrement)
+                {
+                    Memory.WriteUInt32(Address, (uint)CurrentValue - 1);
+                }
+            }
+            else
+            {
+                return MakeError(ErrorModule.Kernel, KernelErr.InvalidState);
+            }
+
+            if (Timeout == 0)
+            {
+                return MakeError(ErrorModule.Kernel, KernelErr.Timeout);
+            }
+
+            return WaitForAddress(Process, ThreadState, Address, Timeout);
+        }
+
+        public static ulong WaitForAddressIfEqual(Process      Process, 
+                                                  AThreadState ThreadState, 
+                                                  AMemory      Memory, 
+                                                  long         Address, 
+                                                  int          Value, 
+                                                  ulong        Timeout)
+        {
+            if (Memory.ReadInt32(Address) != Value)
+            {
+                return MakeError(ErrorModule.Kernel, KernelErr.InvalidState);
+            }
+
+            if (Timeout == 0)
+            {
+                return MakeError(ErrorModule.Kernel, KernelErr.Timeout);
+            }
+
+            return WaitForAddress(Process, ThreadState, Address, Timeout);
+        }
+    }
+
+    enum ArbitrationType : int
+    {
+        WaitIfLessThan,
+        DecrementAndWaitIfLessThan,
+        WaitIfEqual
+    }
+
+    enum SignalType : int
+    {
+        Signal,
+        IncrementAndSignalIfEqual,
+        ModifyByWaitingCountAndSignalIfEqual,
+    }
+}
