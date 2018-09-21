d1556067d289fc5027f314c3b471b49046b57bf2
Ac_K <Acoustik666@gmail.com>
2018-09-21 05:00:20 +0200

    Implement friend:a IFriendService: 10101
    As I said in Ryujinx/Ryujinx-Games-List#42, ARMS need this call. Since it's an online related thing, we can partially stub it for now ( as we haven't got friends :'( ).
    I have refactored other calls a little to get more informations about them, and added symbols too.

---
+++ Ryujinx.HLE/HOS/Services/Friend/IFriendServiceTypes.cs
@@ -0,0 +1,20 @@
+﻿namespace Ryujinx.HLE.HOS.Services.Friend
+{
+    public enum PresenceStatusFilter
+    {
+        None,
+        Online,
+        OnlinePlay,
+        OnlineOrOnlinePlay
+    }
+
+    public struct FriendFilter
+    {
+        public PresenceStatusFilter PresenceStatus;
+        public bool                 IsFavoriteOnly;
+        public bool                 IsSameAppPresenceOnly;
+        public bool                 IsSameAppPlayedOnly;
+        public bool                 IsArbitraryAppPlayedOnly;
+        public long                 PresenceGroupId;
+    };
+}
