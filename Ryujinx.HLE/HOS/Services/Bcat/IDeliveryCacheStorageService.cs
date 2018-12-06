0a033574be9be5e709f5f62a108d5c5288c2e149
jduncanator <jduncanator@hotmail.com>
2018-12-07 00:45:00 +1100

    Implement EnumerateDeliveryCacheDirectory

--- Ryujinx.HLE/HOS/Services/Bcat/IDeliveryCacheStorageService.cs
+++ Ryujinx.HLE/HOS/Services/Bcat/IDeliveryCacheStorageService.cs
@@ -1,3 +1,4 @@
+using Ryujinx.Common.Logging;
 using Ryujinx.HLE.HOS.Ipc;
 using System.Collections.Generic;
 
@@ -13,9 +14,18 @@ namespace Ryujinx.HLE.HOS.Services.Bcat
         {
             _commands = new Dictionary<int, ServiceProcessRequest>
             {
-                //...
+                { 10, EnumerateDeliveryCacheDirectory }
             };
         }
 
+        private long EnumerateDeliveryCacheDirectory(ServiceCtx context)
+        {
+            Logger.PrintStub(LogClass.ServiceBcat);
+
+            // We have no cache directories, so return 0.
+            context.ResponseData.Write(0);
+
+            return 0;
+        }
     }
 }
