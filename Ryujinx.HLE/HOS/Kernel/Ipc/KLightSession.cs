e2eeec817986941eca326c8d0820db297f189e62
gdkchan <gab.dark.100@gmail.com>
2019-01-02 16:05:37 -0300

    KLightSession init

--- Ryujinx.HLE/HOS/Kernel/Ipc/KLightSession.cs
+++ Ryujinx.HLE/HOS/Kernel/Ipc/KLightSession.cs
@@ -7,6 +7,14 @@ namespace Ryujinx.HLE.HOS.Kernel.Ipc
         public KLightServerSession ServerSession { get; }
         public KLightClientSession ClientSession { get; }
 
-        public KLightSession(Horizon system) : base(system) { }
+        private bool _hasBeenInitialized;
+
+        public KLightSession(Horizon system) : base(system)
+        {
+            ServerSession = new KLightServerSession(system, this);
+            ClientSession = new KLightClientSession(system, this);
+
+            _hasBeenInitialized = true;
+        }
     }
 }
