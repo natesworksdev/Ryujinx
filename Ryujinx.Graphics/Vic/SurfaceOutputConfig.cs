bed93cc8711876a8f8cda0be760c36cf395cfb37
Alex Barney <thealexbarney@gmail.com>
2019-03-03 13:36:15 -0600

    Renaming part 9

--- Ryujinx.Graphics/Vic/SurfaceOutputConfig.cs
+++ Ryujinx.Graphics/Vic/SurfaceOutputConfig.cs
@@ -13,21 +13,21 @@ namespace Ryujinx.Graphics.Vic
         public long SurfaceChromaVAddress;
 
         public SurfaceOutputConfig(
-            SurfacePixelFormat PixelFormat,
-            int                SurfaceWidth,
-            int                SurfaceHeight,
-            int                GobBlockHeight,
-            long               OutputSurfaceLumaAddress,
-            long               OutputSurfaceChromaUAddress,
-            long               OutputSurfaceChromaVAddress)
+            SurfacePixelFormat pixelFormat,
+            int                surfaceWidth,
+            int                surfaceHeight,
+            int                gobBlockHeight,
+            long               outputSurfaceLumaAddress,
+            long               outputSurfaceChromaUAddress,
+            long               outputSurfaceChromaVAddress)
         {
-            this.PixelFormat           = PixelFormat;
-            this.SurfaceWidth          = SurfaceWidth;
-            this.SurfaceHeight         = SurfaceHeight;
-            this.GobBlockHeight        = GobBlockHeight;
-            this.SurfaceLumaAddress    = OutputSurfaceLumaAddress;
-            this.SurfaceChromaUAddress = OutputSurfaceChromaUAddress;
-            this.SurfaceChromaVAddress = OutputSurfaceChromaVAddress;
+            this.PixelFormat           = pixelFormat;
+            this.SurfaceWidth          = surfaceWidth;
+            this.SurfaceHeight         = surfaceHeight;
+            this.GobBlockHeight        = gobBlockHeight;
+            this.SurfaceLumaAddress    = outputSurfaceLumaAddress;
+            this.SurfaceChromaUAddress = outputSurfaceChromaUAddress;
+            this.SurfaceChromaVAddress = outputSurfaceChromaVAddress;
         }
     }
 }
