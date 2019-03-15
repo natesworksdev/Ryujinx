170dffd6519b02611fb53dfef69e9e0da71062fc
jduncanator <jduncanator@hotmail.com>
2019-03-15 12:13:21 +1100

    Print Process Name and PID at the start of guest stack trace

--- Ryujinx.HLE/HOS/Kernel/Process/HleProcessDebugger.cs
+++ Ryujinx.HLE/HOS/Kernel/Process/HleProcessDebugger.cs
@@ -67,18 +67,18 @@ namespace Ryujinx.HLE.HOS.Kernel.Process
 
                     string imageName = GetGuessedNsoNameFromIndex(imageIndex);
 
-                    string imageNameAndOffset = $"{imageName}:0x{offset:x8}";
-
-                    trace.AppendLine($" {imageNameAndOffset} {subName}");
+                    trace.AppendLine($"   {imageName}:0x{offset:x8} {subName}");
                 }
                 else
                 {
-                    trace.AppendLine($" ??? {subName}");
+                    trace.AppendLine($"   ??? {subName}");
                 }
             }
 
             //TODO: ARM32.
             long framePointer = (long)threadState.X29;
+
+            trace.AppendLine($"Process: {_owner.Name}, PID: {_owner.Pid}");
 
             while (framePointer != 0)
             {
