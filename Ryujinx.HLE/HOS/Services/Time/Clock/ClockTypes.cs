5a7b8122a4bf45e2c47255538c000048fadf54e2
Thog <me@thog.eu>
2019-07-13 23:38:27 +0200

    Improve SteadyClock implementation accuracy

---
+++ Ryujinx.HLE/HOS/Services/Time/Clock/ClockTypes.cs
@@ -0,0 +1,34 @@
+﻿using Ryujinx.HLE.Utilities;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Ryujinx.HLE.HOS.Services.Time.Clock
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TimeSpanType
+    {
+        public ulong NanoSeconds;
+
+        public TimeSpanType(ulong nanoSeconds)
+        {
+            NanoSeconds = nanoSeconds;
+        }
+
+        public ulong ToSeconds()
+        {
+            return NanoSeconds * 1000000000;
+        }
+
+        public static TimeSpanType FromTicks(ulong ticks, ulong frequency)
+        {
+            return new TimeSpanType((ticks * 1000) / frequency);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SteadyClockTimePoint
+    {
+        public ulong        TimePoint;
+        public UInt128      ClockSourceId;
+    }
+}
