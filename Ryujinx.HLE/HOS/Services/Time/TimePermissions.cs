ae2c967056c973459d0fc6bb5001370d758106e0
Thog <me@thog.eu>
2019-07-14 19:35:59 +0200

    Realign TimePermissions definitions

--- Ryujinx.HLE/HOS/Services/Time/TimePermissions.cs
+++ Ryujinx.HLE/HOS/Services/Time/TimePermissions.cs
@@ -5,12 +5,12 @@ namespace Ryujinx.HLE.HOS.Services.Time
     [Flags]
     enum TimePermissions
     {
-        LocalSystemClockWritableMask = 0x1,
-        UserSystemClockWritableMask = 0x2,
+        LocalSystemClockWritableMask   = 0x1,
+        UserSystemClockWritableMask    = 0x2,
         NetworkSystemClockWritableMask = 0x4,
-        UnknownPermissionMask = 0x8,
+        UnknownPermissionMask          = 0x8,
 
-        User = 0,
+        User   = 0,
         Applet = LocalSystemClockWritableMask | UserSystemClockWritableMask | UnknownPermissionMask,
         System = NetworkSystemClockWritableMask
     }
