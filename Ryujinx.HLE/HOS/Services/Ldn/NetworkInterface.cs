2c0f179041988d7f4ccee74396718e5afcf39feb
Ac_K <Acoustik666@gmail.com>
2019-11-21 01:37:21 +0100

    ldn: Implement calls of UserLocalCommunicationService
    - Implement `IUserServiceCreator: CreateUserLocalCommunicationService` according to RE.
    - Implement `IUserLocalCommunicationService` calls:
      - Every calls in this interface are layered to `NetworkInterface`.
      - `GetState` according to RE.
      - `InitializeOld`, `Initialize` and `Finalize` stubbed with the appropriate result code and some TODO according to RE.
      - `AttachStateChangeEvent` according to RE.

---
+++ Ryujinx.HLE/HOS/Services/Ldn/NetworkInterface.cs
@@ -0,0 +1,58 @@
+﻿using Ryujinx.Common.Logging;
+using Ryujinx.HLE.HOS.Kernel.Threading;
+using Ryujinx.HLE.HOS.Services.Ldn.Types;
+using System.Net;
+
+namespace Ryujinx.HLE.HOS.Services.Ldn
+{
+    internal class NetworkInterface
+    {
+        public ResultCode NetworkInterfaceState { get; set; }
+        public KEvent     StateChangeEvent      { get; private set; }
+
+        private NetworkState _state;
+
+        public NetworkInterface(Horizon system)
+        {
+            NetworkInterfaceState = ResultCode.Success;
+            StateChangeEvent      = new KEvent(system);
+
+            _state = NetworkState.None;
+        }
+
+        public ResultCode Initialize(int unknown, int version = 0, IPAddress ipv4Address = null, IPAddress subnetMaskAddress = null)
+        {
+            // TODO: Call nn::nifm::InitializeSystem().
+            //       If the call failed, it returns the result code.
+            //       If the call succeed, it signal and clear an event then start a new thread named nn.ldn.NetworkInterfaceMonitor.
+
+            Logger.PrintStub(LogClass.ServiceLdn, new { version });
+
+            // NOTE: Since we don't support ldn for now, we can return this following result code to make it disabled.
+            return ResultCode.DeviceDisabled;
+        }
+
+        public ResultCode GetState(out NetworkState state)
+        {
+            // Return ResultCode.InvalidArgument if _state is null, doesn't occur in our case.
+
+            state = _state;
+
+            return ResultCode.Success;
+        }
+
+        public ResultCode Finalize()
+        {
+            // TODO: Finalize nifm service then kill the thread named nn.ldn.NetworkInterfaceMonitor.
+
+            _state = NetworkState.None;
+
+            StateChangeEvent.WritableEvent.Signal();
+            StateChangeEvent.WritableEvent.Clear();
+
+            Logger.PrintStub(LogClass.ServiceLdn);
+
+            return ResultCode.Success;
+        }
+    }
+}
