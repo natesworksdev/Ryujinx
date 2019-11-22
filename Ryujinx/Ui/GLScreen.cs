c9e3820dacd94f3eeae93e6db8c639a5fa129475
Thog <me@thog.eu>
2019-11-22 21:20:19 +0100

    Fix profiler builds

--- Ryujinx/Ui/GLScreen.cs
+++ Ryujinx/Ui/GLScreen.cs
@@ -5,6 +5,7 @@ using Ryujinx.Configuration;
 using Ryujinx.Graphics.Gal;
 using Ryujinx.HLE;
 using Ryujinx.HLE.Input;
+using Ryujinx.Profiler.UI;
 using Ryujinx.Ui;
 using System;
 using System.Threading;
