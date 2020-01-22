natesworksdev/Ryujinx

commit d6b9babe1d73a78e963455a5a6ea3e7431a44ece

Keep the GUI alive when closing a game (#888)
* Keep the GUI alive when closing a game

Make HLE.Switch init when starting a game and dispose it when closing
the GlScreen.

This also make HLE in charge of disposing the audio and gpu backend.

* Address Ac_k's comments

* Make sure to dispose the Discord module and use GTK quit method

Also update Discord Precense when closing a game.

* Make sure to dispose MainWindow

* Address gdk's comments

Ryujinx.HLE/FileSystem/VirtualFileSystem.cs
@@ -1,3 +1,7 @@
+using LibHac;
+using LibHac.Fs;
+using LibHac.FsService;
+using LibHac.FsSystem;
 using Ryujinx.HLE.FileSystem.Content;
 using Ryujinx.HLE.HOS;
 using System;
@@ -15,6 +19,16 @@ namespace Ryujinx.HLE.FileSystem
         public static string SafeNandPath   = Path.Combine(NandPath, "safe");
         public static string SystemNandPath = Path.Combine(NandPath, "system");
         public static string UserNandPath   = Path.Combine(NandPath, "user");
+
+        public Keyset           KeySet   { get; private set; }
+        public FileSystemServer FsServer { get; private set; }
+        public FileSystemClient FsClient { get; private set; }
+        public EmulatedGameCard GameCard { get; private set; }
+
+        public VirtualFileSystem()
+        {
+            Reload();
+        }
 
         public Stream RomFs { get; private set; }
 
@@ -183,6 +197,69 @@ namespace Ryujinx.HLE.FileSystem
             return Path.Combine(appDataPath, BasePath);
         }
 
+        public void Reload()
+        {
+            ReloadKeySet();
+
+            LocalFileSystem serverBaseFs = new LocalFileSystem(GetBasePath());
+
+            DefaultFsServerObjects fsServerObjects = DefaultFsServerObjects.GetDefaultEmulatedCreators(serverBaseFs, KeySet);
+
+            GameCard = fsServerObjects.GameCard;
+
+            FileSystemServerConfig fsServerConfig = new FileSystemServerConfig
+            {
+                FsCreators     = fsServerObjects.FsCreators,
+                DeviceOperator = fsServerObjects.DeviceOperator,
+                ExternalKeySet = KeySet.ExternalKeySet
+            };
+
+            FsServer = new FileSystemServer(fsServerConfig);
+            FsClient = FsServer.CreateFileSystemClient();
+        }
+
+
+        private void ReloadKeySet()
+        {
+            string keyFile        = null;
+            string titleKeyFile   = null;
+            string consoleKeyFile = null;
+
+            string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            LoadSetAtPath(Path.Combine(home, ".switch"));
+            LoadSetAtPath(GetSystemPath());
+
+            void LoadSetAtPath(string basePath)
+            {
+                string localKeyFile        = Path.Combine(basePath, "prod.keys");
+                string localTitleKeyFile   = Path.Combine(basePath, "title.keys");
+                string localConsoleKeyFile = Path.Combine(basePath, "console.keys");
+
+                if (File.Exists(localKeyFile))
+                {
+                    keyFile = localKeyFile;
+                }
+
+                if (File.Exists(localTitleKeyFile))
+                {
+                    titleKeyFile = localTitleKeyFile;
+                }
+
+                if (File.Exists(localConsoleKeyFile))
+                {
+                    consoleKeyFile = localConsoleKeyFile;
+                }
+            }
+
+            KeySet = ExternalKeyReader.ReadKeyFile(keyFile, titleKeyFile, consoleKeyFile);
+        }
+
+        public void Unload()
+        {
+            RomFs?.Dispose();
+        }
+
         public void Dispose()
         {
             Dispose(true);
@@ -192,7 +269,7 @@ namespace Ryujinx.HLE.FileSystem
         {
             if (disposing)
             {
-                RomFs?.Dispose();
+                Unload();
             }
         }
     }

Ryujinx/Ui/ApplicationLibrary.cs
@@ -7,6 +7,7 @@ using LibHac.FsSystem.NcaUtils;
 using LibHac.Ncm;
 using LibHac.Spl;
 using Ryujinx.Common.Logging;
+using Ryujinx.Configuration.System;
 using Ryujinx.HLE.FileSystem;
 using Ryujinx.HLE.Loaders.Npdm;
 using System;
@@ -20,7 +21,6 @@ using Utf8Json;
 using Utf8Json.Resolvers;
 
 using RightsId = LibHac.Fs.RightsId;
-using TitleLanguage = Ryujinx.HLE.HOS.SystemState.TitleLanguage;
 
 namespace Ryujinx.Ui
 {
@@ -34,15 +34,15 @@ namespace Ryujinx.Ui
         private static readonly byte[] _nroIcon = GetResourceBytes("Ryujinx.Ui.assets.NROIcon.png");
         private static readonly byte[] _nsoIcon = GetResourceBytes("Ryujinx.Ui.assets.NSOIcon.png");
 
-        private static Keyset        _keySet;
-        private static TitleLanguage _desiredTitleLanguage;
+        private static Keyset   _keySet;
+        private static Language _desiredTitleLanguage;
 
-        public static void LoadApplications(List<string> appDirs, Keyset keySet, TitleLanguage desiredTitleLanguage, FileSystemClient fsClient = null, VirtualFileSystem vfs = null)
+        public static void LoadApplications(List<string> appDirs, VirtualFileSystem virtualFileSystem, Language desiredTitleLanguage)
         {
             int numApplicationsFound  = 0;
             int numApplicationsLoaded = 0;
 
-            _keySet               = keySet;
+            _keySet               = virtualFileSystem.KeySet;
             _desiredTitleLanguage = desiredTitleLanguage;
 
             // Builds the applications list with paths to found applications
@@ -346,11 +346,11 @@ namespace Ryujinx.Ui
                     filter.SetUserId(new UserId(1, 0));
                     filter.SetTitleId(new TitleId(titleIdNum));
 
-                    Result result = fsClient.FindSaveDataWithFilter(out SaveDataInfo saveDataInfo, SaveDataSpaceId.User, ref filter);
+                    Result result = virtualFileSystem.FsClient.FindSaveDataWithFilter(out SaveDataInfo saveDataInfo, SaveDataSpaceId.User, ref filter);
 
                     if (result.IsSuccess())
                     {
-                        saveDataPath = Path.Combine(vfs.GetNandPath(), $"user/save/{saveDataInfo.SaveDataId:x16}");
+                        saveDataPath = Path.Combine(virtualFileSystem.GetNandPath(), $"user/save/{saveDataInfo.SaveDataId:x16}");
                     }
                 }
 

Ryujinx/Ui/MainWindow.cs
@@ -3,8 +3,10 @@ using JsonPrettyPrinterPlus;
 using Ryujinx.Audio;
 using Ryujinx.Common.Logging;
 using Ryujinx.Configuration;
+using Ryujinx.Graphics.GAL;
 using Ryujinx.Graphics.OpenGL;
 using Ryujinx.HLE.FileSystem;
+using Ryujinx.HLE.FileSystem.Content;
 using Ryujinx.Profiler;
 using System;
 using System.Diagnostics;
@@ -22,11 +24,10 @@ namespace Ryujinx.Ui
 {
     public class MainWindow : Window
     {
-        private static HLE.Switch _device;
+        private static VirtualFileSystem _virtualFileSystem;
+        private static ContentManager    _contentManager;
 
-        private static Renderer _renderer;
-
-        private static IAalOutput _audioOut;
+        private static HLE.Switch _emulationContext;
 
         private static GlScreen _screen;
 
@@ -75,28 +76,28 @@ namespace Ryujinx.Ui
 
             _gameTable.ButtonReleaseEvent += Row_Clicked;
 
+            // First we check that a migration isn't needed. (because VirtualFileSystem will create the new directory otherwise)
             bool continueWithStartup = Migration.PromptIfMigrationNeededForStartup(this, out bool migrationNeeded);
             if (!continueWithStartup)
             {
-                End();
+                End(null);
             }
 
-            _renderer = new Renderer();
-
-            _audioOut = InitializeAudioEngine();
-
-            // TODO: Initialization and dispose of HLE.Switch when starting/stoping emulation.
-            _device = InitializeSwitchInstance();
+            _virtualFileSystem = new VirtualFileSystem();
+            _contentManager    = new ContentManager(_virtualFileSystem);
 
             if (migrationNeeded)
             {
-                bool migrationSuccessful = Migration.DoMigrationForStartup(this, _device);
+                bool migrationSuccessful = Migration.DoMigrationForStartup(this, _virtualFileSystem);
 
                 if (!migrationSuccessful)
                 {
-                    End();
+                    End(null);
                 }
             }
+
+            // Make sure that everything is loaded.
+            _virtualFileSystem.Reload();
 
             _treeView = _gameTable;
 
@@ -199,7 +200,9 @@ namespace Ryujinx.Ui
 
         private HLE.Switch InitializeSwitchInstance()
         {
-            HLE.Switch instance = new HLE.Switch(_renderer, _audioOut);
+            _virtualFileSystem.Reload();
+
+            HLE.Switch instance = new HLE.Switch(_virtualFileSystem, _contentManager, InitializeRenderer(), InitializeAudioEngine());
 
             instance.Initialize();
 
@@ -218,8 +221,7 @@ namespace Ryujinx.Ui
             _tableStore.Clear();
 
             await Task.Run(() => ApplicationLibrary.LoadApplications(ConfigurationState.Instance.Ui.GameDirs,
-                _device.System.KeySet, _device.System.State.DesiredTitleLanguage, _device.System.FsClient,
-                _device.FileSystem));
+                _virtualFileSystem, ConfigurationState.Instance.System.Language));
 
             _updatingGameTable = false;
         }
@@ -234,8 +236,10 @@ namespace Ryujinx.Ui
             {
                 Logger.RestartTime();
 
+                HLE.Switch device = InitializeSwitchInstance();
+
                 // TODO: Move this somewhere else + reloadable?
-                Ryujinx.Graphics.Gpu.GraphicsConfig.ShadersDumpPath = ConfigurationState.Instance.Graphics.ShadersDumpPath;
+                Graphics.Gpu.GraphicsConfig.ShadersDumpPath = ConfigurationState.Instance.Graphics.ShadersDumpPath;
 
                 if (Directory.Exists(path))
                 {
@@ -249,12 +253,12 @@ namespace Ryujinx.Ui
                     if (romFsFiles.Length > 0)
                     {
                         Logger.PrintInfo(LogClass.Application, "Loading as cart with RomFS.");
-                        _device.LoadCart(path, romFsFiles[0]);
+                        device.LoadCart(path, romFsFiles[0]);
                     }
                     else
                     {
                         Logger.PrintInfo(LogClass.Application, "Loading as cart WITHOUT RomFS.");
-                        _device.LoadCart(path);
+                        device.LoadCart(path);
                     }
                 }
                 else if (File.Exists(path))
@@ -263,22 +267,22 @@ namespace Ryujinx.Ui
                     {
                         case ".xci":
                             Logger.PrintInfo(LogClass.Application, "Loading as XCI.");
-                            _device.LoadXci(path);
+                            device.LoadXci(path);
                             break;
                         case ".nca":
                             Logger.PrintInfo(LogClass.Application, "Loading as NCA.");
-                            _device.LoadNca(path);
+                            device.LoadNca(path);
                             break;
                         case ".nsp":
                         case ".pfs0":
                             Logger.PrintInfo(LogClass.Application, "Loading as NSP.");
-                            _device.LoadNsp(path);
+                            device.LoadNsp(path);
                             break;
                         default:
                             Logger.PrintInfo(LogClass.Application, "Loading as homebrew.");
                             try
                             {
-                                _device.LoadProgram(path);
+                                device.LoadProgram(path);
                             }
                             catch (ArgumentOutOfRangeException)
                             {
@@ -290,13 +294,15 @@ namespace Ryujinx.Ui
                 else
                 {
                     Logger.PrintWarning(LogClass.Application, "Please specify a valid XCI/NCA/NSP/PFS0/NRO file.");
-                    End();
+                    End(device);
                 }
 
+                _emulationContext = device;
+
 #if MACOS_BUILD
-                CreateGameWindow();
+                CreateGameWindow(device);
 #else
-                new Thread(CreateGameWindow).Start();
+                new Thread(() => CreateGameWindow(device)).Start();
 #endif
 
                 _gameLoaded              = true;
@@ -305,28 +311,55 @@ namespace Ryujinx.Ui
                 _firmwareInstallFile.Sensitive      = false;
                 _firmwareInstallDirectory.Sensitive = false;
 
-                DiscordIntegrationModule.SwitchToPlayingState(_device.System.TitleIdText, _device.System.TitleName);
+                DiscordIntegrationModule.SwitchToPlayingState(device.System.TitleIdText, device.System.TitleName);
 
-                ApplicationLibrary.LoadAndSaveMetaData(_device.System.TitleIdText, appMetadata =>
+                ApplicationLibrary.LoadAndSaveMetaData(device.System.TitleIdText, appMetadata =>
                 {
                     appMetadata.LastPlayed = DateTime.UtcNow.ToString();
                 });
             }
         }
 
-        private static void CreateGameWindow()
+        private void CreateGameWindow(HLE.Switch device)
         {
-            _device.Hid.InitializePrimaryController(ConfigurationState.Instance.Hid.ControllerType);
+            device.Hid.InitializePrimaryController(ConfigurationState.Instance.Hid.ControllerType);
 
-            using (_screen = new GlScreen(_device, _renderer))
+            using (_screen = new GlScreen(device))
             {
                 _screen.MainLoop();
+            }
 
-                End();
+            device.Dispose();
+
+            _emulationContext = null;
+            _screen           = null;
+            _gameLoaded       = false;
+
+            DiscordIntegrationModule.SwitchToMainMenu();
+
+            Application.Invoke(delegate
+            {
+                _stopEmulation.Sensitive            = false;
+                _firmwareInstallFile.Sensitive      = true;
+                _firmwareInstallDirectory.Sensitive = true;
+            });
+        }
+
+        private static void UpdateGameMetadata(string titleId)
+        {
+            if (_gameLoaded)
+            {
+                ApplicationLibrary.LoadAndSaveMetaData(titleId, appMetadata =>
+                {
+                    DateTime lastPlayedDateTime = DateTime.Parse(appMetadata.LastPlayed);
+                    double   sessionTimePlayed  = DateTime.UtcNow.Subtract(lastPlayedDateTime).TotalSeconds;
+
+                    appMetadata.TimePlayed += Math.Round(sessionTimePlayed, MidpointRounding.AwayFromZero);
+                });
             }
         }
 
-        private static void End()
+        private void End(HLE.Switch device)
         {
             if (_ending)
             {
@@ -335,22 +368,23 @@ namespace Ryujinx.Ui
 
             _ending = true;
 
-            if (_gameLoaded)
+            if (device != null)
             {
-                ApplicationLibrary.LoadAndSaveMetaData(_device.System.TitleIdText, appMetadata =>
-                {
-                    DateTime lastPlayedDateTime = DateTime.Parse(appMetadata.LastPlayed);
-                    double sessionTimePlayed = DateTime.UtcNow.Subtract(lastPlayedDateTime).TotalSeconds;
-
-                    appMetadata.TimePlayed += Math.Round(sessionTimePlayed, MidpointRounding.AwayFromZero);
-                });
+                UpdateGameMetadata(device.System.TitleIdText);
             }
 
+            Dispose();
+
             Profile.FinishProfiling();
-            _device?.Dispose();
-            _audioOut?.Dispose();
+            device?.Dispose();
+            DiscordIntegrationModule.Exit();
             Logger.Shutdown();
-            Environment.Exit(0);
+            Application.Quit();
+        }
+
+        private static IRenderer InitializeRenderer()
+        {
+            return new Renderer();
         }
 
         /// <summary>
@@ -427,7 +461,7 @@ namespace Ryujinx.Ui
 
             if (treeIter.UserData == IntPtr.Zero) return;
 
-            GameTableContextMenu contextMenu = new GameTableContextMenu(_tableStore, treeIter, _device.System.FsClient);
+            GameTableContextMenu contextMenu = new GameTableContextMenu(_tableStore, treeIter, _virtualFileSystem.FsClient);
             contextMenu.ShowAll();
             contextMenu.PopupAtPointer(null);
         }
@@ -477,20 +511,18 @@ namespace Ryujinx.Ui
         private void Exit_Pressed(object sender, EventArgs args)
         {
             _screen?.Exit();
-            End();
+            End(_emulationContext);
         }
 
         private void Window_Close(object sender, DeleteEventArgs args)
         {
             _screen?.Exit();
-            End();
+            End(_emulationContext);
         }
 
         private void StopEmulation_Pressed(object sender, EventArgs args)
         {
-            // TODO: Write logic to kill running game
-
-            _gameLoaded = false;
+            _screen?.Exit();
         }
 
         private void Installer_File_Pressed(object o, EventArgs args)
@@ -525,7 +557,7 @@ namespace Ryujinx.Ui
 
         private void RefreshFirmwareLabel()
         {
-            var currentFirmware = _device.System.GetCurrentFirmwareVersion();
+            var currentFirmware = _contentManager.GetCurrentFirmwareVersion();
 
             GLib.Idle.Add(new GLib.IdleHandler(() =>
             {
@@ -547,7 +579,7 @@ namespace Ryujinx.Ui
 
                     fileChooser.Dispose();
 
-                    var firmwareVersion = _device.System.VerifyFirmwarePackage(filename);
+                    var firmwareVersion = _contentManager.VerifyFirmwarePackage(filename);
 
                     if (firmwareVersion == null)
                     {
@@ -566,7 +598,7 @@ namespace Ryujinx.Ui
                         return;
                     }
 
-                    var currentVersion = _device.System.GetCurrentFirmwareVersion();
+                    var currentVersion = _contentManager.GetCurrentFirmwareVersion();
 
                     string dialogMessage = $"System version {firmwareVersion.VersionString} will be installed.";
 
@@ -606,7 +638,7 @@ namespace Ryujinx.Ui
 
                             try
                             {
-                                _device.System.InstallFirmware(filename);
+                                _contentManager.InstallFirmware(filename);
 
                                 GLib.Idle.Add(new GLib.IdleHandler(() =>
                                 {

Ryujinx.Graphics.Gpu/GpuContext.cs
@@ -116,6 +116,7 @@ namespace Ryujinx.Graphics.Gpu
             Methods.ShaderCache.Dispose();
             Methods.BufferManager.Dispose();
             Methods.TextureManager.Dispose();
+            Renderer.Dispose();
         }
     }
 }

commit 053a36bf5aa0100ae6249653aa783ef6f1a7d760

Make VirtualFileSystem only instanciable once
This fix a regression caused by #888 on temporary saves for SNES Online.
(and probably other games)

Ryujinx.HLE/FileSystem/VirtualFileSystem.cs
@@ -20,12 +20,14 @@ namespace Ryujinx.HLE.FileSystem
         public static string SystemNandPath = Path.Combine(NandPath, "system");
         public static string UserNandPath   = Path.Combine(NandPath, "user");
 
+        private static bool _isInitialized = false;
+
         public Keyset           KeySet   { get; private set; }
         public FileSystemServer FsServer { get; private set; }
         public FileSystemClient FsClient { get; private set; }
         public EmulatedGameCard GameCard { get; private set; }
 
-        public VirtualFileSystem()
+        private VirtualFileSystem()
         {
             Reload();
         }
@@ -271,6 +273,18 @@ namespace Ryujinx.HLE.FileSystem
             {
                 Unload();
             }
+        }
+
+        public static VirtualFileSystem CreateInstance()
+        {
+            if (_isInitialized)
+            {
+                throw new InvalidOperationException($"VirtualFileSystem can only be instanciated once!");
+            }
+
+            _isInitialized = true;
+
+            return new VirtualFileSystem();
         }
     }
 }

Ryujinx/Ui/ApplicationLibrary.cs
@@ -34,15 +34,15 @@ namespace Ryujinx.Ui
         private static readonly byte[] _nroIcon = GetResourceBytes("Ryujinx.Ui.assets.NROIcon.png");
         private static readonly byte[] _nsoIcon = GetResourceBytes("Ryujinx.Ui.assets.NSOIcon.png");
 
-        private static Keyset   _keySet;
-        private static Language _desiredTitleLanguage;
+        private static VirtualFileSystem _virtualFileSystem;
+        private static Language          _desiredTitleLanguage;
 
         public static void LoadApplications(List<string> appDirs, VirtualFileSystem virtualFileSystem, Language desiredTitleLanguage)
         {
             int numApplicationsFound  = 0;
             int numApplicationsLoaded = 0;
 
-            _keySet               = virtualFileSystem.KeySet;
+            _virtualFileSystem    = virtualFileSystem;
             _desiredTitleLanguage = desiredTitleLanguage;
 
             // Builds the applications list with paths to found applications
@@ -77,7 +77,7 @@ namespace Ryujinx.Ui
                             {
                                 nsp.OpenFile(out IFile ncaFile, fileEntry.FullPath, OpenMode.Read).ThrowIfFailure();
 
-                                Nca nca       = new Nca(_keySet, ncaFile.AsStorage());
+                                Nca nca       = new Nca(_virtualFileSystem.KeySet, ncaFile.AsStorage());
                                 int dataIndex = Nca.GetSectionIndexFromType(NcaSectionType.Data, NcaContentType.Program);
 
                                 if (nca.Header.ContentType == NcaContentType.Program && !nca.Header.GetFsHeader(dataIndex).IsPatchSection())
@@ -103,7 +103,7 @@ namespace Ryujinx.Ui
                     {
                         try
                         {
-                            Nca nca       = new Nca(_keySet, new FileStream(app, FileMode.Open, FileAccess.Read).AsStorage());
+                            Nca nca       = new Nca(_virtualFileSystem.KeySet, new FileStream(app, FileMode.Open, FileAccess.Read).AsStorage());
                             int dataIndex = Nca.GetSectionIndexFromType(NcaSectionType.Data, NcaContentType.Program);
 
                             if (nca.Header.ContentType != NcaContentType.Program || nca.Header.GetFsHeader(dataIndex).IsPatchSection())
@@ -145,7 +145,7 @@ namespace Ryujinx.Ui
                              
                             if (Path.GetExtension(applicationPath) == ".xci")
                             {
-                                Xci xci = new Xci(_keySet, file.AsStorage());
+                                Xci xci = new Xci(_virtualFileSystem.KeySet, file.AsStorage());
 
                                 pfs = xci.OpenPartition(XciPartitionType.Secure);
                             }
@@ -409,7 +409,7 @@ namespace Ryujinx.Ui
                 {
                     Ticket ticket = new Ticket(ticketFile.AsStream());
 
-                    _keySet.ExternalKeySet.Add(new RightsId(ticket.RightsId), new AccessKey(ticket.GetTitleKey(_keySet)));
+                    _virtualFileSystem.KeySet.ExternalKeySet.Add(new RightsId(ticket.RightsId), new AccessKey(ticket.GetTitleKey(_virtualFileSystem.KeySet)));
                 }
             }
 
@@ -418,7 +418,7 @@ namespace Ryujinx.Ui
             {
                 pfs.OpenFile(out IFile ncaFile, fileEntry.FullPath, OpenMode.Read).ThrowIfFailure();
 
-                Nca nca = new Nca(_keySet, ncaFile.AsStorage());
+                Nca nca = new Nca(_virtualFileSystem.KeySet, ncaFile.AsStorage());
 
                 if (nca.Header.ContentType == NcaContentType.Control)
                 {
@@ -432,7 +432,7 @@ namespace Ryujinx.Ui
 
         internal static ApplicationMetadata LoadAndSaveMetaData(string titleId, Action<ApplicationMetadata> modifyFunction = null)
         {
-            string metadataFolder = Path.Combine(new VirtualFileSystem().GetBasePath(), "games", titleId, "gui");
+            string metadataFolder = Path.Combine(_virtualFileSystem.GetBasePath(), "games", titleId, "gui");
             string metadataFile = Path.Combine(metadataFolder, "metadata.json");
 
             IJsonFormatterResolver resolver = CompositeResolver.Create(new[] { StandardResolver.AllowPrivateSnakeCase });

Ryujinx/Ui/GameTableContextMenu.cs
@@ -18,7 +18,7 @@ namespace Ryujinx.Ui
     {
         private static ListStore _gameTableStore;
         private static TreeIter  _rowIter;
-        private FileSystemClient _fsClient;
+        private VirtualFileSystem _virtualFileSystem;
 
 #pragma warning disable CS0649
 #pragma warning disable IDE0044
@@ -26,18 +26,18 @@ namespace Ryujinx.Ui
 #pragma warning restore CS0649
 #pragma warning restore IDE0044
 
-        public GameTableContextMenu(ListStore gameTableStore, TreeIter rowIter, FileSystemClient fsClient)
-            : this(new Builder("Ryujinx.Ui.GameTableContextMenu.glade"), gameTableStore, rowIter, fsClient) { }
+        public GameTableContextMenu(ListStore gameTableStore, TreeIter rowIter, VirtualFileSystem virtualFileSystem)
+            : this(new Builder("Ryujinx.Ui.GameTableContextMenu.glade"), gameTableStore, rowIter, virtualFileSystem) { }
 
-        private GameTableContextMenu(Builder builder, ListStore gameTableStore, TreeIter rowIter, FileSystemClient fsClient) : base(builder.GetObject("_contextMenu").Handle)
+        private GameTableContextMenu(Builder builder, ListStore gameTableStore, TreeIter rowIter, VirtualFileSystem virtualFileSystem) : base(builder.GetObject("_contextMenu").Handle)
         {
             builder.Autoconnect(this);
 
             _openSaveDir.Activated += OpenSaveDir_Clicked;
 
-            _gameTableStore = gameTableStore;
-            _rowIter        = rowIter;
-            _fsClient       = fsClient;
+            _gameTableStore    = gameTableStore;
+            _rowIter           = rowIter;
+            _virtualFileSystem = virtualFileSystem;
         }
 
         //Events
@@ -76,7 +76,7 @@ namespace Ryujinx.Ui
             filter.SetUserId(new UserId(1, 0));
             filter.SetTitleId(new TitleId(titleId));
 
-            Result result = _fsClient.FindSaveDataWithFilter(out SaveDataInfo saveDataInfo, SaveDataSpaceId.User, ref filter);
+            Result result = _virtualFileSystem.FsClient.FindSaveDataWithFilter(out SaveDataInfo saveDataInfo, SaveDataSpaceId.User, ref filter);
 
             if (result == ResultFs.TargetNotFound)
             {
@@ -95,7 +95,7 @@ namespace Ryujinx.Ui
                     return false;
                 }
 
-                result = _fsClient.CreateSaveData(new TitleId(titleId), new UserId(1, 0), new TitleId(titleId), 0, 0, 0);
+                result = _virtualFileSystem.FsClient.CreateSaveData(new TitleId(titleId), new UserId(1, 0), new TitleId(titleId), 0, 0, 0);
 
                 if (result.IsFailure())
                 {
@@ -105,7 +105,7 @@ namespace Ryujinx.Ui
                 }
 
                 // Try to find the savedata again after creating it
-                result = _fsClient.FindSaveDataWithFilter(out saveDataInfo, SaveDataSpaceId.User, ref filter);
+                result = _virtualFileSystem.FsClient.FindSaveDataWithFilter(out saveDataInfo, SaveDataSpaceId.User, ref filter);
             }
 
             if (result.IsSuccess())
@@ -122,7 +122,7 @@ namespace Ryujinx.Ui
 
         private string GetSaveDataDirectory(ulong saveDataId)
         {
-            string saveRootPath = System.IO.Path.Combine(new VirtualFileSystem().GetNandPath(), $"user/save/{saveDataId:x16}");
+            string saveRootPath = System.IO.Path.Combine(_virtualFileSystem.GetNandPath(), $"user/save/{saveDataId:x16}");
 
             if (!Directory.Exists(saveRootPath))
             {

Ryujinx/Ui/MainWindow.cs
@@ -83,7 +83,7 @@ namespace Ryujinx.Ui
                 End(null);
             }
 
-            _virtualFileSystem = new VirtualFileSystem();
+            _virtualFileSystem = VirtualFileSystem.CreateInstance();
             _contentManager    = new ContentManager(_virtualFileSystem);
 
             if (migrationNeeded)
@@ -461,7 +461,7 @@ namespace Ryujinx.Ui
 
             if (treeIter.UserData == IntPtr.Zero) return;
 
-            GameTableContextMenu contextMenu = new GameTableContextMenu(_tableStore, treeIter, _virtualFileSystem.FsClient);
+            GameTableContextMenu contextMenu = new GameTableContextMenu(_tableStore, treeIter, _virtualFileSystem);
             contextMenu.ShowAll();
             contextMenu.PopupAtPointer(null);
         }
@@ -502,7 +502,7 @@ namespace Ryujinx.Ui
         {
             Process.Start(new ProcessStartInfo()
             {
-                FileName        = new VirtualFileSystem().GetBasePath(),
+                FileName        = _virtualFileSystem.GetBasePath(),
                 UseShellExecute = true,
                 Verb            = "open"
             });
@@ -738,7 +738,7 @@ namespace Ryujinx.Ui
 
         private void Update_Pressed(object sender, EventArgs args)
         {
-            string ryuUpdater = System.IO.Path.Combine(new VirtualFileSystem().GetBasePath(), "RyuUpdater.exe");
+            string ryuUpdater = System.IO.Path.Combine(_virtualFileSystem.GetBasePath(), "RyuUpdater.exe");
 
             try
             {