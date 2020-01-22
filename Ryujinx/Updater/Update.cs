e94c637559280e3963139255a5f5358ee52f8c34
DrHacknik <zoltx23@gmail.com>
2020-01-22 17:54:08 -0500

    Working

---
+++ Ryujinx/Updater/Update.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ryujinx.Updater
+{
+    public class Update
+    {
+        public static string _RyuDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Ryujinx");
+        public static void PerformUpdate()
+        {
+
+            return;
+        }
+
+        private void Cleanup()
+        {
+            return;
+        }
+    }
+}
