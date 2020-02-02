664c25b54d822804c4a90dd5d6696721c15eecfd
MelonSpeedruns <melonspeedruns@outlook.com>
2020-02-02 15:23:50 -0500

    Now showing download percentage!

--- Ryujinx/Ui/AboutWindow.cs
+++ Ryujinx/Ui/AboutWindow.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using Utf8Json;
-using Utf8Json.Resolvers;
 
 using GUI = Gtk.Builder.ObjectAttribute;
 
