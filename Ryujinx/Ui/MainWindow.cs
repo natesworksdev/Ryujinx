6d7a4bab96d93280fcf93018637dc9ae8eb3d938
Cristallix <jordi55@hotmail.fr>
2020-02-08 00:29:55 +0100

    chore : remove more warnings

--- Ryujinx/Ui/MainWindow.cs
+++ Ryujinx/Ui/MainWindow.cs
@@ -16,11 +16,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Utf8Json;
-using Utf8Json.Resolvers;
 
 using GUI = Gtk.Builder.ObjectAttribute;
 
@@ -42,14 +39,17 @@ namespace Ryujinx.Ui
         private static bool _updatingGameTable;
         private static bool _gameLoaded;
         private static bool _ending;
+#pragma warning disable CS0169
         private static bool _debuggerOpened;
+#pragma warning restore CS0169
 
         private static TreeView _treeView;
 
+#pragma warning disable CS0169
         private static Ryujinx.Debugger.Debugger _debugger;
+#pragma warning restore CS0169
 
-#pragma warning disable CS0649
-#pragma warning disable IDE0044
+#pragma warning disable CS0169, CS0649, IDE0044
 
         [GUI] Window         _mainWin;
         [GUI] MenuBar        _menuBar;
@@ -83,8 +83,7 @@ namespace Ryujinx.Ui
         [GUI] Label          _vSyncStatus;
         [GUI] Box            _listStatusBox;
 
-#pragma warning restore CS0649
-#pragma warning restore IDE0044
+#pragma warning restore CS0649, IDE0044, CS0169
 
         public MainWindow() : this(new Builder("Ryujinx.Ui.MainWindow.glade")) { }
 
