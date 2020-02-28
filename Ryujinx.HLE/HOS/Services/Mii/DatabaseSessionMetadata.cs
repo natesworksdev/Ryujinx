05ac2450fa41ad1b7ae7df8118c27058bd66bb87
Thog <me@thog.eu>
2020-02-29 00:20:43 +0100

    Remove the StructLayout of DatabaseSessionMetadata

--- Ryujinx.HLE/HOS/Services/Mii/DatabaseSessionMetadata.cs
+++ Ryujinx.HLE/HOS/Services/Mii/DatabaseSessionMetadata.cs
@@ -3,14 +3,12 @@ using System.Runtime.InteropServices;
 
 namespace Ryujinx.HLE.HOS.Services.Mii
 {
-    [StructLayout(LayoutKind.Sequential, Pack = 4, Size = 0x10)]
     class DatabaseSessionMetadata
     {
         public uint  InterfaceVersion;
         public ulong UpdateCounter;
 
         public SpecialMiiKeyCode MiiKeyCode { get; private set; }
-
 
         public DatabaseSessionMetadata(ulong updateCounter, SpecialMiiKeyCode miiKeyCode)
         {
