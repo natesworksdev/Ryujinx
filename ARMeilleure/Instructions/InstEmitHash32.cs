562e6b009a645d0b40dab9e05d726932e7e6bfaa
riperiperi <rhy3756547@hotmail.com>
2020-03-13 00:44:17 +0000

    Add CRC32 A32 instructions.

---
+++ ARMeilleure/Instructions/InstEmitHash32.cs
@@ -0,0 +1,54 @@
+﻿using ARMeilleure.Decoders;
+using ARMeilleure.IntermediateRepresentation;
+using ARMeilleure.Translation;
+using System;
+
+using static ARMeilleure.Instructions.InstEmitHelper;
+
+namespace ARMeilleure.Instructions
+{
+    static partial class InstEmit32
+    {
+        public static void Crc32b(ArmEmitterContext context)
+        {
+            EmitCrc32Call(context, new _U32_U32_U8(SoftFallback.Crc32b));
+        }
+
+        public static void Crc32h(ArmEmitterContext context)
+        {
+            EmitCrc32Call(context, new _U32_U32_U16(SoftFallback.Crc32h));
+        }
+
+        public static void Crc32w(ArmEmitterContext context)
+        {
+            EmitCrc32Call(context, new _U32_U32_U32(SoftFallback.Crc32w));
+        }
+
+        public static void Crc32cb(ArmEmitterContext context)
+        {
+            EmitCrc32Call(context, new _U32_U32_U8(SoftFallback.Crc32cb));
+        }
+
+        public static void Crc32ch(ArmEmitterContext context)
+        {
+            EmitCrc32Call(context, new _U32_U32_U16(SoftFallback.Crc32ch));
+        }
+
+        public static void Crc32cw(ArmEmitterContext context)
+        {
+            EmitCrc32Call(context, new _U32_U32_U32(SoftFallback.Crc32cw));
+        }
+
+        private static void EmitCrc32Call(ArmEmitterContext context, Delegate dlg)
+        {
+            IOpCode32AluReg op = (IOpCode32AluReg)context.CurrOp;
+
+            Operand n = GetIntOrZR(context, op.Rn);
+            Operand m = GetIntOrZR(context, op.Rm);
+
+            Operand d = context.Call(dlg, n, m);
+
+            EmitAluStore(context, d);
+        }
+    }
+}
---
+++ Ryujinx.Tests/Cpu/CpuTestAluBinary32.cs
@@ -0,0 +1,41 @@
+﻿#define AluBinary32
+
+using NUnit.Framework;
+
+namespace Ryujinx.Tests.Cpu
+{
+    [Category("AluBinary32")]
+    public sealed class CpuTestAluBinary32 : CpuTest32
+    {
+#if AluBinary32
+        private const int RndCnt = 2;
+
+        [Test, Pairwise, Description("CRC32X <Wd>, <Wn>, <Xm>")]
+        public void Crc32_Crc32c_b_h_w([Values(0u, 13u)] uint rd,
+                                       [Values(1u, 13u)] uint rn,
+                                       [Values(2u, 13u)] uint rm,
+                                       [Values] bool c,
+                                       [Range(0u, 2u)] uint size,
+                                       [Values(0x00000000u, 0xFFFFFFFFu)] [Random(RndCnt)] uint wn,
+                                       [Values(0x00_00_00_00u,
+                                               0x7F_FF_FF_FFu,
+                                               0x80_00_00_00u,
+                                               0xFF_FF_FF_FFu)] [Random(RndCnt)] uint wm)
+        {
+            uint opcode = 0xe1000040; // CRC32B R0, R0, R0
+            opcode |= ((rm & 15) << 0) | ((rd & 15) << 12) | ((rn & 15) << 16);
+            opcode |= size << 21;
+            if (c)
+            {
+                opcode |= 1 << 9;
+            }
+
+            uint sp = TestContext.CurrentContext.Random.NextUInt();
+
+            SingleOpcode(opcode, r1: wn, r2: wm, sp: sp);
+
+            CompareAgainstUnicorn();
+        }
+#endif
+    }
+}
