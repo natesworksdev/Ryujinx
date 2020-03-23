55a7ff7cfa25d8edf56588f3282b596f99128821
gdkchan <gab.dark.100@gmail.com>
2020-03-23 01:34:52 -0300

    Initial conditional rendering support

---
+++ Ryujinx.Graphics.Gpu/Engine/MethodConditionalRendering.cs
@@ -0,0 +1,29 @@
+﻿using Ryujinx.Common.Logging;
+using Ryujinx.Graphics.Gpu.State;
+
+namespace Ryujinx.Graphics.Gpu.Engine
+{
+    partial class Methods
+    {
+        private bool GetRenderEnable(GpuState state)
+        {
+            ConditionState condState = state.Get<ConditionState>(MethodOffset.ConditionState);
+
+            switch (condState.Condition)
+            {
+                case Condition.Always:
+                    return true;
+                case Condition.Never:
+                    return false;
+                case Condition.ResultNonZero:
+                case Condition.Equal:
+                case Condition.NotEqual:
+                    return false; // TODO (should we use the host API?)
+            }
+
+            Logger.PrintWarning(LogClass.Gpu, $"Invalid conditional render condition \"{condState.Condition}\".");
+
+            return true;
+        }
+    }
+}
