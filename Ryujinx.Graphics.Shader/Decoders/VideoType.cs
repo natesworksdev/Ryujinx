5ec9ef279c1a9424cbcbe6afd169eb3bee91a30b
gdkchan <gab.dark.100@gmail.com>
2020-03-24 22:07:26 -0300

    Implement VMNMX shader instruction

---
+++ Ryujinx.Graphics.Shader/Decoders/VideoType.cs
@@ -0,0 +1,15 @@
+﻿namespace Ryujinx.Graphics.Shader.Decoders
+{
+    enum VideoType
+    {
+        U8  = 0,
+        U16 = 2,
+        U32 = 3,
+
+        Signed = 1 << 2,
+
+        S8  = Signed | U8,
+        S16 = Signed | U16,
+        S32 = Signed | U32
+    }
+}
