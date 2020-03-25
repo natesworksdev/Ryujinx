f126991e89adbc4104ea9b529ac8124febf1bd27
EliseZeroTwo <elise@headpat.services>
2020-03-25 18:28:31 +0100

    Remove OpCodeNop/Depbar and use OpCode instead

--- Ryujinx.Graphics.Shader/Decoders/OpCodeDepbar.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ryujinx.Graphics.Shader.Instructions;
-
-namespace Ryujinx.Graphics.Shader.Decoders
-{
-    class OpCodeDepbar : OpCode
-    {
-        public OpCodeDepbar(InstEmitter emitter, ulong address, long opCode) : base(emitter, address, opCode) {}
-    }
-}
--- Ryujinx.Graphics.Shader/Decoders/OpCodeNop.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ryujinx.Graphics.Shader.Instructions;
-
-namespace Ryujinx.Graphics.Shader.Decoders
-{
-    class OpCodeNop : OpCode
-    {
-        public OpCodeNop(InstEmitter emitter, ulong address, long opCode) : base(emitter, address, opCode) {}
-    }
-}
