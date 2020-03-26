2c417d83d2ae5d1c5e42410c84776c93dc57d2c9
EliseZeroTwo <elise@headpat.services>
2020-03-26 15:00:50 +0100

    Fix formatting

--- Ryujinx.HLE/Loaders/Executables/NxStaticObject.cs
+++ Ryujinx.HLE/Loaders/Executables/NxStaticObject.cs
@@ -11,11 +11,11 @@ namespace Ryujinx.HLE.Loaders.Executables
         Nso nso;
 
         public byte[] Text { get; private set; }
-        public byte[] Ro   { get; private set; }
+        public byte[] Ro { get; private set; }
         public byte[] Data { get; private set; }
 
         public int TextOffset => (int)nso.Sections[0].MemoryOffset;
-        public int RoOffset   => (int)nso.Sections[1].MemoryOffset;
+        public int RoOffset => (int)nso.Sections[1].MemoryOffset;
         public int DataOffset => (int)nso.Sections[2].MemoryOffset;
         public int BssOffset => DataOffset + Data.Length;
         public int BssSize => (int)nso.BssSize;
@@ -24,11 +24,11 @@ namespace Ryujinx.HLE.Loaders.Executables
         private enum NsoFlags
         {
             IsTextCompressed = 1 << 0,
-            IsRoCompressed   = 1 << 1,
+            IsRoCompressed = 1 << 1,
             IsDataCompressed = 1 << 2,
-            HasTextHash      = 1 << 3,
-            HasRoHash        = 1 << 4,
-            HasDataHash      = 1 << 5
+            HasTextHash = 1 << 3,
+            HasRoHash = 1 << 4,
+            HasDataHash = 1 << 5
         }
 
         public NxStaticObject(IStorage inStorage)
@@ -38,6 +38,6 @@ namespace Ryujinx.HLE.Loaders.Executables
             Ro = nso.Sections[1].DecompressSection();
             Data = nso.Sections[2].DecompressSection();
         }
-        
+
     }
 }
