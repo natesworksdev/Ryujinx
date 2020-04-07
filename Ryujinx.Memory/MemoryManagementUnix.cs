fa9535872ed24c3d9ee788c4574cd7af151bfd2d
gdkchan <gab.dark.100@gmail.com>
2020-04-07 00:43:25 -0300

    Fix leak on unix memory allocation

--- Ryujinx.Memory/MemoryManagementUnix.cs
+++ Ryujinx.Memory/MemoryManagementUnix.cs
@@ -20,11 +20,9 @@ namespace Ryujinx.Memory
 
         private static IntPtr AllocateInternal(ulong size, MmapProts prot)
         {
-            ulong pageSize = (ulong)Syscall.sysconf(SysconfName._SC_PAGESIZE);
-
             const MmapFlags flags = MmapFlags.MAP_PRIVATE | MmapFlags.MAP_ANONYMOUS;
 
-            IntPtr ptr = Syscall.mmap(IntPtr.Zero, size + pageSize, prot, flags, -1, 0);
+            IntPtr ptr = Syscall.mmap(IntPtr.Zero, size, prot, flags, -1, 0);
 
             if (ptr == new IntPtr(-1L))
             {
