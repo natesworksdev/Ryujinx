08ec1c0805c2425f3aae578d82d62d00c4fe8906
Thog <me@thog.eu>
2020-04-15 15:44:49 +0200

    Fix mm implementation, nvdec works again

---
+++ Ryujinx.HLE/HOS/Services/Mm/Types/MultiMediaSession.cs
@@ -0,0 +1,24 @@
+﻿namespace Ryujinx.HLE.HOS.Services.Mm.Types
+{
+    class MultiMediaSession
+    {
+        public MultiMediaOperationType Type { get; private set; }
+
+        public bool IsAutoClearEvent { get; private set; }
+        public uint Id               { get; private set; }
+        public uint CurrentValue     { get; private set; }
+
+        public MultiMediaSession(uint id, MultiMediaOperationType type, bool isAutoClearEvent)
+        {
+            Type             = type;
+            Id               = id;
+            IsAutoClearEvent = isAutoClearEvent;
+            CurrentValue     = 0;
+        }
+
+        public void SetAndWait(uint value, int timeout)
+        {
+            CurrentValue = value;
+        }
+    }
+}
