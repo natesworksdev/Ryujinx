1398b1c43c225ea0e3480c7500a8bad7cc95b9ba
FICTURE7 <ficture7@gmail.com>
2020-04-19 10:26:27 +0400

    Use Conditional("M_DEBUG") instead
    Address gdkchan's feedback

--- ARMeilleure/Diagnostics/Symbols.cs
+++ ARMeilleure/Diagnostics/Symbols.cs
@@ -1,14 +1,11 @@
-﻿#if M_DEBUG
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
-#endif
+using System.Diagnostics;
 
 namespace ARMeilleure.Diagnostics
 {
     static class Symbols
     {
-#if M_DEBUG
-
         private static readonly ConcurrentDictionary<ulong, string> _symbols;
         private static readonly List<(ulong Start, ulong End, ulong ElementSize, string Name)> _rangedSymbols;
 
@@ -17,11 +14,9 @@ namespace ARMeilleure.Diagnostics
             _symbols = new ConcurrentDictionary<ulong, string>();
             _rangedSymbols = new List<(ulong Start, ulong End, ulong ElementSize, string Name)>();
         }
-#endif
 
         public static string Get(ulong address)
         {
-#if M_DEBUG
             if (_symbols.TryGetValue(address, out string result))
             {
                 return result;
@@ -37,26 +32,23 @@ namespace ARMeilleure.Diagnostics
                     }
                 }
             }
-#endif
 
             return null;
         }
 
+        [Conditional("M_DEBUG")]
         public static void Add(ulong address, string name)
         {
-#if M_DEBUG
             _symbols.TryAdd(address, name);
-#endif
         }
 
+        [Conditional("M_DEBUG")]
         public static void Add(ulong address, ulong size, ulong elemSize, string name)
         {
-#if M_DEBUG
             lock (_rangedSymbols)
             {
                 _rangedSymbols.Add((address, address + size, elemSize, name));
             }
-#endif
         }
     }
 }
