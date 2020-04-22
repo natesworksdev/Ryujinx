b7adade670acd8e0034c230526847cf7073a5bb1
mageven <62494521+mageven@users.noreply.github.com>
2020-04-22 11:47:47 +0530

    Add IPSwitch patcher support

--- Ryujinx.HLE/Loaders/ExePatchers/PatchUtils.cs
+++ Ryujinx.HLE/Loaders/ExePatchers/PatchUtils.cs
@@ -28,24 +28,43 @@ namespace Ryujinx.HLE.Loaders.ExePatchers
                 switch (Path.GetExtension(file))
                 {
                     case ".ips":
-                        string filename = Path.GetFileNameWithoutExtension(file).Split('.')[0];
-                        string buildId = filename.TrimEnd('0');
-
-                        int index = GetIndex(buildId);
-                        if (index == -1)
                         {
-                            continue;
-                        }
+                            string filename = Path.GetFileNameWithoutExtension(file).Split('.')[0];
+                            string buildId = filename.TrimEnd('0');
 
-                        Logger.PrintInfo(LogClass.Loader, $"Found matching IpsPatch for bid={buildId} - {file}");
+                            int index = GetIndex(buildId);
+                            if (index == -1)
+                            {
+                                continue;
+                            }
 
-                        using (var fs = File.OpenRead(file))
-                        using (var reader = new BinaryReader(fs))
-                        {
+                            Logger.PrintInfo(LogClass.Loader, $"Found matching IPS patch for bid={buildId} - {file}");
+
+                            using var fs = File.OpenRead(file);
+                            using var reader = new BinaryReader(fs);
+
                             var patcher = new IpsPatcher(reader);
                             patcher.AddPatches(patches[index]);
-                        }
 
+                        }
+                        break;
+
+                    case ".pchtxt":
+                        using (var fs = File.OpenRead(file))
+                        using (var reader = new StreamReader(fs))
+                        {
+                            var patcher = new IPSwitchPatcher(reader);
+
+                            int index = GetIndex(patcher.BuildId);
+                            if (index == -1)
+                            {
+                                continue;
+                            }
+
+                            Logger.PrintInfo(LogClass.Loader, $"Found matching IPSwitch patch for bid={patcher.BuildId} - {file}");
+
+                            patcher.AddPatches(patches[index]);
+                        }
                         break;
                 }
             }
