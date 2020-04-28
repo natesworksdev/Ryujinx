0d88cfce481356193f7c80ffebec83af8fe4837f
Thog <me@thog.eu>
2020-04-28 02:28:24 +0200

    Fix hbl 2.3.1 and hbmenu 3.3.0

--- Ryujinx.HLE/HOS/Services/Ptm/Ts/IMeasurementServer.cs
+++ Ryujinx.HLE/HOS/Services/Ptm/Ts/IMeasurementServer.cs
@@ -1,8 +1,24 @@
-﻿namespace Ryujinx.HLE.HOS.Services.Ptm.Ts
+﻿using Ryujinx.Common.Logging;
+using Ryujinx.HLE.HOS.Services.Ptm.Ts.Types;
+
+namespace Ryujinx.HLE.HOS.Services.Ptm.Ts
 {
     [Service("ts")]
     class IMeasurementServer : IpcService
     {
         public IMeasurementServer(ServiceCtx context) { }
+
+        [Command(3)]
+        // GetTemperatureMilliC(Location location) -> u32
+        public ResultCode GetTemperatureMilliC(ServiceCtx context)
+        {
+            Location location = (Location)context.RequestData.ReadByte();
+
+            Logger.PrintStub(LogClass.ServicePsm, new { location });
+
+            context.ResponseData.Write(42000u);
+
+            return ResultCode.Success;
+        }
     }
 }
