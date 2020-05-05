92c4f39973be024b6bfcc62656ae7414bbbe6e5b
Ac_K <Acoustik666@gmail.com>
2020-05-06 00:13:18 +0200

    aoc: Implement GetAddOnContentListChangedEvent
    Implement GetAddOnContentListChangedEvent according to RE.
    
    Fix #636

--- Ryujinx.HLE/HOS/Services/Ns/IAddOnContentManager.cs
+++ Ryujinx.HLE/HOS/Services/Ns/IAddOnContentManager.cs
@@ -1,11 +1,21 @@
 using Ryujinx.Common.Logging;
+using Ryujinx.HLE.HOS.Ipc;
+using Ryujinx.HLE.HOS.Kernel.Common;
+using Ryujinx.HLE.HOS.Kernel.Threading;
+using System;
 
 namespace Ryujinx.HLE.HOS.Services.Ns
 {
     [Service("aoc:u")]
     class IAddOnContentManager : IpcService
     {
-        public IAddOnContentManager(ServiceCtx context) { }
+        private KEvent _listChangedEvent;
+
+        public IAddOnContentManager(ServiceCtx context)
+        {
+            // This is signaled when the AddOnContent list has changed to reloaded it while the game is running.
+            _listChangedEvent = new KEvent(context.Device.System.KernelContext);
+        }
 
         [Command(2)]
         // CountAddOnContent(u64, pid) -> u32
@@ -27,6 +37,20 @@ namespace Ryujinx.HLE.HOS.Services.Ns
             // TODO: This is supposed to write a u32 array aswell.
             // It's unknown what it contains.
             context.ResponseData.Write(0);
+
+            return ResultCode.Success;
+        }
+
+        [Command(8)] // 4.0.0+
+        // GetAddOnContentListChangedEvent() -> handle<copy>
+        public ResultCode GetAddOnContentListChangedEvent(ServiceCtx context)
+        {
+            if (context.Process.HandleTable.GenerateHandle(_listChangedEvent.ReadableEvent, out int handle) != KernelResult.Success)
+            {
+                throw new InvalidOperationException("Out of handles!");
+            }
+
+            context.Response.HandleDesc = IpcHandleDesc.MakeCopy(handle);
 
             return ResultCode.Success;
         }
