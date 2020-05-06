97db905610f5289b9145c899dcea63d06d282690
Ac_K <Acoustik666@gmail.com>
2020-05-06 03:13:55 +0200

    pctl: refactoring IParentalControlServiceFactory and IParentalControlService call
    Our previous implementation was totally guessed. Now it's implemented according to RE, even if it's stubbed because we will not support Parental Control for now.

---
+++ Ryujinx.HLE/HOS/Services/Pctl/ResultCode.cs
@@ -0,0 +1,14 @@
+﻿namespace Ryujinx.HLE.HOS.Services.Pctl
+{
+    enum ResultCode
+    {
+        ModuleId       = 142,
+        ErrorCodeShift = 9,
+
+        Success = 0,
+
+        FreeCommunicationDisabled = (101 << ErrorCodeShift) | ModuleId,
+        InvalidPid                = (131 << ErrorCodeShift) | ModuleId,
+        InvalidUnknownFlag        = (133 << ErrorCodeShift) | ModuleId
+    }
+}
