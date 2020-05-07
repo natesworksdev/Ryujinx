b9e9c7ffd94fad3bd4e149aae4c33044b51b71f9
gdkchan <gab.dark.100@gmail.com>
2020-05-07 01:46:06 -0300

    Be explicit about BufferHandle size

--- Ryujinx.Graphics.GAL/BufferHandle.cs
+++ Ryujinx.Graphics.GAL/BufferHandle.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
 
 namespace Ryujinx.Graphics.GAL
 {
+    [StructLayout(LayoutKind.Sequential, Size = 8)]
     public struct BufferHandle : IEquatable<BufferHandle>
     {
         private readonly ulong _value;
