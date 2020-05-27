14e28f5ef2b06a1bc4273373d121a1ddecff2384
shrek5intheatres2019 <liam@tannock.net>
2020-05-27 09:53:30 -0700

    Stubbed ImportServerPki

--- Ryujinx.HLE/HOS/Services/Ssl/SslService/ISslContext.cs
+++ Ryujinx.HLE/HOS/Services/Ssl/SslService/ISslContext.cs
@@ -1,7 +1,25 @@
+using Ryujinx.Common.Logging;
+using System;
+
 namespace Ryujinx.HLE.HOS.Services.Ssl.SslService
 {
     class ISslContext : IpcService
     {
-        public ISslContext() { }
+        public ISslContext(ServiceCtx context ) { }
+        
+        [Command(4)]
+        public ResultCode ImportServerPki(ServiceCtx context)
+        {
+            var CertificateFormat = context.RequestData.ReadInt32();
+            var orOther = context.RequestData.ReadBytes(5);
+            UInt64 nice = 1;
+
+            context.ResponseData.Write(nice);
+
+            Logger.PrintStub(LogClass.ServiceSsl, new { CertificateFormat, orOther });
+
+            return ResultCode.Success;
+        }
+
     }
 }
