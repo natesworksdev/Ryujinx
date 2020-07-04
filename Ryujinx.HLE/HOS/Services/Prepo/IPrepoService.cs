fad7344ef9f2919a8de477f7018603e8b2f71bf7
Thog <me@thog.eu>
2020-07-04 22:02:17 +0200

    Address jD's comment

--- Ryujinx.HLE/HOS/Services/Prepo/IPrepoService.cs
+++ Ryujinx.HLE/HOS/Services/Prepo/IPrepoService.cs
@@ -17,37 +17,7 @@ namespace Ryujinx.HLE.HOS.Services.Prepo
         public IPrepoService(ServiceCtx context) { }
 
         [Command(10100)] // 1.0.0-5.1.0
-        // SaveReport(u64, pid, buffer<u8, 9>, buffer<bytes, 5>)
-        public ResultCode SaveReportOld(ServiceCtx context)
-        {
-            // We don't care about the differences since we don't use the play report.
-            return ProcessReport(context, withUserID: false);
-        }
-
-        [Command(10101)] // 1.0.0-5.1.0
-        // SaveReportWithUserOld(nn::account::Uid, u64, pid, buffer<u8, 9>, buffer<bytes, 5>)
-        public ResultCode SaveReportWithUserOld(ServiceCtx context)
-        {
-            // We don't care about the differences since we don't use the play report.
-            return ProcessReport(context, withUserID: true);
-        }
-
         [Command(10102)] // 6.0.0-9.2.0
-        // SaveReportOld2(u64, pid, buffer<u8, 9>, buffer<bytes, 5>)
-        public ResultCode SaveReportOld2(ServiceCtx context)
-        {
-            // We don't care about the differences since we don't use the play report.
-            return ProcessReport(context, withUserID: false);
-        }
-
-        [Command(10103)] // 6.0.0-9.2.0
-        // SaveReportWithUserOld2(nn::account::Uid, u64, pid, buffer<u8, 9>, buffer<bytes, 5>)
-        public ResultCode SaveReportWithUserOld2(ServiceCtx context)
-        {
-            // We don't care about the differences since we don't use the play report.
-            return ProcessReport(context, withUserID: true);
-        }
-
         [Command(10104)] // 10.0.0+
         // SaveReport(u64, pid, buffer<u8, 9>, buffer<bytes, 5>)
         public ResultCode SaveReport(ServiceCtx context)
@@ -56,6 +26,8 @@ namespace Ryujinx.HLE.HOS.Services.Prepo
             return ProcessReport(context, withUserID: false);
         }
 
+        [Command(10101)] // 1.0.0-5.1.0
+        [Command(10103)] // 6.0.0-9.2.0
         [Command(10105)] // 10.0.0+
         // SaveReportWithUser(nn::account::Uid, u64, pid, buffer<u8, 9>, buffer<bytes, 5>)
         public ResultCode SaveReportWithUser(ServiceCtx context)
