ea3ca40e31a7eb64e67e6cf0e41be6ab2359c510
riperiperi <rhy3756547@hotmail.com>
2020-07-17 16:59:37 +0100

    Oops

--- Ryujinx.Graphics.OpenGL/Renderer.cs
+++ Ryujinx.Graphics.OpenGL/Renderer.cs
@@ -23,7 +23,7 @@ namespace Ryujinx.Graphics.OpenGL
 
         internal TextureCopy TextureCopy { get; }
 
-        internal DisposedResourceCache ResourceCache { get; }
+        internal ResourceCache ResourceCache { get; }
 
         public string GpuVendor { get; private set; }
         public string GpuRenderer { get; private set; }
@@ -35,7 +35,7 @@ namespace Ryujinx.Graphics.OpenGL
             _counters = new Counters();
             _window = new Window(this);
             TextureCopy = new TextureCopy(this);
-            ResourceCache = new DisposedResourceCache();
+            ResourceCache = new ResourceCache();
         }
 
         public IShader CompileShader(ShaderProgram shader)
