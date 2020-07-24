69c0ebac63112379384a6754158ade569deab02a
Ac_K <Acoustik666@gmail.com>
2020-07-24 04:17:43 +0200

    ns/nim: Stub eShop related calls
    As we aren't able to process purchase on the eShop throught the emulator, I have:
    - Stub IPurchaseEventManager::SetDefaultDeliveryTarget (with RE check).
    - Implement IPurchaseEventManager::GetPurchasedEventReadableHandle (with RE check).
    
    As we can't do any eShop async call throught the emulator, I have:
    - Stub IShopServiceAccessServerInterface::CreateServerInterface
    - Stub IShopServiceAccessServer::CreateAccessorInterface
    - Stub IShopServiceAccessor::IShopServiceAsync
    
    Close #1084 and #1322

---
+++ Ryujinx.HLE/HOS/Services/Nim/IShopServiceAccessor.cs
@@ -0,0 +1,21 @@
+﻿using Ryujinx.Common.Logging;
+using Ryujinx.HLE.HOS.Services.Nim.ShopServiceAccessServerInterface.ShopServiceAccessServer.ShopServiceAccessor;
+
+namespace Ryujinx.HLE.HOS.Services.Nim.ShopServiceAccessServerInterface.ShopServiceAccessServer
+{
+    class IShopServiceAccessor : IpcService
+    {
+        public IShopServiceAccessor() { }
+
+        [Command(0)]
+        // CreateAsyncInterface(u64) -> (handle<copy>, object<nn::ec::IShopServiceAsync>)
+        public ResultCode CreateAsyncInterface(ServiceCtx context)
+        {
+            MakeObject(context, new IShopServiceAsync());
+
+            Logger.PrintStub(LogClass.ServiceNim);
+
+            return ResultCode.Success;
+        }
+    }
+}
