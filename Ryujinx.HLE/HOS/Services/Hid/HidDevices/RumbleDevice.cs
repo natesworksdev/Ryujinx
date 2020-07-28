d343c3c6c0aae33d4499499547a0c4749d7541d5
anirudhb <anirudhb@users.noreply.github.com>
2020-07-27 21:31:47 -0700

    XInput check for unsafe rumble

--- Ryujinx.HLE/HOS/Services/Hid/HidDevices/RumbleDevice.cs
+++ Ryujinx.HLE/HOS/Services/Hid/HidDevices/RumbleDevice.cs
@@ -25,12 +25,22 @@ namespace Ryujinx.HLE.HOS.Services.Hid
                 float amplitude = Math.Max(value.AmplitudeLow, value.AmplitudeHigh);
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    // Unsafe access to internal XInput joystick for proper vibration
-                    Type XInputJoystick = Type.GetType("OpenTK.Platform.Windows.XInputJoystick, OpenTK, Version=1.0.5.12, Culture=neutral, PublicKeyToken=bad199fe84eb3df4");
-                    Trace.Assert(XInputJoystick != null);
-                    object joystick = XInputJoystick.GetConstructor(Type.EmptyTypes).Invoke(null);
-                    Trace.Assert(joystick != null);
-                    XInputJoystick.GetMethod("SetVibration").Invoke(joystick, new object[] { _gamepadIndex, amplitude, amplitude });
+                    if (GamePad.GetName(_gamepadIndex).Contains("xinput", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Unsafe access to internal XInput joystick for proper vibration
+                        Type XInputJoystick = typeof(OpenTK.BezierCurve).Assembly.GetType("OpenTK.Platform.Windows.XInputJoystick");
+                        //Type XInputJoystick = Type.GetType("OpenTK.Platform.Windows.XInputJoystick, OpenTK, Version=1.0.5.12, Culture=neutral, PublicKeyToken=bad199fe84eb3df4");
+                        Trace.Assert(XInputJoystick != null);
+                        object joystick = XInputJoystick.GetConstructor(Type.EmptyTypes).Invoke(null);
+                        Trace.Assert(joystick != null);
+                        XInputJoystick.GetMethod("SetVibration").Invoke(joystick, new object[] {
+                            _gamepadIndex, value.AmplitudeLow, value.AmplitudeHigh
+                        });
+                    } else
+                    {
+                        // DInput is not supported
+                        GamePad.SetVibration(_gamepadIndex, amplitude, amplitude);
+                    }
                 } else
                 {
                     // No-op
