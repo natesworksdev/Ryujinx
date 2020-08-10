4ce1c620cfa146565613e0925fec9603d078de68
Ac_K <Acoustik666@gmail.com>
2020-08-10 04:31:50 +0200

    account: Implement IManagerForApplication calls and IAsyncContext
    This implement:
    - IManagerForApplication::EnsureIdTokenCacheAsync (accordingly to RE) but the Async task is stubbed.
    - IAsyncContext interface (accordingly to RE).
    - IManagerForApplication::LoadIdTokenCache (checked with RE, and stubbed).
    
    I've tried some games but now they needs some `sfdnsres` calls, some other boots and crashes with other issues.
    Maybe we should disable the connection somewhere to lets the game think we are offline. I have done many attempts, without success, but since the code is here now, it's better than nothing.
    
    (I've cleaned up `using` of IGeneralService too)
    
    Closes #629 and closes #630

--- Ryujinx.HLE/HOS/Services/Account/Acc/IManagerForApplication.cs
+++ Ryujinx.HLE/HOS/Services/Account/Acc/IManagerForApplication.cs
@@ -1,5 +1,12 @@
 using Ryujinx.Common.Logging;
+using Ryujinx.Cpu;
+using Ryujinx.HLE.HOS.Kernel.Threading;
+using Ryujinx.HLE.HOS.Services.Account.Acc.AsyncContext;
 using Ryujinx.HLE.HOS.Services.Arp;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Ryujinx.HLE.HOS.Services.Account.Acc
 {
@@ -7,21 +14,27 @@ namespace Ryujinx.HLE.HOS.Services.Account.Acc
     {
         private UserId                    _userId;
         private ApplicationLaunchProperty _applicationLaunchProperty;
-
-        private const long NetworkServiceAccountId = 0xcafe;
+        private long                      _networkServiceAccountId;
 
         public IManagerForApplication(UserId userId, ApplicationLaunchProperty applicationLaunchProperty)
         {
             _userId                    = userId;
             _applicationLaunchProperty = applicationLaunchProperty;
+
+            // TODO: Determine where and how NetworkServiceAccountId is set.
+            _networkServiceAccountId = new Random().Next();
         }
 
         [Command(0)]
         // CheckAvailability()
         public ResultCode CheckAvailability(ServiceCtx context)
         {
+            // NOTE: This open the file at "su/baas/USERID_IN_UUID_STRING.dat" where USERID_IN_UUID_STRING is formatted as "%08x-%04x-%04x-%02x%02x-%08x%04x".
+            //       Then its search the Availability of Online Service related to the UserId in this file and return it.
+
             Logger.Stub?.PrintStub(LogClass.ServiceAcc);
 
+            // NOTE: Even if we try to return different error codes here, the guest still needs other calls.
             return ResultCode.Success;
         }
 
@@ -29,9 +42,78 @@ namespace Ryujinx.HLE.HOS.Services.Account.Acc
         // GetAccountId() -> nn::account::NetworkServiceAccountId
         public ResultCode GetAccountId(ServiceCtx context)
         {
-            Logger.Stub?.PrintStub(LogClass.ServiceAcc, new { NetworkServiceAccountId });
+            // NOTE: This open the file at "su/baas/USERID_IN_UUID_STRING.dat" (where USERID_IN_UUID_STRING is formatted 
+            //       as "%08x-%04x-%04x-%02x%02x-%08x%04x") in the account:/ savedata.
+            //       Then its search the NetworkServiceAccountId related to the UserId in this file and return it.
 
-            context.ResponseData.Write(NetworkServiceAccountId);
+            Logger.Stub?.PrintStub(LogClass.ServiceAcc, new { _networkServiceAccountId });
+
+            context.ResponseData.Write(_networkServiceAccountId);
+
+            return ResultCode.Success;
+        }
+
+        [Command(2)]
+        // EnsureIdTokenCacheAsync() -> object<nn::account::detail::IAsyncContext>
+        public ResultCode EnsureIdTokenCacheAsync(ServiceCtx context)
+        {
+            // if (!(this + 0x48)) return ResultCode.Unknown1;
+
+            KEvent         asyncEvent     = new KEvent(context.Device.System.KernelContext);
+            AsyncExecution asyncExecution = new AsyncExecution(asyncEvent);
+
+            asyncExecution.Initialize(1000, EnsureIdTokenCacheAsyncImpl);
+
+            MakeObject(context, new IAsyncContext(asyncExecution));
+
+            // Doesn't occur in our case.
+            // if (IAsyncContext == null) return ResultCode.NullObject;
+
+            return ResultCode.Success;
+        }
+
+        private async Task EnsureIdTokenCacheAsyncImpl(CancellationToken token)
+        {
+            // NOTE: This open the file at "su/baas/USERID_IN_UUID_STRING.dat" (where USERID_IN_UUID_STRING is formatted as "%08x-%04x-%04x-%02x%02x-%08x%04x")
+            //       in the "account:/" savedata.
+            //       Then its read data, use dauth API with this data to get the Token Id and probably store the dauth response
+            //       in "su/cache/USERID_IN_UUID_STRING.dat" (where USERID_IN_UUID_STRING is formatted as "%08x-%04x-%04x-%02x%02x-%08x%04x") in the "account:/" savedata.
+            //       Since we don't support online services, we can stub it.
+
+            Logger.Stub?.PrintStub(LogClass.ServiceAcc);
+
+            // TODO: Use a real function instead of this little async delay.
+            await Task.Delay(10, token);
+        }
+
+        [Command(3)]
+        // LoadIdTokenCache() -> (u32 id_token_cache_size, buffer<bytes, 6>)
+        public ResultCode LoadIdTokenCache(ServiceCtx context)
+        {
+            long bufferPosition = context.Request.ReceiveBuff[0].Position;
+            long bufferSize     = context.Request.ReceiveBuff[0].Size;
+
+            // NOTE: This open the file at "su/cache/USERID_IN_UUID_STRING.dat" (where USERID_IN_UUID_STRING is formatted as "%08x-%04x-%04x-%02x%02x-%08x%04x")
+            //       in the "account:/" savedata and write some data in the buffer.
+            //       Since we don't support online services, we can stub it.
+
+            Logger.Stub?.PrintStub(LogClass.ServiceAcc);
+
+            /*
+            if (internal_object != null)
+            {
+                if (bufferSize > 0xC00)
+                {
+                    return ResultCode.InvalidIdTokenCacheBufferSize;
+                }
+            }
+            */
+
+            int idTokenCacheSize = 0;
+
+            MemoryHelper.FillWithZeros(context.Memory, bufferPosition, (int)bufferSize);
+
+            context.ResponseData.Write(idTokenCacheSize);
 
             return ResultCode.Success;
         }
@@ -40,9 +122,9 @@ namespace Ryujinx.HLE.HOS.Services.Account.Acc
         // GetNintendoAccountUserResourceCacheForApplication() -> (nn::account::NintendoAccountId, buffer<nn::account::nas::NasUserBaseForApplication, 0x1a>, buffer<bytes, 6>)
         public ResultCode GetNintendoAccountUserResourceCacheForApplication(ServiceCtx context)
         {
-            Logger.Stub?.PrintStub(LogClass.ServiceAcc, new { NetworkServiceAccountId });
+            Logger.Stub?.PrintStub(LogClass.ServiceAcc, new { _networkServiceAccountId });
 
-            context.ResponseData.Write(NetworkServiceAccountId);
+            context.ResponseData.Write(_networkServiceAccountId);
 
             // TODO: determine and fill the two output IPC buffers.
 
