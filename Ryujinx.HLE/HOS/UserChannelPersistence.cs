natesworksdev/Ryujinx

commit 33f8284bc0f80b18bdce6f8e9a363f5b70388788

hle/ui: Basic multi programs support (#1560)
* hos/gui: Add a check of NCA program index in titleid

This add a check to `ApplicationLoader` for the last 2 digits of the game TitleId who seems to be the NCA program index.
We currently return the last index, instead of the lower one.
Same check is added to ApplicationLibrary in the UI.

I've cleaned up both file too.

* hle: implement partial relaunch logic

TODO: make the emulator auto relauch.

* Handle auto relaunch

* hle: Unify update usage system

* hle: Implement support of multi programs in update system

* Add some documentation

* Address rip's comment

Co-authored-by: Ac_K <Acoustik666@gmail.com>

Ryujinx.HLE/HOS/UserChannelPersistence.cs
@@ -1,0 +1,56 @@
+﻿using Ryujinx.HLE.HOS.Services.Am.AppletOE.ApplicationProxyService.ApplicationProxy.Types;
+using System;
+using System.Collections.Generic;
+
+namespace Ryujinx.HLE.HOS
+{
+    public class UserChannelPersistence
+    {
+        private Stack<byte[]> _userChannelStorages;
+        public int PreviousIndex { get; private set; }
+        public int Index { get; private set; }
+        public ProgramSpecifyKind Kind { get; private set; }
+
+        public UserChannelPersistence()
+        {
+            _userChannelStorages = new Stack<byte[]>();
+            Kind = ProgramSpecifyKind.ExecuteProgram;
+            PreviousIndex = -1;
+            Index = 0;
+        }
+
+        public void Clear()
+        {
+            _userChannelStorages.Clear();
+        }
+
+        public void Push(byte[] data)
+        {
+            _userChannelStorages.Push(data);
+        }
+
+        public byte[] Pop()
+        {
+            return _userChannelStorages.Pop();
+        }
+
+        public bool IsEmpty => _userChannelStorages.Count == 0;
+
+        public void ExecuteProgram(ProgramSpecifyKind kind, ulong value)
+        {
+            Kind = kind;
+            PreviousIndex = Index;
+
+            switch (kind)
+            {
+                case ProgramSpecifyKind.ExecuteProgram:
+                    Index = (int)value;
+                    break;
+                case ProgramSpecifyKind.RestartProgram:
+                    break;
+                default:
+                    throw new NotImplementedException($"{kind} not implemented");
+            }
+        }
+    }
+}

Ryujinx/Ui/MainWindow.cs
@@ -11,6 +11,7 @@ using Ryujinx.Graphics.GAL;
 using Ryujinx.Graphics.OpenGL;
 using Ryujinx.HLE.FileSystem;
 using Ryujinx.HLE.FileSystem.Content;
+using Ryujinx.HLE.HOS;
 using Ryujinx.Ui.Diagnostic;
 using System;
 using System.Diagnostics;
@@ -27,6 +28,7 @@ namespace Ryujinx.Ui
     {
         private static VirtualFileSystem _virtualFileSystem;
         private static ContentManager    _contentManager;
+        private static UserChannelPersistence _userChannelPersistence;
 
         private static WindowsMultimediaTimerResolution _windowsMultimediaTimerResolution;
         private static HLE.Switch _emulationContext;
@@ -34,12 +36,15 @@ namespace Ryujinx.Ui
         private static GlRenderer _glWidget;
         private static GtkHostUiHandler _uiHandler;
 
+        public static GlRenderer GlWidget => _glWidget;
+
         private static AutoResetEvent _deviceExitStatus = new AutoResetEvent(false);
 
         private static ListStore _tableStore;
 
         private static bool _updatingGameTable;
         private static bool _gameLoaded;
+        private static string _gamePath;
         private static bool _ending;
 
 #pragma warning disable CS0169, CS0649, IDE0044
@@ -110,6 +115,7 @@ namespace Ryujinx.Ui
             }
 
             _virtualFileSystem = VirtualFileSystem.CreateInstance();
+            _userChannelPersistence = new UserChannelPersistence();
             _contentManager    = new ContentManager(_virtualFileSystem);
 
             if (migrationNeeded)
@@ -181,6 +187,7 @@ namespace Ryujinx.Ui
             _statusBar.Hide();
 
             _uiHandler = new GtkHostUiHandler(this);
+            _gamePath = null;
         }
 
         private void MainWindow_WindowStateEvent(object o, WindowStateEventArgs args)
@@ -278,7 +285,7 @@ namespace Ryujinx.Ui
         {
             _virtualFileSystem.Reload();
 
-            HLE.Switch instance = new HLE.Switch(_virtualFileSystem, _contentManager, InitializeRenderer(), InitializeAudioEngine())
+            HLE.Switch instance = new HLE.Switch(_virtualFileSystem, _contentManager, _userChannelPersistence, InitializeRenderer(), InitializeAudioEngine())
             {
                 UiHandler = _uiHandler
             };
@@ -485,6 +492,7 @@ namespace Ryujinx.Ui
                 }
 
                 _emulationContext = device;
+                _gamePath = path;
 
                 _deviceExitStatus.Reset();
 
@@ -589,6 +597,7 @@ namespace Ryujinx.Ui
                 UpdateGameTable();
 
                 Task.Run(RefreshFirmwareLabel);
+                Task.Run(HandleRelaunch);
 
                 _stopEmulation.Sensitive            = false;
                 _firmwareInstallFile.Sensitive      = true;
@@ -960,6 +969,21 @@ namespace Ryujinx.Ui
 
                 return false;
             }));
+        }
+
+        private void HandleRelaunch()
+        {
+            // If the previous index isn't -1, that mean we are relaunching.
+            if (_userChannelPersistence.PreviousIndex != -1)
+            {
+                LoadApplication(_gamePath);
+            }
+            else
+            {
+                // otherwise, clear state.
+                _userChannelPersistence = new UserChannelPersistence();
+                _gamePath = null;
+            }
         }
 
         private void HandleInstallerDialog(FileChooserDialog fileChooser)

Ryujinx.HLE/HOS/Services/Account/Acc/IManagerForApplication.cs
@@ -126,5 +126,14 @@ namespace Ryujinx.HLE.HOS.Services.Account.Acc
 
             return ResultCode.Success;
         }
+
+        [Command(160)] // 5.0.0+
+        // StoreOpenContext()
+        public ResultCode StoreOpenContext(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAcc);
+
+            return ResultCode.Success;
+        }
     }
 }

Ryujinx.HLE/HOS/Services/Am/AppletOE/ApplicationProxyService/ApplicationProxy/IApplicationFunctions.cs
@@ -5,11 +5,13 @@ using LibHac.Fs;
 using LibHac.Ns;
 using Ryujinx.Common;
 using Ryujinx.Common.Logging;
+using Ryujinx.HLE.Exceptions;
 using Ryujinx.HLE.HOS.Ipc;
 using Ryujinx.HLE.HOS.Kernel.Common;
 using Ryujinx.HLE.HOS.Kernel.Memory;
 using Ryujinx.HLE.HOS.Kernel.Threading;
 using Ryujinx.HLE.HOS.Services.Am.AppletAE.Storage;
+using Ryujinx.HLE.HOS.Services.Am.AppletOE.ApplicationProxyService.ApplicationProxy.Types;
 using Ryujinx.HLE.HOS.Services.Sdb.Pdm.QueryService;
 using Ryujinx.HLE.HOS.SystemState;
 using System;
@@ -35,11 +37,27 @@ namespace Ryujinx.HLE.HOS.Services.Am.AppletOE.ApplicationProxyService.Applicati
         }
 
         [Command(1)]
-        // PopLaunchParameter(u32) -> object<nn::am::service::IStorage>
+        // PopLaunchParameter(LaunchParameterKind kind) -> object<nn::am::service::IStorage>
         public ResultCode PopLaunchParameter(ServiceCtx context)
         {
-            // Only the first 0x18 bytes of the Data seems to be actually used.
-            MakeObject(context, new AppletAE.IStorage(StorageHelper.MakeLaunchParams(context.Device.System.State.Account.LastOpenedUser)));
+            LaunchParameterKind kind = (LaunchParameterKind)context.RequestData.ReadUInt32();
+
+            byte[] storageData;
+
+            switch (kind)
+            {
+                case LaunchParameterKind.UserChannel:
+                    storageData = context.Device.UserChannelPersistence.Pop();
+                    break;
+                case LaunchParameterKind.PreselectedUser:
+                    // Only the first 0x18 bytes of the Data seems to be actually used.
+                    storageData = StorageHelper.MakeLaunchParams(context.Device.System.State.Account.LastOpenedUser);
+                    break;
+                default:
+                    throw new NotImplementedException($"Unknown LaunchParameterKind {kind}");
+            }
+
+            MakeObject(context, new AppletAE.IStorage(storageData));
 
             return ResultCode.Success;
         }
@@ -376,14 +394,49 @@ namespace Ryujinx.HLE.HOS.Services.Am.AppletOE.ApplicationProxyService.Applicati
             return (ResultCode)QueryPlayStatisticsManager.GetPlayStatistics(context, true);
         }
 
+        [Command(120)] // 5.0.0+
+        // ExecuteProgram(ProgramSpecifyKind kind, u64 value)
+        public ResultCode ExecuteProgram(ServiceCtx context)
+        {
+            ProgramSpecifyKind kind = (ProgramSpecifyKind)context.RequestData.ReadUInt32();
+
+            // padding
+            context.RequestData.ReadUInt32();
+
+            ulong value = context.RequestData.ReadUInt64();
+
+            Logger.Stub?.PrintStub(LogClass.ServiceAm, new { kind, value });
+
+            context.Device.UiHandler.ExecuteProgram(context.Device, kind, value);
+
+            return ResultCode.Success;
+        }
+
+        [Command(121)] // 5.0.0+
+        // ClearUserChannel()
+        public ResultCode ClearUserChannel(ServiceCtx context)
+        {
+            context.Device.UserChannelPersistence.Clear();
+
+            return ResultCode.Success;
+        }
+
+        [Command(122)] // 5.0.0+
+        // UnpopToUserChannel(object<nn::am::service::IStorage> input_storage)
+        public ResultCode UnpopToUserChannel(ServiceCtx context)
+        {
+            AppletAE.IStorage data = GetObject<AppletAE.IStorage>(context, 0);
+
+            context.Device.UserChannelPersistence.Push(data.Data);
+
+            return ResultCode.Success;
+        }
+
         [Command(123)] // 5.0.0+
         // GetPreviousProgramIndex() -> s32 program_index
         public ResultCode GetPreviousProgramIndex(ServiceCtx context)
         {
-            // TODO: The output PreviousProgramIndex is -1 when there was no previous title.
-            //       When multi-process will be supported, return the last program index.
-
-            int previousProgramIndex = -1;
+            int previousProgramIndex = context.Device.UserChannelPersistence.PreviousIndex;
 
             context.ResponseData.Write(previousProgramIndex);
 

Ryujinx.HLE/HOS/Services/Am/AppletOE/ApplicationProxyService/ApplicationProxy/Types/LaunchParameterKind.cs
@@ -1,0 +1,9 @@
+﻿namespace Ryujinx.HLE.HOS.Services.Am.AppletOE.ApplicationProxyService.ApplicationProxy.Types
+{
+    public enum LaunchParameterKind : uint
+    {
+        UserChannel = 1,
+        PreselectedUser,
+        Unknown
+    }
+}

Ryujinx.HLE/HOS/Services/Am/AppletOE/ApplicationProxyService/ApplicationProxy/Types/ProgramSpecifyKind.cs
@@ -1,0 +1,9 @@
+﻿namespace Ryujinx.HLE.HOS.Services.Am.AppletOE.ApplicationProxyService.ApplicationProxy.Types
+{
+    public enum ProgramSpecifyKind : uint
+    {
+        ExecuteProgram,
+        SubApplicationProgram,
+        RestartProgram
+    }
+}

Ryujinx.HLE/IHostUiHandler.cs
@@ -1,4 +1,5 @@
 using Ryujinx.HLE.HOS.Applets;
+using Ryujinx.HLE.HOS.Services.Am.AppletOE.ApplicationProxyService.ApplicationProxy.Types;
 
 namespace Ryujinx.HLE
 {
@@ -22,5 +23,13 @@ namespace Ryujinx.HLE
         /// </summary>
         /// <returns>True when OK is pressed, False otherwise.</returns>
         bool DisplayMessageDialog(ControllerAppletUiArgs args);
+
+        /// <summary>
+        /// Tell the UI that we need to transisition to another program.
+        /// </summary>
+        /// <param name="device">The device instance.</param>
+        /// <param name="kind">The program kind.</param>
+        /// <param name="value">The value associated to the <paramref name="kind"/>.</param>
+        void ExecuteProgram(Switch device, ProgramSpecifyKind kind, ulong value);
     }
 }

commit 7cca1d1d1441310c04332123f300ac801fc7bf41

Fix exiting emulator with multi programs
THis fix a bug introduced in #1560 that would cause "Stop emulation" to
actually restart the game all the time.

Ryujinx.HLE/HOS/UserChannelPersistence.cs
@@ -10,6 +10,7 @@ namespace Ryujinx.HLE.HOS
         public int PreviousIndex { get; private set; }
         public int Index { get; private set; }
         public ProgramSpecifyKind Kind { get; private set; }
+        public bool ShouldRestart { get; set; }
 
         public UserChannelPersistence()
         {
@@ -42,6 +43,7 @@ namespace Ryujinx.HLE.HOS
         {
             Kind = kind;
             PreviousIndex = Index;
+            ShouldRestart = true;
 
             switch (kind)
             {

Ryujinx/Ui/MainWindow.cs
@@ -975,9 +975,10 @@ namespace Ryujinx.Ui
 
         private void HandleRelaunch()
         {
-            // If the previous index isn't -1, that mean we are relaunching.
-            if (_userChannelPersistence.PreviousIndex != -1)
+            if (_userChannelPersistence.PreviousIndex != -1 && _userChannelPersistence.ShouldRestart)
             {
+                _userChannelPersistence.ShouldRestart = false;
+
                 LoadApplication(_gamePath);
             }
             else