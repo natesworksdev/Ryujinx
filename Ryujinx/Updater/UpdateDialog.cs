natesworksdev/Ryujinx

commit 920e84c1e10834f6eea610cb7a36798ca236a160

Preserve command line arguments when updater restarts Ryujinx (#1643)
Command line arguements are not preserved by the updater, and this causes an issue when the updater restarts Ryujinx in portable mode as it will create/use/modify its default directory instead.

This simple pull addresses the first part of #1594 

As far as the second part of the issue, where the updater perhaps overzealously deletes everything but log files and folders, which causes problems in portable installs with a user subfolder or something else inside the Ryujinx folder. 

Perhaps an UpdateList.txt containing a list of files that the updater deletes is included in the ryujinx.zip/tar.gz/etc and placed in the upacked directory for the updater to use upon the next upgrade.

The build system can run something like `forfiles /s /m *.txt /c "cmd /c echo @relpath"` or the correct command for the OS and include it in the distribution, or it could be generated by the updater itself.

Ryujinx/Updater/UpdateDialog.cs
@@ -45,6 +45,7 @@ namespace Ryujinx.Ui
             {
                 string ryuName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Ryujinx.exe" : "Ryujinx";
                 string ryuExe  = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ryuName);
+                string ryuArg = String.Join(" ", Environment.GetCommandLineArgs());
 
                 if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
@@ -52,7 +53,7 @@ namespace Ryujinx.Ui
                     unixFileInfo.FileAccessPermissions |= FileAccessPermissions.UserExecute;
                 }
 
-                Process.Start(ryuExe);
+                Process.Start(ryuExe, ryuArg);
 
                 Environment.Exit(0);
             }

commit 0e9b963d5b12df9c7fca4c9fb4fa2aca715f9fe9

updater: Ignore first commandline argument when passing commandline to updated binary
Fix a regression caused by #1643.

Ryujinx/Updater/UpdateDialog.cs
@@ -3,6 +3,7 @@ using Gtk;
 using Mono.Unix;
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace Ryujinx.Ui
@@ -45,7 +46,7 @@ namespace Ryujinx.Ui
             {
                 string ryuName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Ryujinx.exe" : "Ryujinx";
                 string ryuExe  = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ryuName);
-                string ryuArg = String.Join(" ", Environment.GetCommandLineArgs());
+                string ryuArg = String.Join(" ", Environment.GetCommandLineArgs().AsEnumerable().Skip(1).ToArray());
 
                 if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {