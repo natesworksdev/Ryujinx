af53bc0e87df3caee09fef21ffe12c405ad96080
SeraUX <xploitrisgod@gmail.com>
2020-11-08 21:13:56 +0100

    GLRenderer: Change between docked and handheld mode using a keybinding

--- Ryujinx/Ui/GLRenderer.cs
+++ Ryujinx/Ui/GLRenderer.cs
@@ -41,6 +41,7 @@ namespace Ryujinx.Ui
         private bool   _mousePressed;
 
         private bool _toggleFullscreen;
+        private bool _toggleDockedMode;
 
         private readonly long _ticksPerFrame;
 
@@ -162,6 +163,19 @@ namespace Ryujinx.Ui
             }
 
             _toggleFullscreen = toggleFullscreen;
+
+            bool toggleDockedMode = keyboard.IsKeyDown(OpenTK.Input.Key.F9);
+
+            if(toggleDockedMode != _toggleDockedMode)
+            {
+                if (toggleDockedMode)
+                {
+                    ConfigurationState.Instance.System.EnableDockedMode.Value =
+                        !ConfigurationState.Instance.System.EnableDockedMode.Value;
+                }
+            }
+
+            _toggleDockedMode = toggleDockedMode;
         }
 
         private void GLRenderer_Initialized(object sender, EventArgs e)
