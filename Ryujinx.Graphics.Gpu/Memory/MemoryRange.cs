6efc51cb989abdfe62abcbb8e5d30dcc86c222a4
Sharbel Aldabain <saldabain.dev@gmail.com>
2020-11-13 10:24:49 -0500

    Add representation of memory ranges

---
+++ Ryujinx.Graphics.Gpu/Memory/MemoryRange.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Ryujinx.Graphics.Gpu.Memory
+{
+    class MemoryRange : IEquatable<MemoryRange>, IComparable<MemoryRange>
+    {
+        public ulong startAddress { get; }
+
+        public ulong endAddress { get; }
+        public MemoryRange(ulong address, ulong endAddress)
+        {
+            this.startAddress = address;
+            this.endAddress = endAddress;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as MemoryRange);
+        }
+
+        public bool Equals(MemoryRange other)
+        {
+            return other != null &&
+                   startAddress == other.startAddress &&
+                   endAddress == other.endAddress;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(startAddress, endAddress);
+        }
+
+        public int CompareTo(MemoryRange other)
+        {
+            return this.startAddress.CompareTo(other.startAddress);
+        }
+    }
+}
