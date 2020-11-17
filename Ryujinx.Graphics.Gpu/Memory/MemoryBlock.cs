d1a5cb442d65b47a88d54cbc33b8e9398b425971
Sharbel Aldabain <saldabain.dev@gmail.com>
2020-11-16 20:10:59 -0500

    Implement TreeMap from scratch.
    Begin implementation of MemoryBlockManager

---
+++ Ryujinx.Graphics.Gpu/Memory/MemoryBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryujinx.Graphics.Gpu.Memory
+{
+    struct MemoryBlock : IComparable<MemoryBlock>
+    {
+        private ulong address { get; }
+        private ulong size { get; }
+        private ulong endAddress { get; }
+
+        public MemoryBlock(ulong address, ulong size)
+        {
+            this.address = address;
+            this.size = size;
+            this.endAddress = address + size - 1UL;
+        }
+
+        public int CompareTo(MemoryBlock other)
+        {
+            return this.address.CompareTo(other.address);
+        }
+    }
+}
---
+++ Ryujinx.Graphics.Gpu/Memory/MemoryBlockManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryujinx.Graphics.Gpu.Memory
+{
+    class MemoryBlockManager
+    {
+        private const int PtLvl0Bits = 14;
+        private const int PtLvl1Bits = 14;
+        private const int PtPageBits = 12;
+
+        private const ulong PtLvl0Size = 1UL << PtLvl0Bits;
+        private const ulong PtLvl1Size = 1UL << PtLvl1Bits;
+        private const ulong PageSize = 1UL << PtPageBits;
+
+        private const ulong PtLvl0Mask = PtLvl0Size - 1;
+        private const ulong PtLvl1Mask = PtLvl1Size - 1;
+        private const ulong PageMask = PageSize - 1;
+
+        private const int PtLvl0Bit = PtPageBits + PtLvl1Bits;
+        private const int PtLvl1Bit = PtPageBits;
+
+        private const ulong PteUnmapped = 0xffffffff_ffffffff;
+        private const ulong PteReserved = 0xffffffff_fffffffe;
+
+        private LinkedList<MemoryBlock> _list = new LinkedList<MemoryBlock>();
+        private TreeMap<ulong, MemoryBlock> _map = new TreeMap<ulong, MemoryBlock>();
+        public MemoryBlockManager(ulong maxAddressSize)
+        {
+            _list.AddFirst(new MemoryBlock(1UL, maxAddressSize));
+            _map.Put(1UL, _list.First.Value);
+        }
+
+        public ulong FindFreeAddress(ulong size, ulong alignment, ulong start)
+        {
+            return -1ul;
+        }
+
+        public void Allocate(ulong address, ulong size)
+        {
+            lock(_list) {
+                lock(_map)
+                {
+
+                }
+            }
+        }
+
+        public void Deallocate(ulong address)
+        {
+            lock (_list)
+            {
+                lock (_map)
+                {
+
+                }
+            }
+        }
+    }
+}
