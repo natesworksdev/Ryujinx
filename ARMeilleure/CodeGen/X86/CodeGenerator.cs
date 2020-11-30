62201a759352e7f597ad7e2693e006e1d150d189
Sharbel Aldabain <saldabain.dev@gmail.com>
2020-11-30 13:35:37 -0500

    Fix indentation

--- ARMeilleure/CodeGen/X86/CodeGenerator.cs
+++ ARMeilleure/CodeGen/X86/CodeGenerator.cs
@@ -435,19 +435,19 @@ namespace ARMeilleure.CodeGen.X86
 
                     case IntrinsicType.Fma:
                     {
-                            Operand dest = operation.Destination;
-                            Operand src1 = operation.GetSource(0);
-                            Operand src2 = operation.GetSource(1);
-                            Operand src3 = operation.GetSource(2);
+                        Operand dest = operation.Destination;
+                        Operand src1 = operation.GetSource(0);
+                        Operand src2 = operation.GetSource(1);
+                        Operand src3 = operation.GetSource(2);
 
-                            EnsureSameType(dest, src1, src2, src3);
-                            EnsureSameReg(dest, src1);
-                            Debug.Assert(!dest.Type.IsInteger());
-                            Debug.Assert(HardwareCapabilities.SupportsVexEncoding);
+                        EnsureSameType(dest, src1, src2, src3);
+                        EnsureSameReg(dest, src1);
+                        Debug.Assert(!dest.Type.IsInteger());
+                        Debug.Assert(HardwareCapabilities.SupportsVexEncoding);
 
-                            context.Assembler.WriteInstruction(info.Inst, dest, src2, src3);
+                        context.Assembler.WriteInstruction(info.Inst, dest, src2, src3);
 
-                            break;
+                        break;
                     }
                 }
             }
