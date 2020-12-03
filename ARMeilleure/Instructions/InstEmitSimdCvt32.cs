857f7e94dccff78478e8f34433a9c09214336e45
Sharbel Aldabain <saldabain.dev@gmail.com>
2020-12-02 23:09:35 -0500

    Clean up code.

--- ARMeilleure/Instructions/InstEmitSimdCvt32.cs
+++ ARMeilleure/Instructions/InstEmitSimdCvt32.cs
@@ -326,7 +326,6 @@ namespace ARMeilleure.Instructions
         // VRINTX (floating-point)
         public static void Vrintx_S(ArmEmitterContext context)
         {
-            OpCode32SimdS op = (OpCode32SimdS)context.CurrOp;
             if(Optimizations.UseSse2)
             {
                 EmitScalarUnaryOpSimd32(context, (m) => 
@@ -339,24 +338,6 @@ namespace ARMeilleure.Instructions
                 EmitScalarUnaryOpF32(context, (op1) => EmitUnaryMathCall(context, nameof(Math.Round), op1));
             }
         }
-
-        //// VRINTX (advanced-simd)
-        //public static void Vrintx_V(ArmEmitterContext context)
-        //{
-        //    OpCode32SimdS op = (OpCode32SimdS)context.CurrOp;
-        //    if (Optimizations.UseSse2)
-        //    {
-        //        EmitScalarUnaryOpSimd32(context, (m) =>
-        //        {
-        //            Intrinsic inst = (op.Size & 1) == 0 ? Intrinsic.X86Roundps : Intrinsic.X86Roundpd;
-        //            return context.AddIntrinsic(inst, m, Const(X86GetRoundControl(FPRoundingMode.ToNearest)));
-        //        });
-        //    }
-        //    else
-        //    {
-        //        EmitVectorUnaryOpF32(context, (op1) => EmitUnaryMathCall(context, nameof(Math.Round), op1));
-        //    }
-        //}
 
         // VRINTZ (floating-point).
         public static void Vrint_Z(ArmEmitterContext context)
