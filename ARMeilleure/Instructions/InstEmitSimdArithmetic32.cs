f102be4d7158e86378a0982157a25f38ba851031
Sharbel Aldabain <saldabain.dev@gmail.com>
2020-12-07 16:29:20 -0500

    Add comments / re-oreder opcode table.

--- ARMeilleure/Instructions/InstEmitSimdArithmetic32.cs
+++ ARMeilleure/Instructions/InstEmitSimdArithmetic32.cs
@@ -256,6 +256,8 @@ namespace ARMeilleure.Instructions
         {
             if (Optimizations.FastFP && Optimizations.UseFma)
             {
+                // Vectors contain elements that are 32-bits in length always. The only thing that will change is the number of elements in a vector. 
+                // PS for both intrinsics is intentional.
                 EmitVectorTernaryOpF32(context, Intrinsic.X86Vfmadd231ps, Intrinsic.X86Vfmadd231ps);
             }
             else
