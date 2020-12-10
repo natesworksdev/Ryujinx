8214eac625c202e37947e0f1430af9851d4ee5a2
gdkchan <gab.dark.100@gmail.com>
2020-12-10 13:00:16 -0300

    Update PPTC version

--- ARMeilleure/Translation/PTC/Ptc.cs
+++ ARMeilleure/Translation/PTC/Ptc.cs
@@ -27,7 +27,7 @@ namespace ARMeilleure.Translation.PTC
         private const string OuterHeaderMagicString = "PTCohd\0\0";
         private const string InnerHeaderMagicString = "PTCihd\0\0";
 
-        private const int InternalVersion = 123; //! To be incremented manually for each change to the ARMeilleure project.
+        private const int InternalVersion = 1798; //! To be incremented manually for each change to the ARMeilleure project.
 
         private const string ActualDir = "0";
         private const string BackupDir = "1";
