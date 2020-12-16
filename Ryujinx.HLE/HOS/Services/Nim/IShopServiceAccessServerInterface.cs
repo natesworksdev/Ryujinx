42190aad84fabd399484f8515a8b058333decf6b
Ac_K <Acoustik666@gmail.com>
2020-12-16 04:08:10 +0100

    Fix comments

--- Ryujinx.HLE/HOS/Services/Nim/IShopServiceAccessServerInterface.cs
+++ Ryujinx.HLE/HOS/Services/Nim/IShopServiceAccessServerInterface.cs
@@ -25,9 +25,10 @@ namespace Ryujinx.HLE.HOS.Services.Nim
         // IsLargeResourceAvailable(pid) -> b8
         public ResultCode IsLargeResourceAvailable(ServiceCtx context)
         {
-            // TODO: Service call arp:r GetApplicationInstanceId (10.0.0+) then if it fails it calls arp:r GetMicroApplicationInstanceId (10.0.0+)
+            // TODO: Service calls arp:r GetApplicationInstanceId (10.0.0+) then if it fails it calls arp:r GetMicroApplicationInstanceId (10.0.0+)
             //       then if it fails it returns the arp:r result code.
 
+            // NOTE: Firmare 10.0.0+ don't use the Pid here anymore, but the returned InstanceId. We don't support that for now so we can just use the Pid instead.
             StorageId baseStorageId = (StorageId)ApplicationLaunchProperty.GetByPid(context).BaseGameStorageId;
 
             // NOTE: Service returns ResultCode.InvalidArgument if baseStorageId is null, doesn't occur in our case.
