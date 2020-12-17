6d76e1473b6ffe4e5dcd535f1185289f8a3590f0
LDj3SNuD <dvitiello@gmail.com>
2020-12-17 05:07:13 +0100

    GetIndirectFunctionAddress(): Validate that writing actually takes place in dynamic table memory range (and not elsewhere).

--- ARMeilleure/Instructions/NativeInterface.cs
+++ ARMeilleure/Instructions/NativeInterface.cs
@@ -2,6 +2,7 @@ using ARMeilleure.Memory;
 using ARMeilleure.State;
 using ARMeilleure.Translation;
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace ARMeilleure.Instructions
@@ -254,8 +255,10 @@ namespace ARMeilleure.Instructions
 
             if (function.HighCq)
             {
+                Debug.Assert(Context.Translator.JumpTable.CheckEntryFromAddressDynamicTable((IntPtr)entryAddress));
+
                 // Rewrite the host function address in the table to point to the highCq function.
-                Marshal.WriteInt64((IntPtr)entryAddress, 8, (long)ptr); // TODO: Debug.Assert(): Validate that writing actually takes place in table memory range (and not elsewhere).
+                Marshal.WriteInt64((IntPtr)entryAddress, 8, (long)ptr);
             }
 
             return ptr;
