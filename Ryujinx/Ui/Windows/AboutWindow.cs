7c6dd5ac9f383801f221a8fcd599226ec48c800b
Ac_K <Acoustik666@gmail.com>
2021-01-02 22:50:55 +0100

    gui: Refactoring Part 1

---
+++ Ryujinx/Ui/Windows/AboutWindow.cs
@@ -0,0 +1,46 @@
+﻿using Gtk;
+using Ryujinx.Ui.Helper;
+
+namespace Ryujinx.Ui.Windows
+{
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow() : base($"Ryujinx {Program.Version} - About")
+        {
+            InitializeComponent();
+        }
+
+        //
+        // Events
+        //
+        private void RyujinxButton_Pressed(object sender, ButtonPressEventArgs args)
+        {
+            OpenHelper.OpenUrl("https://ryujinx.org");
+        }
+
+        private void PatreonButton_Pressed(object sender, ButtonPressEventArgs args)
+        {
+            OpenHelper.OpenUrl("https://www.patreon.com/ryujinx");
+        }
+
+        private void GitHubButton_Pressed(object sender, ButtonPressEventArgs args)
+        {
+            OpenHelper.OpenUrl("https://github.com/Ryujinx/Ryujinx");
+        }
+
+        private void DiscordButton_Pressed(object sender, ButtonPressEventArgs args)
+        {
+            OpenHelper.OpenUrl("https://discordapp.com/invite/N2FmfVc");
+        }
+
+        private void TwitterButton_Pressed(object sender, ButtonPressEventArgs args)
+        {
+            OpenHelper.OpenUrl("https://twitter.com/RyujinxEmu");
+        }
+
+        private void ContributorsButton_Pressed(object sender, ButtonPressEventArgs args)
+        {
+            OpenHelper.OpenUrl("https://github.com/Ryujinx/Ryujinx/graphs/contributors?type=a");
+        }
+    }
+}
