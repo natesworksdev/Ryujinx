d12385c2d8b273105280c69566bba8f43c63cf4b
LDj3SNuD <dvitiello@gmail.com>
2021-01-04 19:38:28 +0100

    InternalVersion = 1865

--- ARMeilleure/Translation/PTC/Ptc.cs
+++ ARMeilleure/Translation/PTC/Ptc.cs
@@ -23,7 +23,7 @@ namespace ARMeilleure.Translation.PTC
     {
         private const string HeaderMagic = "PTChd";
 
-        private const int InternalVersion = 1814; //! To be incremented manually for each change to the ARMeilleure project.
+        private const int InternalVersion = 1865; //! To be incremented manually for each change to the ARMeilleure project.
 
         private const string ActualDir = "0";
         private const string BackupDir = "1";
