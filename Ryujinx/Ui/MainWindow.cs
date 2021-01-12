28ca05226fb11c67d8ae3349fd8fe08558a63666
SeraUX <xploitrisgod@gmail.com>
2021-01-12 17:08:08 +0100

    Fix some GLXBadDrawable crashes on linux
    Fixes the crashes on linux when you stop emulation, and when you try to
    exit the emulator while a game is running.
    Also tested on windows without problems on my side.

--- Ryujinx/Ui/MainWindow.cs
+++ Ryujinx/Ui/MainWindow.cs
@@ -591,7 +591,6 @@ namespace Ryujinx.Ui
                     ToggleExtraWidgets(true);
                 }
 
-                _viewBox.Remove(GlRendererWidget);
                 GlRendererWidget.Exit();
 
                 if(GlRendererWidget.Window != Window && GlRendererWidget.Window != null)
@@ -604,6 +603,7 @@ namespace Ryujinx.Ui
                 _windowsMultimediaTimerResolution?.Dispose();
                 _windowsMultimediaTimerResolution = null;
 
+                _viewBox.Remove(GlRendererWidget);
                 _viewBox.Add(_gameTableWindow);
 
                 _gameTableWindow.Expand = true;
@@ -711,6 +711,7 @@ namespace Ryujinx.Ui
 
                     // Wait for the other thread to dispose the HLE context before exiting.
                     _deviceExitStatus.WaitOne();
+                    GlRendererWidget.Dispose();
                 }
             }
 
