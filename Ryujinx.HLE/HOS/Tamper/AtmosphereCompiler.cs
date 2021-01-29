acda44b7835d84604168e80b2c7853e9c4e1f669
Caian Benedicto <caianbene@gmail.com>
2021-01-28 21:33:24 -0300

    Implement extended log opcode FFF

--- Ryujinx.HLE/HOS/Tamper/AtmosphereCompiler.cs
+++ Ryujinx.HLE/HOS/Tamper/AtmosphereCompiler.cs
@@ -24,6 +24,7 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         const ushort Ex3OpCodePause  = 0xFF0;
         const ushort Ex3OpCodeResume = 0xFF1;
+        const ushort Ex3OpCodeLog    = 0xFFF;
 
         /////////////////////////////////////////////
 
@@ -168,6 +169,22 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         /////////////////////////////////////////////
 
+        const int LogWidthIndex   = 3;
+        const int LogLogIdIndex   = 4;
+        const int LogOpTypeIndex  = 5;
+        const int LogSourceIndex  = 6;
+        const int LogOffsetIndex  = 7;
+
+        const int LogModeWithImmOff    = 0;
+        const int LogModeWithRegOff    = 1;
+        const int LogAddrRegWithImmOff = 2;
+        const int LogAddrRegWithRegOff = 3;
+        const int LogAddrReg           = 4;
+
+        const int LogOffImmSize = 9;
+
+        /////////////////////////////////////////////
+
         struct CompilationBlock
         {
             public byte[] BaseInstruction;
@@ -253,6 +270,7 @@ namespace Ryujinx.HLE.HOS.Tamper
                     case OpCodeStoreImRgA:  EmitStoreImRgA (instruction, cData); break;
                     case Ex3OpCodePause:    EmitPause      (instruction, cData); break;
                     case Ex3OpCodeResume:   EmitResume     (instruction, cData); break;
+                    case Ex3OpCodeLog:      EmitLog        (instruction, cData); break;
                     default:
                         throw new TamperCompilationException($"Opcode {opcode} not implemented in Atmosphere cheat");
                 }
@@ -289,10 +307,7 @@ namespace Ryujinx.HLE.HOS.Tamper
             byte source = instruction[LdMemIndex];
             Register dstReg = GetRegister(instruction[LdRegIndex], cData);
             ulong address = GetImmediate(instruction, LdAddrIndex, LdAddrSize);
-            address += GetAddressShift(source, cData);
-
-            Value<ulong> loadAddr = new Value<ulong>(address);
-            Pointer srcMem = new Pointer(loadAddr, cData.Process);
+            Pointer srcMem = EmitPointer(source, address, cData);
 
             Emit(typeof(OpMov<>), width, cData, dstReg, srcMem);
         }
@@ -310,16 +325,11 @@ namespace Ryujinx.HLE.HOS.Tamper
             byte source = instruction[StIMemIndex];
             Register offReg = GetRegister(instruction[StIOffRegIndex], cData);
             ulong offImm = GetImmediate(instruction, StIOffImmIndex, StIOffImmSize);
-            offImm += GetAddressShift(source, cData);
+
+            Pointer dstMem = EmitPointer(source, offReg, offImm, cData);
 
             ulong value = GetImmediate(instruction, StIValueIndex, width > 4 ? StIValueSize8 : StIValueSize4);
             Value<ulong> storeValue = new Value<ulong>(value);
-
-            Value<ulong> storeAddr = new Value<ulong>(0);
-            Value<ulong> offImmValue = new Value<ulong>(offImm);
-            cData.CurrentOperations.Add(new OpAdd<ulong>(storeAddr, offReg, offImmValue));
-
-            Pointer dstMem = new Pointer(storeAddr, cData.Process);
 
             Emit(typeof(OpMov<>), width, cData, dstMem, storeValue);
         }
@@ -335,29 +345,28 @@ namespace Ryujinx.HLE.HOS.Tamper
             // V: Value to write to memory.
 
             byte width = instruction[StRWidthIndex];
-            IOperand srcReg = GetRegister(instruction[StRAddrRegIndex], cData);
-            IOperand storeAddr = srcReg;
+            Register srcReg = GetRegister(instruction[StRAddrRegIndex], cData);
             byte doIncrement = instruction[StRDoIncIndex];
             byte doOffset = instruction[StRDoOffIndex];
             ulong value = GetImmediate(instruction, StRValueIndex, StRValueSize); // TODO: Optimize to 'width'?
             Value<ulong> storeValue = new Value<ulong>(value);
 
+            Pointer dstMem;
+
             switch (doOffset)
             {
                 case 0:
                     // Don't offset the address register by another register.
+                    dstMem = EmitPointer(srcReg, cData);
                     break;
                 case 1:
                     // Replace the source address by the sum of the base and offset registers.
-                    storeAddr = new Value<ulong>(0);
-                    IOperand offsetReg = GetRegister(instruction[StROffRegIndex], cData);
-                    cData.CurrentOperations.Add(new OpAdd<ulong>(storeAddr, srcReg, offsetReg));
+                    Register offsetReg = GetRegister(instruction[StROffRegIndex], cData);
+                    dstMem = EmitPointer(srcReg, offsetReg, cData);
                     break;
                 default:
                     throw new TamperCompilationException($"Invalid offset mode {doOffset} in Atmosphere cheat");
             }
-
-            Pointer dstMem = new Pointer(storeAddr, cData.Process);
 
             Emit(typeof(OpMov<>), width, cData, dstMem, storeValue);
 
@@ -387,24 +396,6 @@ namespace Ryujinx.HLE.HOS.Tamper
             // x: Register used as offset when O is 1, Memory type when O is 3, 4 or 5.
             // a: Value used as offset when O is 2, 4 or 5.
 
-            /* const int StAWidthIndex   = 1;
-               const int StASrcRegIndex  = 2;
-               const int StAAddrRegIndex = 3;
-               const int StADoIncIndex   = 4;
-               const int StAOffTypeIndex = 5;
-               const int StAOffRegIndex  = 6;
-               const int StAOffImmIndex  = 7;
-
-               const int StANoOff           = 0;
-               const int StARegOff          = 1;
-               const int StAImmOff          = 2;
-               const int StAMRWithBaseReg   = 3;
-               const int StAMRWithImmOff    = 4;
-               const int StAMRWithImmRegOff = 5;
-
-               const int StAValueSize1  = 1;
-               const int StAValueSize8  = 9;*/
-
             byte width = instruction[StAWidthIndex];
             Register srcReg = GetRegister(instruction[StASrcRegIndex], cData);
             Register addrReg = GetRegister(instruction[StAAddrRegIndex], cData);
@@ -414,48 +405,37 @@ namespace Ryujinx.HLE.HOS.Tamper
             Register offReg = GetRegister(offRegOrMem, cData);
             ulong offImm = GetImmediate(instruction, StAOffImmIndex, instruction.Length <= 8 ? StAValueSize1 : StAValueSize8);
 
-            Value<ulong> offImmValue;
-            IOperand storeAddr;
+            Pointer dstMem;
 
             switch (offsetType)
             {
                 case StANoOff:
                     // *($R) = $S
-                    storeAddr = addrReg;
+                    dstMem = EmitPointer(addrReg, cData);
                     break;
                 case StARegOff:
                     // *($R + $x) = $S
-                    storeAddr = new Value<ulong>(0);
-                    cData.CurrentOperations.Add(new OpAdd<ulong>(storeAddr, addrReg, offReg));
+                    dstMem = EmitPointer(addrReg, offReg, cData);
                     break;
                 case StAImmOff:
                     // *(#a) = $S
-                    storeAddr = new Value<ulong>(offImm);
+                    dstMem = EmitPointer(offImm, cData);
                     break;
                 case StAMRWithBaseReg:
                     // *(?x + $R) = $S
-                    offImm = GetAddressShift(offRegOrMem, cData);
-                    offImmValue = new Value<ulong>(offImm);
-                    storeAddr = new Value<ulong>(0);
-                    cData.CurrentOperations.Add(new OpAdd<ulong>(storeAddr, addrReg, offImmValue));
+                    dstMem = EmitPointer(offRegOrMem, addrReg, cData);
                     break;
                 case StAMRWithImmOff:
                     // *(?x + #a) = $S
-                    offImm += GetAddressShift(offRegOrMem, cData);
-                    storeAddr = new Value<ulong>(offImm);
+                    dstMem = EmitPointer(offRegOrMem, offImm, cData);
                     break;
                 case StAMRWithImmRegOff:
                     // *(?x + #a + $R) = $S
-                    offImm += GetAddressShift(offRegOrMem, cData);
-                    offImmValue = new Value<ulong>(offImm);
-                    storeAddr = new Value<ulong>(0);
-                    cData.CurrentOperations.Add(new OpAdd<ulong>(storeAddr, addrReg, offImmValue));
+                    dstMem = EmitPointer(offRegOrMem, addrReg, offImm, cData);
                     break;
                 default:
                     throw new TamperCompilationException($"Invalid offset type {offsetType} in Atmosphere cheat");
             }
-
-            Pointer dstMem = new Pointer(storeAddr, cData.Process);
 
             Emit(typeof(OpMov<>), width, cData, dstMem, srcReg);
 
@@ -664,12 +644,128 @@ namespace Ryujinx.HLE.HOS.Tamper
             cData.CurrentOperations.Add(new OpProcCtrl(false));
         }
 
-        private void Emit(Type instruction, byte width, CompilationData cData, params IOperand[] operands)
+        private void EmitLog(byte[] instruction, CompilationData cData)
+        {
+            // FFFTIX##
+            // FFFTI0Ma aaaaaaaa
+            // FFFTI1Mr
+            // FFFTI2Ra aaaaaaaa
+            // FFFTI3Rr
+            // FFFTI4V0
+            // T: Width of memory write (1, 2, 4, or 8 bytes).
+            // I: Log id.
+            // X: Operand Type, see below.
+            // M: Memory Type (operand types 0 and 1).
+            // R: Address Register (operand types 2 and 3).
+            // a: Relative Address (operand types 0 and 2).
+            // r: Offset Register (operand types 1 and 3).
+            // V: Value Register (operand type 4).
+
+            byte width = instruction[LogWidthIndex];
+            byte logId = instruction[LogLogIdIndex];
+            byte opType = instruction[LogOpTypeIndex];
+            byte addrRegOrMem = instruction[LogSourceIndex];
+            byte offRegOrImm = instruction[LogOffsetIndex];
+            ulong offImm;
+            Register addrReg;
+            Register offReg;
+            IOperand srcOp;
+
+            switch (opType)
+            {
+                case LogModeWithImmOff:
+                    // *(?x + #a)
+                    offImm = GetImmediate(instruction, LogOffsetIndex, LogOffImmSize);
+                    srcOp = EmitPointer(addrRegOrMem, offImm, cData);
+                    break;
+                case LogModeWithRegOff:
+                    // *(?x + $r)
+                    offReg = GetRegister(instruction[offRegOrImm], cData);
+                    srcOp = EmitPointer(addrRegOrMem, offReg, cData);
+                    break;
+                case LogAddrRegWithImmOff:
+                    // *($R + #a)
+                    addrReg = GetRegister(instruction[addrRegOrMem], cData);
+                    offImm = GetImmediate(instruction, LogOffsetIndex, LogOffImmSize);
+                    srcOp = EmitPointer(addrReg, offImm, cData);
+                    break;
+                case LogAddrRegWithRegOff:
+                    // *($R + $r)
+                    addrReg = GetRegister(instruction[addrRegOrMem], cData);
+                    offReg = GetRegister(instruction[offRegOrImm], cData);
+                    srcOp = EmitPointer(addrReg, offReg, cData);
+                    break;
+                case LogAddrReg:
+                    // $V
+                    srcOp = GetRegister(instruction[addrRegOrMem], cData);
+                    break;
+                default:
+                    throw new TamperCompilationException($"Invalid operand type {opType} in Atmosphere cheat");
+            }
+
+            Emit(typeof(OpLog<>), width, cData, logId, srcOp);
+        }
+
+        private Pointer EmitPointer(ulong addressImmediate, CompilationData cData)
+        {
+            Value<ulong> addressImmediateValue = new Value<ulong>(addressImmediate);
+            return new Pointer(addressImmediateValue, cData.Process);
+        }
+
+        private Pointer EmitPointer(Register addressRegister, CompilationData cData)
+        {
+            return new Pointer(addressRegister, cData.Process);
+        }
+
+        private Pointer EmitPointer(Register addressRegister, ulong offsetImmediate, CompilationData cData)
+        {
+            Value<ulong> offsetImmediateValue = new Value<ulong>(offsetImmediate);
+            Value<ulong> finalAddressValue = new Value<ulong>(0);
+            cData.CurrentOperations.Add(new OpAdd<ulong>(finalAddressValue, addressRegister, offsetImmediateValue));
+            return new Pointer(finalAddressValue, cData.Process);
+        }
+
+        private Pointer EmitPointer(Register addressRegister, Register offsetRegister, CompilationData cData)
+        {
+            Value<ulong> finalAddressValue = new Value<ulong>(0);
+            cData.CurrentOperations.Add(new OpAdd<ulong>(finalAddressValue, addressRegister, offsetRegister));
+            return new Pointer(finalAddressValue, cData.Process);
+        }
+
+        private Pointer EmitPointer(Register addressRegister, Register offsetRegister, ulong offsetImmediate, CompilationData cData)
+        {
+            Value<ulong> offsetImmediateValue = new Value<ulong>(offsetImmediate);
+            Value<ulong> finalOffsetValue = new Value<ulong>(0);
+            cData.CurrentOperations.Add(new OpAdd<ulong>(finalOffsetValue, offsetRegister, offsetImmediateValue));
+            Value<ulong> finalAddressValue = new Value<ulong>(0);
+            cData.CurrentOperations.Add(new OpAdd<ulong>(finalAddressValue, addressRegister, finalOffsetValue));
+            return new Pointer(finalAddressValue, cData.Process);
+        }
+
+        private Pointer EmitPointer(byte memorySource, ulong offsetImmediate, CompilationData cData)
+        {
+            offsetImmediate += GetAddressShift(memorySource, cData);
+            return EmitPointer(offsetImmediate, cData);
+        }
+
+        private Pointer EmitPointer(byte memorySource, Register offsetRegister, CompilationData cData)
+        {
+            ulong offsetImmediate = GetAddressShift(memorySource, cData);
+            return EmitPointer(offsetRegister, offsetImmediate, cData);
+        }
+
+        private Pointer EmitPointer(byte memorySource, Register offsetRegister, ulong offsetImmediate, CompilationData cData)
+        {
+            offsetImmediate += GetAddressShift(memorySource, cData);
+            return EmitPointer(offsetRegister, offsetImmediate, cData);
+        }
+
+        private void Emit(Type instruction, byte width, CompilationData cData, params Object[] operands)
         {
             cData.CurrentOperations.Add((IOperation)Create(instruction, width, operands));
         }
 
-        private Object Create(Type instruction, byte width, params IOperand[] operands)
+        private Object Create(Type instruction, byte width, params Object[] operands)
         {
             Type realType;
 
@@ -688,6 +784,8 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         private ICondition GetIfCondition(byte[] instruction, CompilationData cData)
         {
+            // WARNING: Do not use EmitPointer because this method is not supposed to emit new instructions!
+
             byte width = instruction[IfWidthIndex];
             byte source = instruction[IfMemIndex];
             byte condition = instruction[IfCondTypeIndex];
@@ -716,6 +814,8 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         private ICondition GetInputCondition(byte[] instruction, CompilationData cData)
         {
+            // WARNING: Do not use EmitPointer because this method is not supposed to emit new instructions!
+
             ulong mask = GetImmediate(instruction, InputMaskIndex, InputMaskSize);
             return new InputMask((long)mask, cData.PressedKeys);
         }
