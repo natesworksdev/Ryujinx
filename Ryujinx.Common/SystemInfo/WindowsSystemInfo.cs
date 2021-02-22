58bcc0a65f427fec2d2ccaf127498d23a62af9a0
mageven <62494521+mageven@users.noreply.github.com>
2021-02-22 20:45:47 +0530

    Revise SystemInfo
    Cleans up and adds a bit more info (logical core count and available mem at launch) to logs.
    
    - Extract CPU name from CPUID when supported.
    - Linux: Robust parsing of procfs files
    - Windows: Prefer native calls to WMI
    - Remove unnecessary virtual specifiers

--- Ryujinx.Common/SystemInfo/WindowsSystemInfo.cs
+++ Ryujinx.Common/SystemInfo/WindowsSystemInfo.cs
@@ -1,48 +1,105 @@
-using Ryujinx.Common.Logging;
 using System;
+using System.Globalization;
 using System.Management;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
+using Ryujinx.Common.Logging;
 
 namespace Ryujinx.Common.SystemInfo
 {
     [SupportedOSPlatform("windows")]
-    internal class WindowsSystemInfo : SystemInfo
+    class WindowsSystemInfo : SystemInfo
     {
-        public override string CpuName { get; }
-        public override ulong RamSize { get; }
-
-        public WindowsSystemInfo()
+        internal WindowsSystemInfo()
         {
-            bool wmiNotAvailable = false;
+            CpuName = $"{GetCpuidCpuName() ?? GetCpuNameWMI()} ; {LogicalCoreCount} logical"; // WMI is very slow
+            (RamTotal, RamAvailable) = GetMemoryStats();
+        }
 
+        private static (ulong Total, ulong Available) GetMemoryStats()
+        {
+            MEMORYSTATUSEX memStatus = new MEMORYSTATUSEX();
+            if (GlobalMemoryStatusEx(memStatus))
+            {
+                return (memStatus.TotalPhys, memStatus.AvailPhys); // Bytes
+            }
+            else
+            {
+                Logger.Error?.Print(LogClass.Application, $"GlobalMemoryStatusEx failed. Error {Marshal.GetLastWin32Error():X}");
+            }
+
+            // Alternative WMI path
+            ManagementObjectCollection memObjs = GetWMIObjects("root\\CIMV2", "SELECT * FROM Win32_OperatingSystem");
+
+            if (memObjs != null)
+            {
+                foreach (var memObj in memObjs)
+                {
+                    // Entries are in KB
+                    _ = ulong.TryParse(memObj["TotalVisibleMemorySize"].ToString(), NumberStyles.Integer, CultureInfo.InvariantCulture, out ulong totalKB);
+                    _ = ulong.TryParse(memObj["FreePhysicalMemory"].ToString(), NumberStyles.Integer, CultureInfo.InvariantCulture, out ulong availableKB);
+
+                    return (totalKB * 1024, availableKB * 1024);
+                }
+            }
+
+            return (0, 0);
+        }
+
+        private static string GetCpuNameWMI()
+        {
+            ManagementObjectCollection cpuObjs = GetWMIObjects("root\\CIMV2", "SELECT * FROM Win32_Processor");
+
+            if (cpuObjs != null)
+            {
+                foreach (var cpuObj in cpuObjs)
+                {
+                    return cpuObj["Name"].ToString().Trim();
+                }
+            }
+
+            return Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER").Trim();
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private class MEMORYSTATUSEX
+        {
+            public uint Length;
+            public uint MemoryLoad;
+            public ulong TotalPhys;
+            public ulong AvailPhys;
+            public ulong TotalPageFile;
+            public ulong AvailPageFile;
+            public ulong TotalVirtual;
+            public ulong AvailVirtual;
+            public ulong AvailExtendedVirtual;
+
+            public MEMORYSTATUSEX()
+            {
+                Length = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+            }
+        }
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
+
+        private static ManagementObjectCollection GetWMIObjects(string scope, string query)
+        {
             try
             {
-                foreach (ManagementBaseObject mObject in new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor").Get())
-                {
-                    CpuName = mObject["Name"].ToString();
-                }
-
-                foreach (ManagementBaseObject mObject in new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_OperatingSystem").Get())
-                {
-                    RamSize = ulong.Parse(mObject["TotalVisibleMemorySize"].ToString()) * 1024;
-                }
+                return new ManagementObjectSearcher(scope, query).Get();
             }
-            catch (PlatformNotSupportedException)
+            catch (PlatformNotSupportedException e)
             {
-                wmiNotAvailable = true;
+                Logger.Error?.Print(LogClass.Application, $"WMI isn't available : {e.Message}");
             }
-            catch (COMException)
+            catch (COMException e)
             {
-                wmiNotAvailable = true;
+                Logger.Error?.Print(LogClass.Application, $"WMI isn't available : {e.Message}");
             }
 
-            if (wmiNotAvailable)
-            {
-                Logger.Error?.Print(LogClass.Application, "WMI isn't available, system informations will use default values.");
-
-                CpuName = "Unknown";
-            }
+            return null;
         }
     }
 }
