d2d868081640cf2f795f1ddbe149e9d1000a734d
jms-c <79325704+jms-c@users.noreply.github.com>
2021-02-23 18:24:42 +0000

    Minor Typo
    Just a quick fix. (I wish the ci didn't retry everu time I commit tho)

--- Ryujinx/Ui/Windows/DlcWindow.cs
+++ Ryujinx/Ui/Windows/DlcWindow.cs
@@ -77,7 +77,7 @@ namespace Ryujinx.Ui.Windows
             {
                 if (!File.Exists(dlcContainer.Path))
                 {
-                    Console.WriteLine("DLC files has been moved or deleted; purging dlc.json file located at " + _dlcJsonPath);
+                    Console.WriteLine("DLC files have been moved or deleted; purging dlc.json file located at " + _dlcJsonPath);
                     GtkDialog.CreateErrorDialog("DLC files have been moved or deleted; please re-add your DLC.");
                     File.Delete(_dlcJsonPath);
                     break; // don't handle current dlc.
