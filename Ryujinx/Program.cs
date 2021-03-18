36281122eda72d658b843c20d5eaca8818cae85f
Olivier Tremblay-Boulet <melonspeedruns@outlook.com>
2021-03-18 10:53:04 -0400

    Addressed Ac_K's feedback

--- Ryujinx/Program.cs
+++ Ryujinx/Program.cs
@@ -5,7 +5,6 @@ using Ryujinx.Common.GraphicsDriver;
 using Ryujinx.Common.Logging;
 using Ryujinx.Common.System;
 using Ryujinx.Common.SystemInfo;
-using Ryujinx.Common.Platform;
 using Ryujinx.Configuration;
 using Ryujinx.Modules;
 using Ryujinx.Ui;
@@ -142,9 +141,9 @@ namespace Ryujinx
                 ConfigurationState.Instance.Ui.StartFullscreen.Value = true;
             }
 
-            if (!ConfigurationState.Instance.ShowConsole)
+            if (ConfigurationState.Instance.ShowConsole)
             {
-                Platform.Instance.HideConsole();
+                ConsoleHelper.ShowConsole();
             }
 
             // Logging system information.
--- Ryujinx/Ui/Windows/SettingsWindow.cs
+++ Ryujinx/Ui/Windows/SettingsWindow.cs
@@ -4,6 +4,7 @@ using Ryujinx.Audio.Backends.SDL2;
 using Ryujinx.Audio.Backends.SoundIo;
 using Ryujinx.Common.Configuration;
 using Ryujinx.Common.Configuration.Hid;
+using Ryujinx.Common.System;
 using Ryujinx.Common.Platform;
 using Ryujinx.Common.GraphicsDriver;
 using Ryujinx.Configuration;
@@ -18,7 +19,6 @@ using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 
 using GUI = Gtk.Builder.ObjectAttribute;
 
@@ -280,7 +280,7 @@ namespace Ryujinx.Ui.Windows
             }
 
             // Hide specific-platform elements
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            if (!ConsoleHelper.IsHideSupported())
             {
                 _showConsole.Hide();
             }
@@ -526,11 +526,11 @@ namespace Ryujinx.Ui.Windows
 
             if (ConfigurationState.Instance.ShowConsole)
             {
-                Platform.Instance.ShowConsole();
+                ConsoleHelper.ShowConsole();
             }
             else
             {
-                Platform.Instance.HideConsole();
+                ConsoleHelper.HideConsole();
             }
 
             ConfigurationState.Instance.ToFileFormat().SaveConfig(Program.ConfigurationPath);
