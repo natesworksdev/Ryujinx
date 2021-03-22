c03005915267df380a253f21f78f60ffa6ffeebc
Ac_K <Acoustik666@gmail.com>
2021-03-22 19:23:38 +0100

    sfdnsres: Cleanup service and implements some calls
    This PR is a big cleanup to our current implementation of `sfdnsres` service.
    Additionnaly to that, some calls and fix have been done by @Thog (PRd with approval, thanks to her).
    - Implementation of `GetAddrInfoRequest` (Fixes #637).
    - Partial implementation of `GetHostByNameRequestWithOptions`, `GetHostByAddrRequestWithOptions` and `GetAddrInfoRequestWithOptions` (Fixes #642, Fixes #1233).
    
    A DNS Blacklist have been done by @riperiperi (which is currently used in LDN build, then that reduces code differences between the LDN build and master.
    
    Now a lot of games are playable or are blocked to the menu because it needs online service:
    
    Co-Authored-By: Mary <1760003+Thog@users.noreply.github.com>
    Co-Authored-By: riperiperi <6294155+riperiperi@users.noreply.github.com>

---
+++ Ryujinx.HLE/HOS/Services/Sockets/Sfdnsres/Types/AddrInfoSerializedHeader.cs
@@ -0,0 +1,37 @@
+﻿using System.Buffers.Binary;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Ryujinx.HLE.HOS.Services.Sockets.Sfdnsres.Types
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 6 * sizeof(int))]
+    struct AddrInfoSerializedHeader
+    {
+        public uint Magic;
+        public int  Flags;
+        public int  Family;
+        public int  SocketType;
+        public int  Protocol;
+        public uint AddressLength;
+
+        public AddrInfoSerializedHeader(IPAddress address, SocketType socketType)
+        {
+            Magic      = (uint)BinaryPrimitives.ReverseEndianness(unchecked((int)SfdnsresContants.AddrInfoMagic));
+            Flags      = BinaryPrimitives.ReverseEndianness(0);
+            Family     = BinaryPrimitives.ReverseEndianness((int)address.AddressFamily);
+            SocketType = BinaryPrimitives.ReverseEndianness((int)socketType);
+            Protocol   = BinaryPrimitives.ReverseEndianness(0);
+
+            if (address.AddressFamily == AddressFamily.InterNetwork)
+            {
+                AddressLength = (uint)Unsafe.SizeOf<AddrInfo4>();
+            }
+            else
+            {
+                AddressLength = 4;
+            }
+        }
+    }
+}
