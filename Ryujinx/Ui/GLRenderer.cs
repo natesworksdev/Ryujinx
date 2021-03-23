0cd231f9a685b73fd96e30e74434074fe5c48028
Mary <me@thog.eu>
2021-03-23 23:48:09 +0100

    Enforce RGB only framebuffer on the GLWidget
    Fix possible transparent window

--- Ryujinx/Ui/GLRenderer.cs
+++ Ryujinx/Ui/GLRenderer.cs
@@ -264,7 +264,7 @@ namespace Ryujinx.Ui
 
         private static FramebufferFormat GetGraphicsMode()
         {
-            return Environment.OSVersion.Platform == PlatformID.Unix ? new FramebufferFormat(new ColorFormat(8, 8, 8, 0), 16, 0, ColorFormat.Zero, 0, 2, false) : FramebufferFormat.Default;
+            return new FramebufferFormat(new ColorFormat(8, 8, 8, 0), 16, 0, ColorFormat.Zero, 0, 2, false);
         }
 
         private void GLRenderer_ShuttingDown(object sender, EventArgs args)
