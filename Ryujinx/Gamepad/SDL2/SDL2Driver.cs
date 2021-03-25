852d704f8bec6491a85184ffb103856086b3fe5a
Mary <me@thog.eu>
2021-03-25 21:10:25 +0100

    Filter SDL2 events a bit

--- Ryujinx/Gamepad/SDL2/SDL2Driver.cs
+++ Ryujinx/Gamepad/SDL2/SDL2Driver.cs
@@ -24,7 +24,7 @@ namespace Ryujinx.Gamepad.SDL2
             }
         }
 
-        private const uint SdlInitFlags = SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC;
+        private const uint SdlInitFlags = SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER | SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC;
 
         private bool _isRunning;
         private Thread _worker;
@@ -50,9 +50,55 @@ namespace Ryujinx.Gamepad.SDL2
                 throw new Exception(errorMessage);
             }
 
+            // First ensure that we only enable joystick events (for connected/disconnected).
+            SDL_GameControllerEventState(SDL_DISABLE);
+            SDL_JoystickEventState(SDL_ENABLE);
+
+            // Disable all joysticks information, we don't need them no need to flood the event queue for that.
+            SDL_EventState(SDL_EventType.SDL_JOYAXISMOTION, SDL_DISABLE);
+            SDL_EventState(SDL_EventType.SDL_JOYBALLMOTION, SDL_DISABLE);
+            SDL_EventState(SDL_EventType.SDL_JOYHATMOTION, SDL_DISABLE);
+            SDL_EventState(SDL_EventType.SDL_JOYBUTTONDOWN, SDL_DISABLE);
+            SDL_EventState(SDL_EventType.SDL_JOYBUTTONUP, SDL_DISABLE);
+
             _worker = new Thread(EventWorker);
             _isRunning = true;
             _worker.Start();
+        }
+
+        private unsafe int FilterEvent(IntPtr userdata, IntPtr sdlevent)
+        {
+            HandleSDLEvent(ref *((SDL_Event*)sdlevent));
+
+            return 0;
+        }
+
+        private void HandleSDLEvent(ref SDL_Event evnt)
+        {
+            Logger.Error?.Print(LogClass.Application, $"{evnt.type}");
+
+            if (evnt.type == SDL_EventType.SDL_JOYDEVICEADDED)
+            {
+                int deviceId = evnt.cbutton.which;
+
+                // SDL2 loves to be inconsistent here by providing the device id instead of the instance id (like on removed event), as such we just grab it and send it inside our system.
+                int instanceId = SDL_JoystickGetDeviceInstanceID(evnt.cbutton.which);
+
+                if (instanceId == -1)
+                {
+                    return;
+                }
+
+                Logger.Debug?.Print(LogClass.Application, $"Added joystick instance id {evnt.cbutton.which}");
+
+                OnJoyStickConnected?.Invoke(deviceId, instanceId);
+            }
+            else if (evnt.type == SDL_EventType.SDL_JOYDEVICEREMOVED)
+            {
+                Logger.Debug?.Print(LogClass.Application, $"Removed joystick instance id {evnt.cbutton.which}");
+
+                OnJoystickDisconnected?.Invoke(evnt.cbutton.which);
+            }
         }
 
         private void EventWorker()
@@ -60,42 +106,16 @@ namespace Ryujinx.Gamepad.SDL2
             // Change this... maybe
             const int WaitTimeMs = 10;
 
-            AutoResetEvent waitHandle = new AutoResetEvent(false);
+            ManualResetEventSlim waitHandle = new ManualResetEventSlim(false);
 
             while (_isRunning)
             {
                 while (SDL_PollEvent(out SDL_Event evnt) != 0)
                 {
-                    // TODO: fire add event
-                    // TODO: aggregate joystick ids that were gamepad to fire remove event
-
-                    if (evnt.type == SDL_EventType.SDL_JOYDEVICEADDED)
-                    {
-                        int deviceId = evnt.cbutton.which;
-
-                        // SDL2 loves to be inconsistent here by providing the device id instead of the instance id (like on removed event), as such we just grab it and send it inside our system.
-                        int instanceId = SDL_JoystickGetDeviceInstanceID(evnt.cbutton.which);
-
-                        if (instanceId == -1)
-                        {
-                            continue;
-                        }
-
-                        Logger.Debug?.Print(LogClass.Application, $"Added joystick instance id {evnt.cbutton.which}");
-
-                        OnJoyStickConnected?.Invoke(deviceId, instanceId);
-                    }
-                    else if (evnt.type == SDL_EventType.SDL_JOYDEVICEREMOVED)
-                    {
-                        Logger.Debug?.Print(LogClass.Application, $"Removed joystick instance id {evnt.cbutton.which}");
-
-                        OnJoystickDisconnected?.Invoke(evnt.cbutton.which);
-                    }
-
-                    // Maybe handle some events here if we really feel like it
+                    HandleSDLEvent(ref evnt);
                 }
 
-                waitHandle.WaitOne(WaitTimeMs);
+                waitHandle.Wait(WaitTimeMs);
             }
 
             waitHandle.Dispose();
