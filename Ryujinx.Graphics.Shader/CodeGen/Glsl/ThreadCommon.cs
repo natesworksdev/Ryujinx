cf6a20c8f8ce578a5842205a00e7ad080a0c8594
gdkchan <gab.dark.100@gmail.com>
2021-04-05 15:38:04 -0300

    Nits

--- Ryujinx.Graphics.Shader/CodeGen/Glsl/ThreadCommon.cs
+++ Ryujinx.Graphics.Shader/CodeGen/Glsl/ThreadCommon.cs
@@ -6,14 +6,14 @@ namespace Ryujinx.Graphics.Shader.CodeGen.Glsl
     {
         public static string GenerateMaskInvocation(ShaderConfig config, string invocation)
         {
-            return config.GpuAccessor.QueryShaderMaxThreads32() ? invocation : $"({invocation} & 31)";
+            return config.GpuAccessor.QueryShaderMaxThreads32() ? invocation : $"({invocation} & 0x1fu)";
         }
 
         public static string GenerateUnpackMask(ShaderConfig config, string u64mask)
         {
             return config.GpuAccessor.QueryShaderMaxThreads32()
                 ? $"unpackUint2x32({u64mask}).x"
-                : $"(gl_SubGroupInvocationARB < 32 ? unpackUint2x32({u64mask}).x : unpackUint2x32({u64mask}).y)";
+                : $"(gl_SubGroupInvocationARB < 32u ? unpackUint2x32({u64mask}).x : unpackUint2x32({u64mask}).y)";
         }
     }
 }
