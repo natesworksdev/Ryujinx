7373f1f4be6c4387746f33048ab8b9b858a04806
riperiperi <rhy3756547@hotmail.com>
2021-04-08 16:37:39 +0100

    Some minor optimisation

---
+++ Ryujinx.Graphics.GAL/Multithreading/Commands/SetStorageBuffersCommand.cs
@@ -0,0 +1,23 @@
+﻿using Ryujinx.Graphics.GAL.Multithreading.Model;
+using System;
+
+namespace Ryujinx.Graphics.GAL.Multithreading.Commands
+{
+    struct SetStorageBuffersCommand : IGALCommand
+    {
+        public CommandType CommandType => CommandType.SetStorageBuffers;
+        private SpanRef<BufferRange> _buffers;
+
+        public void Set(SpanRef<BufferRange> buffers)
+        {
+            _buffers = buffers;
+        }
+
+        public void Run(ThreadedRenderer threaded, IRenderer renderer)
+        {
+            Span<BufferRange> buffers = _buffers.Get(threaded);
+            renderer.Pipeline.SetStorageBuffers(threaded.Buffers.MapBufferRanges(buffers));
+            _buffers.Dispose(threaded);
+        }
+    }
+}
---
+++ Ryujinx.Graphics.GAL/Multithreading/Commands/SetUniformBuffersCommand.cs
@@ -0,0 +1,23 @@
+﻿using Ryujinx.Graphics.GAL.Multithreading.Model;
+using System;
+
+namespace Ryujinx.Graphics.GAL.Multithreading.Commands
+{
+    struct SetUniformBuffersCommand : IGALCommand
+    {
+        public CommandType CommandType => CommandType.SetUniformBuffers;
+        private SpanRef<BufferRange> _buffers;
+
+        public void Set(SpanRef<BufferRange> buffers)
+        {
+            _buffers = buffers;
+        }
+
+        public void Run(ThreadedRenderer threaded, IRenderer renderer)
+        {
+            Span<BufferRange> buffers = _buffers.Get(threaded);
+            renderer.Pipeline.SetUniformBuffers(threaded.Buffers.MapBufferRanges(buffers));
+            _buffers.Dispose(threaded);
+        }
+    }
+}
