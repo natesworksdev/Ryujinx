e891f62769347f0c8f92b7ed4e1023550cabe9f6
Ac_K <Acoustik666@gmail.com>
2021-04-08 19:55:53 +0200

    nifm/ssl: Implement GetCurrentNetworkProfile and stub Ssl Service

---
+++ Ryujinx.HLE/HOS/Services/Ssl/Types/InterfaceVersion.cs
@@ -0,0 +1,9 @@
+﻿namespace Ryujinx.HLE.HOS.Services.Ssl.Types
+{
+    enum InterfaceVersion : uint
+    {
+        Unknown1 = 1, // 3.0.0+
+        Unknown2 = 2, // 5.0.0+
+        Unknown3 = 3  // 6.0.0+
+    }
+}
