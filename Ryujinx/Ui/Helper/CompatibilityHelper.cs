bc72a49ac1edf3d39b48358dbb5027f64f7ac838
Joshi234 <46032261+Joshi234@users.noreply.github.com>
2021-04-13 00:36:37 +0200

    fix url

--- Ryujinx/Ui/Helper/CompatibilityHelper.cs
+++ Ryujinx/Ui/Helper/CompatibilityHelper.cs
@@ -132,7 +132,7 @@ namespace Ryujinx.Ui.Helper
 
     public class CompatibilityItem
     {
-        public string url { get; set; }
+        public string html_url { get; set; }
         public string title { get; set; }
         public IList<CompatibilityLabel> labels { get; set; }
     }
