42fe89c882aef55d391ca5d1f43fc988019b9f25
sharmander <saldabain.dev@gmail.com>
2021-04-13 10:55:21 -0400

    Add files from https://github.com/mbuchetics/RangeTree

---
+++ Ryujinx.Common/Collections/IntervalTree.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ryujinx.Common.Collections
+{
+    public class IntervalTree<TKey, TValue> : IIntervalTree<TKey, TValue>
+    {
+        private IntervalTreeNode<TKey, TValue> root;
+        private List<RangeValuePair<TKey, TValue>> items;
+        private readonly IComparer<TKey> comparer;
+        private bool isInSync;
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public TKey Max
+        {
+            get
+            {
+                if (!isInSync)
+                    Rebuild();
+
+                return root.Max;
+            }
+        }
+
+        public TKey Min
+        {
+            get
+            {
+                if (!isInSync)
+                    Rebuild();
+
+                return root.Min;
+            }
+        }
+
+        public IEnumerable<TValue> Values => items.Select(i => i.Value);
+
+        public int Count => items.Count;
+
+        /// <summary>
+        /// Initializes an empty tree.
+        /// </summary>
+        public IntervalTree() : this(Comparer<TKey>.Default) { }
+
+        /// <summary>
+        /// Initializes an empty tree.
+        /// </summary>
+        public IntervalTree(IComparer<TKey> comparer)
+        {
+            this.comparer = comparer ?? Comparer<TKey>.Default;
+            isInSync = true;
+            root = new IntervalTreeNode<TKey, TValue>(this.comparer);
+            items = new List<RangeValuePair<TKey, TValue>>();
+        }
+
+        public IEnumerable<TValue> Query(TKey value)
+        {
+            if (!isInSync)
+                Rebuild();
+
+            return root.Query(value);
+        }
+
+        public IEnumerable<TValue> Query(TKey from, TKey to)
+        {
+            if (!isInSync)
+                Rebuild();
+
+            return root.Query(from, to);
+        }
+
+        public void Add(TKey from, TKey to, TValue value)
+        {
+            if (comparer.Compare(from, to) > 0)
+                throw new ArgumentOutOfRangeException($"{nameof(from)} cannot be larger than {nameof(to)}");
+
+            isInSync = false;
+            items.Add(new RangeValuePair<TKey, TValue>(from, to, value));
+        }
+
+        public void Remove(TValue value)
+        {
+            isInSync = false;
+            items = items.Where(l => !l.Value.Equals(value)).ToList();
+        }
+
+        public void Remove(IEnumerable<TValue> items)
+        {
+            isInSync = false;
+            this.items = this.items.Where(l => !items.Contains(l.Value)).ToList();
+        }
+
+        public void Clear()
+        {
+            root = new IntervalTreeNode<TKey, TValue>(comparer);
+            items = new List<RangeValuePair<TKey, TValue>>();
+            isInSync = true;
+        }
+
+        public IEnumerator<RangeValuePair<TKey, TValue>> GetEnumerator()
+        {
+            if (!isInSync)
+                Rebuild();
+
+            return items.GetEnumerator();
+        }
+
+        private void Rebuild()
+        {
+            if (isInSync)
+                return;
+
+            if (items.Count > 0)
+                root = new IntervalTreeNode<TKey, TValue>(items, comparer);
+            else
+                root = new IntervalTreeNode<TKey, TValue>(comparer);
+            isInSync = true;
+        }
+    }
+}
---
+++ Ryujinx.Common/Collections/IntervalTreeNode.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Ryujinx.Common.Collections
+{
+    /// <summary>
+    /// A node of the range tree. Given a list of items, it builds
+    /// its subtree. Also contains methods to query the subtree.
+    /// Basically, all interval tree logic is here.
+    /// </summary>
+    internal class IntervalTreeNode<TKey, TValue> : IComparer<RangeValuePair<TKey, TValue>>
+    {
+        private readonly TKey center;
+        private readonly IntervalTreeNode<TKey, TValue> leftNode;
+        private readonly IntervalTreeNode<TKey, TValue> rightNode;
+        private readonly RangeValuePair<TKey, TValue>[] items;
+
+        private readonly IComparer<TKey> comparer;
+
+        /// <summary>
+        /// Initializes an empty node.
+        /// </summary>
+        /// <param name="comparer">The comparer used to compare two items.</param>
+        public IntervalTreeNode(IComparer<TKey> comparer)
+        {
+            this.comparer = comparer ?? Comparer<TKey>.Default;
+
+            center = default(TKey);
+            leftNode = null;
+            rightNode = null;
+            items = null;
+        }
+
+        /// <summary>
+        /// Initializes a node with a list of items, builds the sub tree.
+        /// </summary>
+        /// <param name="items">The items that should be added to this node</param>
+        /// <param name="comparer">The comparer used to compare two items.</param>
+        public IntervalTreeNode(IList<RangeValuePair<TKey, TValue>> items, IComparer<TKey> comparer)
+        {
+            this.comparer = comparer ?? Comparer<TKey>.Default;
+
+            // first, find the median
+            var endPoints = new List<TKey>(items.Count * 2);
+            foreach (var item in items)
+            {
+                endPoints.Add(item.From);
+                endPoints.Add(item.To);
+            }
+            endPoints.Sort(this.comparer);
+
+            // the median is used as center value
+            if (endPoints.Count > 0)
+            {
+                center = endPoints[endPoints.Count / 2];
+            }
+
+            var inner = new List<RangeValuePair<TKey, TValue>>();
+            var left = new List<RangeValuePair<TKey, TValue>>();
+            var right = new List<RangeValuePair<TKey, TValue>>();
+
+            // iterate over all items
+            // if the range of an item is completely left of the center, add it to the left items
+            // if it is on the right of the center, add it to the right items
+            // otherwise (range overlaps the center), add the item to this node's items
+            foreach (var o in items)
+            {
+                if (this.comparer.Compare(o.To, center) < 0)
+                    left.Add(o);
+                else if (this.comparer.Compare(o.From, center) > 0)
+                    right.Add(o);
+                else
+                    inner.Add(o);
+            }
+
+            // sort the items, this way the query is faster later on
+            if (inner.Count > 0)
+            {
+                if (inner.Count > 1)
+                    inner.Sort(this);
+                this.items = inner.ToArray();
+            }
+            else
+            {
+                this.items = null;
+            }
+
+            // create left and right nodes, if there are any items
+            if (left.Count > 0)
+                leftNode = new IntervalTreeNode<TKey, TValue>(left, this.comparer);
+            if (right.Count > 0)
+                rightNode = new IntervalTreeNode<TKey, TValue>(right, this.comparer);
+        }
+
+        /// <summary>
+        /// Performs a point query with a single value.
+        /// All items with overlapping ranges are returned.
+        /// </summary>
+        public IEnumerable<TValue> Query(TKey value)
+        {
+            var results = new List<TValue>();
+
+            // If the node has items, check for leaves containing the value.
+            if (items != null)
+            {
+                foreach (var o in items)
+                {
+                    if (comparer.Compare(o.From, value) > 0)
+                        break;
+                    else if (comparer.Compare(value, o.From) >= 0 && comparer.Compare(value, o.To) <= 0)
+                    {
+                        results.Add(o.Value);
+                    }
+                }
+            }
+
+            // go to the left or go to the right of the tree, depending
+            // where the query value lies compared to the center
+            var centerComp = comparer.Compare(value, center);
+            if (leftNode != null && centerComp < 0)
+                results.AddRange(leftNode.Query(value));
+            else if (rightNode != null && centerComp > 0)
+                results.AddRange(rightNode.Query(value));
+
+            return results;
+        }
+
+        /// <summary>
+        /// Performs a range query.
+        /// All items with overlapping ranges are returned.
+        /// </summary>
+        public IEnumerable<TValue> Query(TKey from, TKey to)
+        {
+            var results = new List<TValue>();
+
+            // If the node has items, check for leaves intersecting the range.
+            if (items != null)
+            {
+                foreach (var o in items)
+                {
+                    if (comparer.Compare(o.From, to) > 0)
+                        break;
+                    else if (comparer.Compare(to, o.From) >= 0 && comparer.Compare(from, o.To) <= 0)
+                        results.Add(o.Value);
+                }
+            }
+
+            // go to the left or go to the right of the tree, depending
+            // where the query value lies compared to the center
+            if (leftNode != null && comparer.Compare(from, center) < 0)
+                results.AddRange(leftNode.Query(from, to));
+            if (rightNode != null && comparer.Compare(to, center) > 0)
+                results.AddRange(rightNode.Query(from, to));
+
+            return results;
+        }
+
+        /// <summary>
+        /// Returns less than 0 if this range's From is less than the other, greater than 0 if greater.
+        /// If both are equal, the comparison of the To values is returned.
+        /// 0 if both ranges are equal.
+        /// </summary>
+        /// <param name="x">The first item.</param>
+        /// <param name="y">The other item.</param>
+        /// <returns></returns>
+        int IComparer<RangeValuePair<TKey, TValue>>.Compare(RangeValuePair<TKey, TValue> x, RangeValuePair<TKey, TValue> y)
+        {
+            var fromComp = comparer.Compare(x.From, y.From);
+            if (fromComp == 0)
+                return comparer.Compare(x.To, y.To);
+            return fromComp;
+        }
+
+        public TKey Max
+        {
+            get
+            {
+                if (rightNode != null)
+                    return rightNode.Max;
+                else if (items != null)
+                    return items.Max(i => i.To);
+                else
+                    return default(TKey);
+            }
+        }
+
+        public TKey Min
+        {
+            get
+            {
+                if (leftNode != null)
+                    return leftNode.Min;
+                else if (items != null)
+                    return items.Min(i => i.From);
+                else
+                    return default(TKey);
+            }
+        }
+    }
+}
