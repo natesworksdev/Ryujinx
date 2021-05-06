a23881406464aa7d1f92b5d1db9caa1f448484c1
Twilight-Struggle <pikupiken@yahoo.co.jp>
2021-05-07 01:00:21 +0900

    Assign _jitUnwindWindows null when platform is not windows.

--- ARMeilleure/Translation/Cache/JitCache.cs
+++ ARMeilleure/Translation/Cache/JitCache.cs
@@ -22,7 +22,7 @@ namespace ARMeilleure.Translation.Cache
 
         private readonly List<CacheEntry> _cacheEntries = new List<CacheEntry>();
 
-        private JitUnwindWindows _jitUnwindWindows;
+        private JitUnwindWindows _jitUnwindWindows = null;
 
         private readonly object _lock = new object();
 
@@ -171,7 +171,7 @@ namespace ARMeilleure.Translation.Cache
 
         public void Dispose()
         {
-            _jitUnwindWindows.Dispose();
+            _jitUnwindWindows?.Dispose();
             _jitRegion.Dispose();
         }
     }
