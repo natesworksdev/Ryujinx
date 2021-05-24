5df8c7ef04951587c43eaed9d6b6f736967c0f80
riperiperi <rhy3756547@hotmail.com>
2021-05-24 20:04:08 +0100

    Fixup to work with new renderer stuff

--- Ryujinx.Graphics.GAL/Multithreading/ThreadedWindow.cs
+++ Ryujinx.Graphics.GAL/Multithreading/ThreadedWindow.cs
@@ -1,6 +1,7 @@
 ﻿using Ryujinx.Graphics.GAL.Multithreading.Commands.Window;
 using Ryujinx.Graphics.GAL.Multithreading.Model;
 using Ryujinx.Graphics.GAL.Multithreading.Resources;
+using System;
 
 namespace Ryujinx.Graphics.GAL.Multithreading
 {
@@ -15,14 +16,14 @@ namespace Ryujinx.Graphics.GAL.Multithreading
             _impl = window;
         }
 
-        public void Present(ITexture texture, ImageCrop crop)
+        public void Present(ITexture texture, ImageCrop crop, Action swapBuffersCallback)
         {
             // If there's already a frame in the pipeline, wait for it to be presented first.
             // This is a multithread rate limit - we can't be more than one frame behind the command queue.
 
             _renderer.WaitForFrame();
-            _renderer.New<WindowPresentCommand>().Set(new TableRef<ThreadedTexture>(_renderer, texture as ThreadedTexture), crop);
-            _renderer.InvokeCommand();
+            _renderer.New<WindowPresentCommand>().Set(new TableRef<ThreadedTexture>(_renderer, texture as ThreadedTexture), crop, new TableRef<Action>(_renderer, swapBuffersCallback));
+            _renderer.QueueCommand();
         }
 
         public void SetSize(int width, int height)
