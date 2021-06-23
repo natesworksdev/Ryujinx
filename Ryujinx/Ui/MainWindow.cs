dd7d343bd209894f231a12f566d9646d27a82733
ooa113y <13thSlayer@gmail.com>
2021-06-24 02:30:54 +0300

    Simplify searchEqualFunc
    Co-authored-by: Ac_K <Acoustik666@gmail.com>

--- Ryujinx/Ui/MainWindow.cs
+++ Ryujinx/Ui/MainWindow.cs
@@ -226,7 +226,7 @@ namespace Ryujinx.Ui
 
             _gameTable.EnableSearch = true;
             _gameTable.SearchColumn = 2;
-            _gameTable.SearchEqualFunc = (model, col, key, iter) => {
+            _gameTable.SearchEqualFunc = (model, col, key, iter) => !((string)model.GetValue(iter, col)).Contains(key, StringComparison.InvariantCultureIgnoreCase);
                 string NameAndTitleId = (string) model.GetValue(iter, col);
                 return !NameAndTitleId.Contains(key, StringComparison.InvariantCultureIgnoreCase);
             };
