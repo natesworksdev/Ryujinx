6b72d03ae141cc1436c01dd5677d0f5870dcabc8
gdkchan <gab.dark.100@gmail.com>
2021-09-04 02:03:56 -0300

    New depth-stencil blit method for AMD

--- Ryujinx.Graphics.Vulkan/VulkanConfiguration.cs
+++ Ryujinx.Graphics.Vulkan/VulkanConfiguration.cs
@@ -7,5 +7,7 @@
         public const bool UseFastBufferUpdates = true;
         public const bool UseGranularBufferTracking = false;
         public const bool UseSlowSafeBlitOnAmd = true;
+
+        public const bool ForceD24S8Unsupported = false;
     }
 }
