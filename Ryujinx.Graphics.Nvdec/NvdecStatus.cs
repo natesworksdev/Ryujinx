e300cc3b2510e98d47e33624dd0679c48038525d
gdkchan <gab.dark.100@gmail.com>
2021-09-21 14:37:14 -0300

    Silence warnings

--- Ryujinx.Graphics.Nvdec/NvdecStatus.cs
+++ Ryujinx.Graphics.Nvdec/NvdecStatus.cs
@@ -4,11 +4,13 @@ namespace Ryujinx.Graphics.Nvdec
 {
     struct NvdecStatus
     {
+#pragma warning disable CS0649
         public uint MbsCorrectlyDecoded;
         public uint MbsInError;
         public uint Reserved;
         public uint ErrorStatus;
         public FrameStats Stats;
         public uint SliceHeaderErrorCode;
+#pragma warning restore CS0649
     }
 }
