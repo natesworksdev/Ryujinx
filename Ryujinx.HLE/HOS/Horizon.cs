f643d991947f217206243d8292ad2641127dfe6a
Alex Barney <thealexbarney@gmail.com>
2021-12-16 22:07:05 -0700

    Update to LibHac v0.14.3

--- Ryujinx.HLE/HOS/Horizon.cs
+++ Ryujinx.HLE/HOS/Horizon.cs
@@ -1,3 +1,4 @@
+using LibHac.Common;
 using LibHac.Common.Keys;
 using LibHac.Fs;
 using LibHac.Fs.Shim;
@@ -304,9 +305,9 @@ namespace Ryujinx.HLE.HOS
 
         public void LoadKip(string kipPath)
         {
-            using IStorage kipFile = new LocalStorage(kipPath, FileAccess.Read);
+            using var kipFile = new SharedRef<IStorage>(new LocalStorage(kipPath, FileAccess.Read));
 
-            ProgramLoader.LoadKip(KernelContext, new KipExecutable(kipFile));
+            ProgramLoader.LoadKip(KernelContext, new KipExecutable(in kipFile));
         }
 
         public void ChangeDockedModeState(bool newState)
