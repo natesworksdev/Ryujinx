3da099ac8d2efffbe863713b22c295efabf53096
JavidPack <javidpack@gmail.com>
2022-01-20 04:01:14 -0700

    Address feedback

--- Ryujinx.HLE/HOS/Services/Account/Acc/AccountManager.cs
+++ Ryujinx.HLE/HOS/Services/Account/Acc/AccountManager.cs
@@ -3,6 +3,7 @@ using LibHac.Common;
 using LibHac.Fs;
 using LibHac.Fs.Shim;
 using Ryujinx.Common;
+using Ryujinx.Common.Logging;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -25,7 +26,7 @@ namespace Ryujinx.HLE.HOS.Services.Account.Acc
 
         public UserProfile LastOpenedUser { get; private set; }
 
-        public AccountManager(HorizonClient horizonClient, string CommandLineProfile = null)
+        public AccountManager(HorizonClient horizonClient, string initialProfileName = null)
         {
             _horizonClient = horizonClient;
 
@@ -44,11 +45,11 @@ namespace Ryujinx.HLE.HOS.Services.Account.Acc
             else
             {
                 UserId commandLineUserProfileOverride = default; 
-                if(!string.IsNullOrEmpty(CommandLineProfile))
+                if (!string.IsNullOrEmpty(initialProfileName))
                 { 
-                    commandLineUserProfileOverride = _profiles.FirstOrDefault(x => x.Value.Name == CommandLineProfile).Value?.UserId ?? default;
-                    if(commandLineUserProfileOverride.IsNull)
-                        Common.Logging.Logger.Warning?.Print(Common.Logging.LogClass.Application, $"The command line specified profile named '{CommandLineProfile}' was not found");
+                    commandLineUserProfileOverride = _profiles.Values.FirstOrDefault(x => x.Name == initialProfileName)?.UserId ?? default;
+                    if (commandLineUserProfileOverride.IsNull)
+                        Logger.Warning?.Print(LogClass.Application, $"The command line specified profile named '{initialProfileName}' was not found");
                 }
                 OpenUser(commandLineUserProfileOverride.IsNull ? _accountSaveDataManager.LastOpened : commandLineUserProfileOverride);
             }
