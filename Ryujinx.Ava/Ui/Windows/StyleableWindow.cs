374e5e947dab25bcbd44495fc32b9a94218552f4
Emmanuel Hansen <emmausssss@gmail.com>
2022-02-07 09:21:29 +0000

    apply core window style

--- Ryujinx.Ava/Ui/Windows/StyleableWindow.cs
+++ Ryujinx.Ava/Ui/Windows/StyleableWindow.cs
@@ -2,6 +2,7 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using FluentAvalonia.Core.ApplicationModel;
 using FluentAvalonia.UI.Controls;
@@ -11,15 +12,14 @@ using System.Reflection;
 
 namespace Ryujinx.Ava.Ui.Windows
 {
-    public class StyleableWindow : Window
+    public class StyleableWindow : CoreWindow
     {
         public ContentDialog ContentDialog { get; private set; }
+        public IBitmap IconImage { get; set; }
 
         public StyleableWindow()
         {
-            ExtendClientAreaToDecorationsHint = false;
-            TransparencyLevelHint             = WindowTransparencyLevel.None;
-
+            ExtendClientAreaToDecorationsHint = true;
             this.GetObservable(WindowStateProperty)
                 .Subscribe(x =>
                 {
@@ -27,21 +27,13 @@ namespace Ryujinx.Ava.Ui.Windows
                     PseudoClasses.Set(":fullscreen", x == WindowState.FullScreen);
                 });
 
-            this.GetObservable(IsExtendedIntoWindowDecorationsProperty)
-                .Subscribe(x =>
-                {
-                    if (!x)
-                    {
-                        TransparencyLevelHint = WindowTransparencyLevel.None;
-                        SystemDecorations     = SystemDecorations.Full;
-                    }
-                });
-
             WindowStartupLocation = WindowStartupLocation.CenterOwner;
 
             Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Ryujinx.Ava.Assets.Images.Logo_Ryujinx.png");
 
             Icon = new WindowIcon(stream);
+            stream.Position = 0;
+            IconImage = new Bitmap(stream);
         }
 
         public void LoadDialog()
@@ -52,6 +44,7 @@ namespace Ryujinx.Ava.Ui.Windows
         protected override void OnOpened(EventArgs e)
         {
             base.OnOpened(e);
+            SetTitleBar(this);
             ContentDialog = this.FindControl<ContentDialog>("ContentDialog");
         }
 
@@ -60,6 +53,20 @@ namespace Ryujinx.Ava.Ui.Windows
             base.OnApplyTemplate(e);
 
             ExtendClientAreaChromeHints = ExtendClientAreaChromeHints.PreferSystemChrome | ExtendClientAreaChromeHints.OSXThickTitleBar;
+        }
+        
+        private void SetTitleBar(CoreWindow cw)
+        {
+            var titleBar = cw.TitleBar;
+            if (titleBar != null)
+            {
+                titleBar.ExtendViewIntoTitleBar = true;
+                if (this.FindControl<Grid>("TitleBarHost") is Grid g)
+                {
+                    cw.SetTitleBar(g);
+                    g.Margin = new Thickness(0, 0, titleBar.SystemOverlayRightInset, 0);
+                }
+            }
         }
     }
 }
