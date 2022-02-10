d4fe1ee5a80b7cd232c1fa3ec45e91b6e044fdc3
merry <git@mary.rs>
2022-02-10 17:15:31 +0000

    Tests: Prepare for thumb tests

---
+++ Ryujinx.Tests/Cpu/CpuTestThumb.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Ryujinx.Tests.Cpu
+{
+    [Category("Thumb")]
+    public sealed class CpuTestThumb : CpuTest32
+    {
+        private const int RndCnt = 2;
+    }
+}
