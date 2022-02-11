06f9a3dc605a4eb38d85c100d9ccb3c5e342cafe
merry <git@mary.rs>
2022-02-11 21:27:42 +0000

    T16: Implement SVC

---
+++ ARMeilleure/Decoders/OpCodeT16Exception.cs
@@ -0,0 +1,14 @@
+﻿namespace ARMeilleure.Decoders
+{
+    class OpCodeT16Exception : OpCodeT16, IOpCode32Exception
+    {
+        public int Id { get; }
+
+        public new static OpCode Create(InstDescriptor inst, ulong address, int opCode, bool inITBlock) => new OpCodeT16Exception(inst, address, opCode, inITBlock);
+
+        public OpCodeT16Exception(InstDescriptor inst, ulong address, int opCode, bool inITBlock) : base(inst, address, opCode, inITBlock)
+        {
+            Id = opCode & 0xFF;
+        }
+    }
+}
