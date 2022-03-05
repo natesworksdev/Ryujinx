2bc51aa00dcaa610a474bac01c07f71a73e9ac03
merry <git@mary.rs>
2022-03-05 16:39:11 +0000

    CpuTestAluImm32: Add more opcodes

--- Ryujinx.Tests/Cpu/CpuTestAluImm32.cs
+++ Ryujinx.Tests/Cpu/CpuTestAluImm32.cs
@@ -11,16 +11,24 @@ namespace Ryujinx.Tests.Cpu
 #if AluRs32
 
 #region "ValueSource (Opcodes)"
-        private static uint[] _Adc_Adcs_Rsc_Rscs_Sbc_Sbcs_()
+        private static uint[] _opcodes()
         {
             return new uint[]
             {
                 0xe2a00000u, // ADC R0, R0, #0
                 0xe2b00000u, // ADCS R0, R0, #0
+                0xe2800000u, // ADD R0, R0, #0
+                0xe2900000u, // ADDS R0, R0, #0
+                0xe3c00000u, // BIC R0, R0, #0
+                0xe3d00000u, // BICS R0, R0, #0
+                0xe2600000u, // RSB R0, R0, #0
+                0xe2700000u, // RSBS R0, R0, #0
                 0xe2e00000u, // RSC R0, R0, #0
                 0xe2f00000u, // RSCS R0, R0, #0
                 0xe2c00000u, // SBC R0, R0, #0
-                0xe2d00000u  // SBCS R0, R0, #0
+                0xe2d00000u, // SBCS R0, R0, #0
+                0xe2400000u, // SUB R0, R0, #0
+                0xe2500000u, // SUBS R0, R0, #0
             };
         }
 #endregion
@@ -29,12 +37,12 @@ namespace Ryujinx.Tests.Cpu
         private const int RndCntAmount = 2;
 
         [Test, Pairwise]
-        public void Adc_Adcs_Rsc_Rscs_Sbc_Sbcs([ValueSource("_Adc_Adcs_Rsc_Rscs_Sbc_Sbcs_")] uint opcode,
-                                               [Values(0u, 13u)] uint rd,
-                                               [Values(1u, 13u)] uint rn,
-                                               [Random(RndCnt)] uint imm,
-                                               [Random(RndCnt)] uint wn,
-                                               [Values(true, false)] bool carryIn)
+        public void TestCpuTestAluImm32([ValueSource("_opcodes")] uint opcode,
+                                        [Values(0u, 13u)] uint rd,
+                                        [Values(1u, 13u)] uint rn,
+                                        [Random(RndCnt)] uint imm,
+                                        [Random(RndCnt)] uint wn,
+                                        [Values(true, false)] bool carryIn)
         {
             opcode |= ((imm & 0xfff) << 0) | ((rn & 15) << 16) | ((rd & 15) << 12);
 
