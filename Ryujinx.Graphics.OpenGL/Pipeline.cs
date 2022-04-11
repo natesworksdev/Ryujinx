0bc693c89406400264d557ced51073a70e05a94f
gdk <gab.dark.100@gmail.com>
2022-04-11 18:21:03 -0300

    _unit0Sampler should be set only for binding 0

--- Ryujinx.Graphics.OpenGL/Pipeline.cs
+++ Ryujinx.Graphics.OpenGL/Pipeline.cs
@@ -1220,7 +1220,11 @@ namespace Ryujinx.Graphics.OpenGL
             Sampler glSampler = (Sampler)sampler;
 
             glSampler?.Bind(binding);
-            _unit0Sampler = glSampler;
+
+            if (binding == 0)
+            {
+                _unit0Sampler = glSampler;
+            }
         }
 
 
