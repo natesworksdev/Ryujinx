a729f5ceeab6a4adb0d8faab0411e8af09f7ece1
gdk <gab.dark.100@gmail.com>
2022-04-12 17:28:04 -0300

    Raise textures limit to 64 on Vulkan

--- Ryujinx.Graphics.Vulkan/Constants.cs
+++ Ryujinx.Graphics.Vulkan/Constants.cs
@@ -10,7 +10,7 @@
         public const int MaxShaderStages = 5;
         public const int MaxUniformBuffersPerStage = 18;
         public const int MaxStorageBuffersPerStage = 16;
-        public const int MaxTexturesPerStage = 32;
+        public const int MaxTexturesPerStage = 64;
         public const int MaxImagesPerStage = 8;
         public const int MaxUniformBufferBindings = MaxUniformBuffersPerStage * MaxShaderStages;
         public const int MaxStorageBufferBindings = MaxStorageBuffersPerStage * MaxShaderStages;
