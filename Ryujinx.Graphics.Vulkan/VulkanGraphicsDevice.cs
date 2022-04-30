daaa7fda11dc15353a4f5a723dee310ae3150164
gdk <gab.dark.100@gmail.com>
2022-04-29 23:15:47 -0300

    Do not report R4G4 format as supported on Vulkan
    It was causing mario head to become white on Super Mario 64 (???)

--- Ryujinx.Graphics.Vulkan/VulkanGraphicsDevice.cs
+++ Ryujinx.Graphics.Vulkan/VulkanGraphicsDevice.cs
@@ -311,7 +311,7 @@ namespace Ryujinx.Graphics.Vulkan
                 supportsAstcCompression: features.TextureCompressionAstcLdr,
                 supports3DTextureCompression: true,
                 supportsBgraFormat: true,
-                supportsR4G4Format: FormatCapabilities.FormatSupports(GAL.Format.R4G4Unorm, FormatFeatureFlags.FormatFeatureSampledImageBit),
+                supportsR4G4Format: false,
                 supportsFragmentShaderInterlock: SupportsFragmentShaderInterlock,
                 supportsFragmentShaderOrderingIntel: false,
                 supportsGeometryShaderPassthrough: SupportsGeometryShaderPassthrough,
