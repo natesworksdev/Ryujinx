a4968aa8ed02bf9aae614e5aa16ab2b05fb9fdf1
gdk <gab.dark.100@gmail.com>
2022-05-02 17:37:19 -0300

    Make RO hold a reference to the guest process memory manager to avoid early disposal

--- Ryujinx.Cpu/MemoryManagerHostMapped.cs
+++ Ryujinx.Cpu/MemoryManagerHostMapped.cs
@@ -5,7 +5,6 @@ using Ryujinx.Memory.Range;
 using Ryujinx.Memory.Tracking;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
