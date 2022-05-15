iczero/notryujinx

add settings windows and children views

Ryujinx.Ava/Ui/Applet/AvaHostUiHandler.cs
@@ -45,13 +45,54 @@ namespace Ryujinx.Ava.Ui.Applet
 
         public bool DisplayMessageDialog(string title, string message)
         {
-            // TODO
-            Dispatcher.UIThread.InvokeAsync(() =>
+            ManualResetEvent dialogCloseEvent = new(false);
+
+            bool okPressed = false;
+
+            Dispatcher.UIThread.Post(async () =>
             {
-                ContentDialogHelper.ShowNotAvailableMessage(_parent);
+                try
+                {
+                    ManualResetEvent deferEvent = new(false);
+
+                    bool opened = false;
+
+                    UserResult response = await ContentDialogHelper.ShowDeferredContentDialog(_parent, title, message, "",
+                        LocaleManager.Instance["DialogOpenSettingsWindow"], "", "Close", 0xF4A3, deferEvent,
+                       async (window) =>
+                       {
+                           if (opened)
+                           {
+                               return;
+                           }
+
+                           opened = true;
+
+                           _parent.SettingsWindow = new SettingsWindow(_parent.VirtualFileSystem, _parent.ContentManager);
+
+                           await _parent.SettingsWindow.ShowDialog(window);
+
+                           opened = false;
+                       });
+
+                    if (response == UserResult.Ok)
+                    {
+                        okPressed = true;
+                    }
+
+                    dialogCloseEvent.Set();
+                }
+                catch (Exception ex)
+                {
+                    ContentDialogHelper.CreateErrorDialog(_parent, string.Format(LocaleManager.Instance["DialogMessageDialogErrorExceptionMessage"], ex));
+
+                    dialogCloseEvent.Set();
+                }
             });
 
-            return true;
+            dialogCloseEvent.WaitOne();
+
+            return okPressed;
         }
 
         public bool DisplayInputDialog(SoftwareKeyboardUiArgs args, out string userText)

natesworksdev/Ryujinx

Ryujinx.Ava/Ui/Applet/AvaHostUiHandler.cs
@@ -45,13 +45,54 @@ namespace Ryujinx.Ava.Ui.Applet
 
         public bool DisplayMessageDialog(string title, string message)
         {
-            // TODO : Show controller applet. Needs settings window to be implemented.
-            Dispatcher.UIThread.InvokeAsync(() =>
+            ManualResetEvent dialogCloseEvent = new(false);
+
+            bool okPressed = false;
+
+            Dispatcher.UIThread.InvokeAsync(async () =>
             {
-                ContentDialogHelper.ShowNotAvailableMessage(_parent);
+                try
+                {
+                    ManualResetEvent deferEvent = new(false);
+
+                    bool opened = false;
+
+                    UserResult response = await ContentDialogHelper.ShowDeferredContentDialog(_parent, title, message, "",
+                        LocaleManager.Instance["DialogOpenSettingsWindow"], "", "Close", 0xF4A3, deferEvent,
+                       async (window) =>
+                       {
+                           if (opened)
+                           {
+                               return;
+                           }
+
+                           opened = true;
+
+                           _parent.SettingsWindow = new SettingsWindow(_parent.VirtualFileSystem, _parent.ContentManager);
+
+                           await _parent.SettingsWindow.ShowDialog(window);
+
+                           opened = false;
+                       });
+
+                    if (response == UserResult.Ok)
+                    {
+                        okPressed = true;
+                    }
+
+                    dialogCloseEvent.Set();
+                }
+                catch (Exception ex)
+                {
+                    ContentDialogHelper.CreateErrorDialog(_parent, string.Format(LocaleManager.Instance["DialogMessageDialogErrorExceptionMessage"], ex));
+
+                    dialogCloseEvent.Set();
+                }
             });
 
-            return true;
+            dialogCloseEvent.WaitOne();
+
+            return okPressed;
         }
 
         public bool DisplayInputDialog(SoftwareKeyboardUiArgs args, out string userText)