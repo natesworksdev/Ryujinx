2d221438c27867619fa6add152730bb22de368d3
Mary <mary@mary.zone>
2022-05-26 00:35:45 +0200

    Add missing Box type

---
+++ Ryujinx.Common/Memory/Box.cs
@@ -0,0 +1,14 @@
+﻿namespace Ryujinx.Common.Memory
+{
+    public class Box<T> where T : unmanaged
+    {
+        private T _data;
+
+        public ref T Data => ref _data;
+
+        public Box()
+        {
+            _data = new T();
+        }
+    }
+}
