17b0e9edf8badfc25645052a1013b46947d67724
riperiperi <rhy3756547@hotmail.com>
2022-06-11 18:33:03 +0100

    Use RobustBufferAccess on NVIDIA gpus
    Avoids the SMO waterfall triangle on older NVIDIA gpus.

--- Ryujinx.Graphics.Vulkan/Vendor.cs
+++ Ryujinx.Graphics.Vulkan/Vendor.cs
@@ -8,4 +8,19 @@ namespace Ryujinx.Graphics.Vulkan
         Qualcomm,
         Unknown
     }
+
+    static class VendorUtils
+    {
+        public static Vendor FromId(uint id)
+        {
+            return id switch
+            {
+                0x1002 => Vendor.Amd,
+                0x10DE => Vendor.Nvidia,
+                0x8086 => Vendor.Intel,
+                0x5143 => Vendor.Qualcomm,
+                _ => Vendor.Unknown
+            };
+        }
+    }
 }
