d3301444b808bcacaf93a23e14cb320a0e6ed336
Liam <byteslice@airmail.cc>
2022-06-30 20:48:11 -0400

    loader: improve handling of raw NCAs for multi-program applications

--- Ryujinx.HLE/HOS/ApplicationLoader.cs
+++ Ryujinx.HLE/HOS/ApplicationLoader.cs
@@ -512,9 +512,7 @@ namespace Ryujinx.HLE.HOS
             // Don't create save data for system programs.
             if (TitleId != 0 && (TitleId < SystemProgramId.Start.Value || TitleId > SystemAppletId.End.Value))
             {
-                // Multi-program applications can technically use any program ID for the main program, but in practice they always use 0 in the low nibble.
-                // We'll know if this changes in the future because stuff will get errors when trying to mount the correct save.
-                EnsureSaveData(new ApplicationId(TitleId & ~0xFul));
+                EnsureSaveData(new ApplicationId(TitleId));
             }
 
             LoadExeFs(codeFs, metaData);
