5714fdbb826ec11ca513333b6b29e13f808e715b
gdk <gab.dark.100@gmail.com>
2022-07-29 00:53:36 -0300

    Remove RectangleF that is now unused

--- Ryujinx.Graphics.GAL/Rectangle.cs
+++ Ryujinx.Graphics.GAL/Rectangle.cs
@@ -2,16 +2,16 @@ namespace Ryujinx.Graphics.GAL
 {
     public struct Rectangle<T> where T : unmanaged
     {
-        public T X      { get; }
-        public T Y      { get; }
-        public T Width  { get; }
+        public T X { get; }
+        public T Y { get; }
+        public T Width { get; }
         public T Height { get; }
 
         public Rectangle(T x, T y, T width, T height)
         {
-            X      = x;
-            Y      = y;
-            Width  = width;
+            X = x;
+            Y = y;
+            Width = width;
             Height = height;
         }
     }
