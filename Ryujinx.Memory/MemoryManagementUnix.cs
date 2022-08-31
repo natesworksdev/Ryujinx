c87fcffb6e2a50d24d0502ae3ff9eb70d45257c1
gdk <gab.dark.100@gmail.com>
2022-08-30 23:41:36 -0300

    Remove throw

--- Ryujinx.Memory/MemoryManagementUnix.cs
+++ Ryujinx.Memory/MemoryManagementUnix.cs
@@ -177,12 +177,7 @@ namespace Ryujinx.Memory
 
         public static void UnmapView(IntPtr location, ulong size)
         {
-            IntPtr ptr = mmap(location, size, MmapProts.PROT_NONE, MmapFlags.MAP_FIXED | MmapFlags.MAP_PRIVATE | MmapFlags.MAP_ANONYMOUS | MmapFlags.MAP_NORESERVE, -1, 0);
-
-            if (ptr == new IntPtr(-1L))
-            {
-                throw new Exception("UnmapView failed");
-            }
+            mmap(location, size, MmapProts.PROT_NONE, MmapFlags.MAP_FIXED | MmapFlags.MAP_PRIVATE | MmapFlags.MAP_ANONYMOUS | MmapFlags.MAP_NORESERVE, -1, 0);
         }
     }
 }
