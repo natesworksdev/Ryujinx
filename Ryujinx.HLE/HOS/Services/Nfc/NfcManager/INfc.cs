9ac50cb1bc506c336e5d97547b3a90c52fc81641
Carl Ouellette <carlouellette2@gmail.com>
2022-09-06 17:35:52 -0400

    Manage state of NfcManager
    Very basic state management but works with Hyrule Warriors Definitive Edition. Partially fixes #2122

--- Ryujinx.HLE/HOS/Services/Nfc/NfcManager/INfc.cs
+++ Ryujinx.HLE/HOS/Services/Nfc/NfcManager/INfc.cs
@@ -5,6 +5,7 @@ namespace Ryujinx.HLE.HOS.Services.Nfc.NfcManager
     class INfc : IpcService
     {
         private NfcPermissionLevel _permissionLevel;
+        private State _state = State.NonInitialized;
 
         public INfc(NfcPermissionLevel permissionLevel)
         {
@@ -17,6 +18,30 @@ namespace Ryujinx.HLE.HOS.Services.Nfc.NfcManager
         public ResultCode Initialize(ServiceCtx context)
         {
             Logger.Stub?.PrintStub(LogClass.ServiceNfc, new { _permissionLevel });
+
+            _state = State.Initialized;
+
+            return ResultCode.Success;
+        }
+
+        [CommandHipc(1)]
+        [CommandHipc(401)] // 4.0.0+
+        // Initialize()
+        public ResultCode Finalize(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceNfc, new { _permissionLevel });
+
+            _state = State.NonInitialized;
+
+            return ResultCode.Success;
+        }
+
+        [CommandHipc(2)]
+        [CommandHipc(402)] // 4.0.0+
+        // GetState() -> u32
+        public ResultCode GetState(ServiceCtx context) 
+        {
+            context.ResponseData.Write((int)_state);
 
             return ResultCode.Success;
         }
---
+++ Ryujinx.HLE/HOS/Services/Nfc/NfcManager/Types/State.cs
@@ -0,0 +1,8 @@
+namespace Ryujinx.HLE.HOS.Services.Nfc.NfcManager
+{
+    enum State
+    {
+        NonInitialized = 0,
+        Initialized    = 1
+    }
+}
