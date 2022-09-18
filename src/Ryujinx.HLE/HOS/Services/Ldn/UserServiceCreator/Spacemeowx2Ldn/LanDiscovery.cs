ba516959fa13ef54199ce69cf3918509f6d7362b
riperiperi <rhy3756547@hotmail.com>
2022-09-18 23:36:54 +0100

    Always force a network sync when updating AdvertiseData

--- src/Ryujinx.HLE/HOS/Services/Ldn/UserServiceCreator/Spacemeowx2Ldn/LanDiscovery.cs
+++ src/Ryujinx.HLE/HOS/Services/Ldn/UserServiceCreator/Spacemeowx2Ldn/LanDiscovery.cs
@@ -214,7 +214,7 @@ namespace Ryujinx.HLE.HOS.Services.Ldn.UserServiceCreator.Spacemeowx2Ldn
             {
                 if (NetworkInfo.Ldn.Nodes[0].IsConnected == 1)
                 {
-                    UpdateNodes();
+                    UpdateNodes(true);
                 }
             }
 
@@ -429,7 +429,7 @@ namespace Ryujinx.HLE.HOS.Services.Ldn.UserServiceCreator.Spacemeowx2Ldn
             return -1;
         }
 
-        protected void UpdateNodes()
+        protected void UpdateNodes(bool forceUpdate = false)
         {
             int countConnected = 1;
 
@@ -445,7 +445,7 @@ namespace Ryujinx.HLE.HOS.Services.Ldn.UserServiceCreator.Spacemeowx2Ldn
 
             Logger.Debug?.PrintMsg(LogClass.ServiceLdn, $"NetworkInfoNodeCount: {NetworkInfo.Ldn.NodeCount} | new NodeCount: {nodeCount}");
 
-            bool networkInfoChanged = NetworkInfo.Ldn.NodeCount != nodeCount;
+            bool networkInfoChanged = forceUpdate || NetworkInfo.Ldn.NodeCount != nodeCount;
 
             NetworkInfo.Ldn.NodeCount = nodeCount;
 
