e741eb16213929a8171621c991949fd11a73d281
LDj3SNuD <dvitiello@gmail.com>
2022-09-19 23:54:06 +0200

    De-magic Masks.
    Remove the Stride and Len flags; Fpsr.NZCV are A32 only, then moved to Fpscr: this leads to emitting less IR in reference to Get/Set Fpsr/Fpcr/Fpscr methods in reference to Mrs/Msr (A64) and Vmrs/Vmsr (A32) instructions.

--- ARMeilleure/State/FPSCR.cs
+++ ARMeilleure/State/FPSCR.cs
@@ -5,6 +5,11 @@ namespace ARMeilleure.State
     [Flags]
     public enum FPSCR : uint
     {
-        Mask = FPSR.Mask | FPCR.Mask
+        V = 1u << 28,
+        C = 1u << 29,
+        Z = 1u << 30,
+        N = 1u << 31,
+
+        Mask = N | Z | C | V | FPSR.Mask | FPCR.Mask // 0xFFC09F9Fu
     }
 }
