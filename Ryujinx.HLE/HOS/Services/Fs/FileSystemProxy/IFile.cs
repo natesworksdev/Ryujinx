8fba013a085ce9aa1ae6c5c56320373ef9dc49d5
gdk <gab.dark.100@gmail.com>
2022-10-04 15:01:53 -0300

    nit: Don't move this around

--- Ryujinx.HLE/HOS/Services/Fs/FileSystemProxy/IFile.cs
+++ Ryujinx.HLE/HOS/Services/Fs/FileSystemProxy/IFile.cs
@@ -19,14 +19,14 @@ namespace Ryujinx.HLE.HOS.Services.Fs.FileSystemProxy
         // Read(u32 readOption, u64 offset, u64 size) -> (u64 out_size, buffer<u8, 0x46, 0> out_buf)
         public ResultCode Read(ServiceCtx context)
         {
+            ulong bufferAddress = context.Request.ReceiveBuff[0].Position;
+            ulong bufferLen = context.Request.ReceiveBuff[0].Size;
+
             ReadOption readOption = context.RequestData.ReadStruct<ReadOption>();
             context.RequestData.BaseStream.Position += 4;
 
             long offset = context.RequestData.ReadInt64();
             long size   = context.RequestData.ReadInt64();
-
-            ulong bufferAddress = context.Request.ReceiveBuff[0].Position;
-            ulong bufferLen = context.Request.ReceiveBuff[0].Size;
 
             using (var region = context.Memory.GetWritableRegion(bufferAddress, (int)bufferLen, true))
             {
