8b560fb6d6a135f097f6ac6ab45730876cb623a7
Mary <mary@mary.zone>
2022-10-26 18:43:05 +0200

    time: Fix missing alignment enforcement in SteadyClockContext
    Fix games (like recent version of Smash) using time shared memory

--- Ryujinx.HLE/HOS/Services/Time/Types/SteadyClockContext.cs
+++ Ryujinx.HLE/HOS/Services/Time/Types/SteadyClockContext.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace Ryujinx.HLE.HOS.Services.Time.Types
 {
-    [StructLayout(LayoutKind.Sequential)]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     struct SteadyClockContext
     {
         public ulong   InternalOffset;
