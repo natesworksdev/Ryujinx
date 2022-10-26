332bf3318f771ed8c38824f51180ae6afefc6871
Mary <mary@mary.zone>
2022-10-26 09:54:51 +0200

    time: Fix missing alignment enforcement in SystemClockContext
    Fixes at least Smash

--- Ryujinx.HLE/HOS/Services/Time/Clock/Types/SteadyClockTimePoint.cs
+++ Ryujinx.HLE/HOS/Services/Time/Clock/Types/SteadyClockTimePoint.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 namespace Ryujinx.HLE.HOS.Services.Time.Clock
 {
-    [StructLayout(LayoutKind.Sequential)]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     struct SteadyClockTimePoint
     {
         public long    TimePoint;
