4957c819a905dcfb27ff446d4e0c629d717cbf07
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2022-11-27 16:27:05 -0400

    Start Games in Fullscreen Mode is now toggleable in the options menu

--- Ryujinx.Ava/Ui/ViewModels/MainWindowViewModel.cs
+++ Ryujinx.Ava/Ui/ViewModels/MainWindowViewModel.cs
@@ -551,6 +551,11 @@ namespace Ryujinx.Ava.Ui.ViewModels
                 OnPropertyChanged();
             }
         }
+        
+        public void ToggleStartGamesInFullscreen()
+        {
+            StartGamesInFullscreen = !StartGamesInFullscreen;
+        }
 
         public bool ShowConsole
         {
