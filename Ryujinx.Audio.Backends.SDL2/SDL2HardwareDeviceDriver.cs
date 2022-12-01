6f63526aacaacb0250d35e34ea75c6e1d29421ee
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2022-11-30 19:45:27 -0500

    Update SDL2HardwareDeviceDriver.cs

--- Ryujinx.Audio.Backends.SDL2/SDL2HardwareDeviceDriver.cs
+++ Ryujinx.Audio.Backends.SDL2/SDL2HardwareDeviceDriver.cs
@@ -117,6 +117,7 @@ namespace Ryujinx.Audio.Backends.SDL2
 
                 Logger.Error?.Print(LogClass.Application, 
                     $"SDL2 open audio device initialization failed with error \"{SDL_GetError()}\"");
+
                 return 0;
             }
 
