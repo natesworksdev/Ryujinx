fe6384f7953402e81adf576ef4e3e17f4d5e708e
RMED24 <81475204+RMED24@users.noreply.github.com>
2022-12-04 19:44:20 +0000

    Update distribution/linux/start.sh
    Co-authored-by: TSRBerry <20988865+TSRBerry@users.noreply.github.com>

--- distribution/linux/start.sh
+++ distribution/linux/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-SCRIPT_DIR=$(dirname $0)
+SCRIPT_DIR=$(dirname $(realpath $0))
+
 chmod +x "$SCRIPT_DIR/Ryujinx"
 
 DOTNET_EnableAlternateStackCheck=1 GDK_BACKEND=x11 "$SCRIPT_DIR/Ryujinx" "$@"
