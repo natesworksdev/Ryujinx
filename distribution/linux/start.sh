75ab7ba01e5c3461ae617e485acc0c62a9d98446
RMED24 <81475204+RMED24@users.noreply.github.com>
2022-12-04 19:32:52 +0000

    Remove notice from start.sh
    Co-authored-by: Mary-nyan <thog@protonmail.com>

--- distribution/linux/start.sh
+++ distribution/linux/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-# We recommend using Pine-jinx if you want the game to be optimised and installed for your system. This is mostly for PR builds and people who want a portable install.
 # It is also recommended to run start.sh in the terminal, although this isn't required in order for it to operate
 SCRIPT_DIR=$(dirname $0)
 chmod +x "$SCRIPT_DIR/Ryujinx"
