3ba26e0762b95541350a4e9fca7fbcb10f3c67b6
RMED24 <81475204+RMED24@users.noreply.github.com>
2022-12-05 09:10:13 +0000

    Update Ryujinx.sh

--- distribution/linux/Ryujinx.sh
+++ distribution/linux/Ryujinx.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 
 chmod +x "$SCRIPT_DIR/Ryujinx"
 
-DOTNET_EnableAlternateStackCheck=1 GDK_BACKEND=x11 "$SCRIPT_DIR/Ryujinx" "$@"
+DOTNET_EnableAlternateStackCheck=1 "$SCRIPT_DIR/Ryujinx" "$@"
 
 exit
