75963bb394b4d373534f5a21111ff2ee59218cb7
RMED24 <81475204+RMED24@users.noreply.github.com>
2022-12-05 13:18:21 +0000

    Update Program.cs

--- Ryujinx/Program.cs
+++ Ryujinx/Program.cs
@@ -102,6 +102,7 @@ namespace Ryujinx
             if (OperatingSystem.IsLinux())
             {
                 XInitThreads();
+                Environment.SetEnvironmentVariable("GDK_BACKEND", "x11");
                 setenv("GDK_BACKEND", "x11", 1);
             }
 
