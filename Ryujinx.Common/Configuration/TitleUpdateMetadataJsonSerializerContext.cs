c6a755fbb690a2e85401157d431dd782727fe803
Andrey Sukharev <SukharevAndrey@users.noreply.github.com>
2022-12-09 02:03:06 +0300

    Use source generated json serializers in order to improve code trimming

---
+++ Ryujinx.Common/Configuration/TitleUpdateMetadataJsonSerializerContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ryujinx.Common.Configuration;
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(TitleUpdateMetadata))]
+public partial class TitleUpdateMetadataJsonSerializerContext : JsonSerializerContext
+{
+}
---
+++ Ryujinx.Common/Logging/LogEventJsonSerializerContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ryujinx.Common.Logging;
+
+[JsonSerializable(typeof(LogEventArgs))]
+internal partial class LogEventJsonSerializerContext : JsonSerializerContext
+{
+}
