756f1604deb4e5719cc59f0d9c3a5aeb27d31c6b
Andrey Sukharev <SukharevAndrey@users.noreply.github.com>
2022-12-10 02:35:10 +0300

    Use strongly typed github releases model to fetch updates instead of raw Newtonsoft.Json parsing

---
+++ Ryujinx.Ui.Common/Models/Github/GithubReleaseAssetJsonResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ryujinx.Ui.Common.Models.Github;
+
+public class GithubReleaseAssetJsonResponse
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("state")]
+    public string State { get; set; }
+    [JsonPropertyName("browser_download_url")]
+    public string BrowserDownloadUrl { get; set; }
+}
---
+++ Ryujinx.Ui.Common/Models/Github/GithubReleasesJsonSerializerContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ryujinx.Ui.Common.Models.Github;
+
+[JsonSerializable(typeof(GithubReleasesJsonResponse), GenerationMode = JsonSourceGenerationMode.Metadata)]
+public partial class GithubReleasesJsonSerializerContext : JsonSerializerContext
+{
+}
