19c54f88abf1b944cf63ae814b56ab2927746966
Isaac Marovitz <isaacryu@icloud.com>
2023-01-01 17:34:47 +0100

    Remove catch all

--- Ryujinx.Ava/UI/ViewModels/MainWindowViewModel.cs
+++ Ryujinx.Ava/UI/ViewModels/MainWindowViewModel.cs
@@ -1065,50 +1065,46 @@ namespace Ryujinx.Ava.UI.ViewModels
         {
             Dispatcher.UIThread.Post((() =>
             {
-                try
-                {
-                    ProgressMaximum = total;
-                    ProgressValue = current;
+                ProgressMaximum = total;
+                ProgressValue = current;
 
-                    switch (state)
-                    {
-                        case PtcLoadingState ptcState:
-                            CacheLoadStatus = $"{current} / {total}";
-                            switch (ptcState)
-                            {
-                                case PtcLoadingState.Start:
-                                case PtcLoadingState.Loading:
-                                    LoadHeading = LocaleManager.Instance["CompilingPPTC"];
-                                    IsLoadingIndeterminate = false;
-                                    break;
-                                case PtcLoadingState.Loaded:
-                                    LoadHeading = string.Format(LocaleManager.Instance["LoadingHeading"], TitleName);
-                                    IsLoadingIndeterminate = true;
-                                    CacheLoadStatus = "";
-                                    break;
-                            }
-                            break;
-                        case ShaderCacheLoadingState shaderCacheState:
-                            CacheLoadStatus = $"{current} / {total}";
-                            switch (shaderCacheState)
-                            {
-                                case ShaderCacheLoadingState.Start:
-                                case ShaderCacheLoadingState.Loading:
-                                    LoadHeading = LocaleManager.Instance["CompilingShaders"];
-                                    IsLoadingIndeterminate = false;
-                                    break;
-                                case ShaderCacheLoadingState.Loaded:
-                                    LoadHeading = string.Format(LocaleManager.Instance["LoadingHeading"], TitleName);
-                                    IsLoadingIndeterminate = true;
-                                    CacheLoadStatus = "";
-                                    break;
-                            }
-                            break;
-                        default:
-                            throw new ArgumentException($"Unknown Progress Handler type {typeof(T)}");
-                    }
+                switch (state)
+                {
+                    case PtcLoadingState ptcState:
+                        CacheLoadStatus = $"{current} / {total}";
+                        switch (ptcState)
+                        {
+                            case PtcLoadingState.Start:
+                            case PtcLoadingState.Loading:
+                                LoadHeading = LocaleManager.Instance["CompilingPPTC"];
+                                IsLoadingIndeterminate = false;
+                                break;
+                            case PtcLoadingState.Loaded:
+                                LoadHeading = string.Format(LocaleManager.Instance["LoadingHeading"], TitleName);
+                                IsLoadingIndeterminate = true;
+                                CacheLoadStatus = "";
+                                break;
+                        }
+                        break;
+                    case ShaderCacheLoadingState shaderCacheState:
+                        CacheLoadStatus = $"{current} / {total}";
+                        switch (shaderCacheState)
+                        {
+                            case ShaderCacheLoadingState.Start:
+                            case ShaderCacheLoadingState.Loading:
+                                LoadHeading = LocaleManager.Instance["CompilingShaders"];
+                                IsLoadingIndeterminate = false;
+                                break;
+                            case ShaderCacheLoadingState.Loaded:
+                                LoadHeading = string.Format(LocaleManager.Instance["LoadingHeading"], TitleName);
+                                IsLoadingIndeterminate = true;
+                                CacheLoadStatus = "";
+                                break;
+                        }
+                        break;
+                    default:
+                        throw new ArgumentException($"Unknown Progress Handler type {typeof(T)}");
                 }
-                catch (Exception) { }
             }));
         }
 
