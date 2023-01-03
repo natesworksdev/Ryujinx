9b403ecffffec498dde749c243a9d5637e6de1f0
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2023-01-03 18:02:25 -0500

    Update Ryujinx.Ava/UI/Views/Settings/SettingsSystemView.axaml.cs
    Co-authored-by: TSRBerry <20988865+TSRBerry@users.noreply.github.com>

--- Ryujinx.Ava/UI/Views/Settings/SettingsSystemView.axaml.cs
+++ Ryujinx.Ava/UI/Views/Settings/SettingsSystemView.axaml.cs
@@ -18,6 +18,7 @@ public partial class SettingsSystemView : UserControl
         
         FuncMultiValueConverter<string, string> converter = new(parts => string.Format("{0}  {1}   {2}", parts.ToArray()).Trim());
         MultiBinding tzMultiBinding = new() { Converter = converter };
+
         tzMultiBinding.Bindings.Add(new Binding("UtcDifference"));
         tzMultiBinding.Bindings.Add(new Binding("Location"));
         tzMultiBinding.Bindings.Add(new Binding("Abbreviation"));
