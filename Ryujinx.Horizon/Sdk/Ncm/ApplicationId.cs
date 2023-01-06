1d3164c0a54a0dd3e40efb72bd0f85bd863bf69c
Isaac Marovitz <isaacryu@icloud.com>
2023-01-06 17:15:50 -0500

    Remove Ncm types

--- Ryujinx.Horizon/Sdk/Ncm/ApplicationId.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace Ryujinx.Horizon.Sdk.Ncm
-{
-    [StructLayout(LayoutKind.Sequential, Size = 0x8, Pack = 0x8)]
-    struct ApplicationId
-    {
-    }
-}
--- Ryujinx.Horizon/Sdk/Ncm/StorageId.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace Ryujinx.Horizon.Sdk.Ncm
-{
-    [StructLayout(LayoutKind.Sequential, Size = 0x1, Pack = 0x1)]
-    struct StorageId
-    {
-    }
-}
