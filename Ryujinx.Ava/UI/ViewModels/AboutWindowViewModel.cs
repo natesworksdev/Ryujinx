dd8c94962f1b4e87c5e8d4408ba061a4bd31e315
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2023-01-06 19:01:55 -0500

    Update Ryujinx.Ava/UI/ViewModels/AboutWindowViewModel.cs
    Co-authored-by: Ac_K <Acoustik666@gmail.com>

--- Ryujinx.Ava/UI/ViewModels/AboutWindowViewModel.cs
+++ Ryujinx.Ava/UI/ViewModels/AboutWindowViewModel.cs
@@ -83,7 +83,7 @@ namespace Ryujinx.Ava.UI.ViewModels
 
         public string Developers
         {
-            get => string.Format(LocaleManager.Instance[LocaleKeys.AboutPageDeveloperListMore], "gdkchan, Ac_K, marysaka, rip in peri peri, LDj3SNuD, emmaus, Thealexbarney, Xpl0itR, GoffyDude, »jD«");
+            get => string.Format(LocaleManager.Instance[LocaleKeys.AboutPageDeveloperListMore], "gdkchan, Ac_K, marysaka, rip in peri peri, LDj3SNuD, emmaus, Thealexbarney, GoffyDude, TSRBerry, IsaacMarovitz");
         }
 
         public AboutWindowViewModel()
