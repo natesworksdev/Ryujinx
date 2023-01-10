5e61e2ae2a4c28912ba41c4f473f66b9160fd1a0
Isaac Marovitz <isaacryu@icloud.com>
2023-01-10 18:09:00 -0500

    InputStickView

---
+++ Ryujinx.Ava/UI/Views/Input/InputStickView.axaml.cs
@@ -0,0 +1,48 @@
+using Avalonia;
+using Avalonia.Controls;
+using Ryujinx.Ava.UI.ViewModels;
+using Ryujinx.Ava.UI.Windows;
+using Ryujinx.Common.Configuration.Hid.Controller;
+
+namespace Ryujinx.Ava.UI.Views.Input
+{
+    public partial class InputStickView : UserControl
+    {
+        public static readonly DirectProperty<InputStickView, StickInputId> SideProperty =
+            AvaloniaProperty.RegisterDirect<InputStickView, StickInputId>(
+                nameof(Side),
+                o => o.Side,
+                (o, v) => o.Side = v);
+        public InputStickView ViewModel;
+
+        private StickInputId _side;
+        public StickInputId Side
+        {
+            get
+            {
+                return _side;
+            }
+            set
+            {
+                SetAndRaise(SideProperty, ref _side, value);
+            }
+        }
+
+        public InputStickView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToVisualTree(e);
+
+            if (VisualRoot is ControllerSettingsWindow window)
+            {
+                ViewModel = window.ViewModel;
+            }
+
+            DataContext = ViewModel;
+        }
+    }
+}
---
+++ src/Ryujinx.Ava/UI/ViewModels/InputStickViewModel.cs
@@ -0,0 +1,168 @@
+using Ryujinx.Common.Configuration.Hid;
+using Ryujinx.Common.Configuration.Hid.Controller;
+
+namespace Ryujinx.Ava.UI.ViewModels
+{
+    public class InputStickViewModel : BaseModel
+    {
+        private string _stickName;
+
+        private bool _isController;
+        private bool _invertXAxis;
+        private bool _invertYAxis;
+        private bool _rotate90;
+
+        private Key _joystick;
+        private Key _keyboardStickButton;
+        private Key _controllerStickButton;
+        private Key _stickUp;
+        private Key _stickDown;
+        private Key _stickLeft;
+        private Key _stickRight;
+
+        private float _deadzone;
+        private float _range;
+
+        public StickInputId Side;
+
+        public string StickName
+        {
+            get => _stickName;
+            set
+            {
+                _stickName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsController
+        {
+            get => _isController;
+            set
+            {
+                _isController = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool InvertXAxis
+        {
+            get => _invertXAxis;
+            set
+            {
+                _invertXAxis = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool InvertYAxis
+        {
+            get => _invertYAxis;
+            set
+            {
+                _invertYAxis = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Rotate90
+        {
+            get => _rotate90;
+            set
+            {
+                _rotate90 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Key Joystick
+        {
+            get => _joystick;
+            set
+            {
+                _joystick = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Key KeyboardStickButton
+        {
+            get => _keyboardStickButton;
+            set
+            {
+                _keyboardStickButton = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Key ControllerStickButton
+        {
+            get => _controllerStickButton;
+            set
+            {
+                _controllerStickButton = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Key StickUp
+        {
+            get => _stickUp;
+            set
+            {
+                _stickUp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Key StickDown
+        {
+            get => _stickDown;
+            set
+            {
+                _stickDown = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Key StickLeft
+        {
+            get => _stickLeft;
+            set
+            {
+                _stickLeft = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Key StickRight
+        {
+            get => _stickRight;
+            set
+            {
+                _stickRight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public float Deadzone
+        {
+            get => _deadzone;
+            set
+            {
+                _deadzone = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public float Range
+        {
+            get => _range;
+            set
+            {
+                _range = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
