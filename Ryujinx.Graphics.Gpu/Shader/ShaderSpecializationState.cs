2cd1971f82683b1b04ec3cec3a27c3ec58acda7b
riperiperi <rhy3756547@hotmail.com>
2023-01-11 23:49:52 +0000

    Add missing check for fragment output types
    Might have caused the crash in MK8

--- Ryujinx.Graphics.Gpu/Shader/ShaderSpecializationState.cs
+++ Ryujinx.Graphics.Gpu/Shader/ShaderSpecializationState.cs
@@ -530,6 +530,11 @@ namespace Ryujinx.Graphics.Gpu.Shader
                 return false;
             }
 
+            if (!graphicsState.FragmentOutputTypes.AsSpan().SequenceEqual(GraphicsState.FragmentOutputTypes.AsSpan()))
+            {
+                return false;
+            }
+
             return Matches(channel, ref poolState, checkTextures, isCompute: false);
         }
 
