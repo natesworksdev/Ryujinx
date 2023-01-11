a5fa472f13b0a975e32b1b17ca6ac712d3091b42
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2023-01-11 12:41:38 -0500

    Update Ryujinx.Ava/UI/Models/SaveModel.cs
    Co-authored-by: Ac_K <Acoustik666@gmail.com>

--- Ryujinx.Ava/UI/Models/SaveModel.cs
+++ Ryujinx.Ava/UI/Models/SaveModel.cs
@@ -41,7 +41,7 @@ namespace Ryujinx.Ava.UI.Models
         private string GetSizeString()
         {
             const int scale = 1024;
-            string[] orders = { "GB", "MB", "KB" };
+            string[] orders = { "GiB", "MiB", "KiB" };
             long max = (long)Math.Pow(scale, orders.Length);
 
             foreach (string order in orders)
