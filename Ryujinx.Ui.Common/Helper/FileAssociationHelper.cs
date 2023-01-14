cf2a42da06bbb2d2d5ff7326c9f977b705547fac
AcK77 <acoustik666@gmail.com>
2023-01-14 11:45:47 +0100

    Add Windows support to FileAssociationHelper.cs

--- Ryujinx.Ui.Common/Helper/FileAssociationHelper.cs
+++ Ryujinx.Ui.Common/Helper/FileAssociationHelper.cs
@@ -1,24 +1,28 @@
+using Microsoft.Win32;
 using Ryujinx.Common;
 using Ryujinx.Common.Logging;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
 namespace Ryujinx.Ui.Common.Helper
 {
-    public static class FileAssociationHelper
+    public static partial class FileAssociationHelper
     {
-        public static bool IsTypeAssociationSupported => OperatingSystem.IsLinux();
+        private const int SHCNE_ASSOCCHANGED = 0x8000000;
+        private const int SHCNF_FLUSH        = 0x1000;
+
+        [LibraryImport("shell32.dll", SetLastError = true)]
+        public static partial void SHChangeNotify(uint wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+
+        public static bool IsTypeAssociationSupported => (OperatingSystem.IsLinux() || OperatingSystem.IsWindows()) &&
+                                                         !ReleaseInformation.IsFlatHubBuild();
 
         [SupportedOSPlatform("linux")]
         private static bool RegisterLinuxMimeTypes()
         {
-            if (ReleaseInformation.IsFlatHubBuild())
-            {
-                return false;
-            }
-
             string mimeDbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local", "share", "mime");
 
             if (!File.Exists(Path.Combine(mimeDbPath, "packages", "Ryujinx.xml")))
@@ -55,6 +59,37 @@ namespace Ryujinx.Ui.Common.Helper
             return true;
         }
 
+        [SupportedOSPlatform("windows")]
+        private static bool RegisterWindowsMimeTypes()
+        {
+            static bool RegisterExtension(string ext)
+            {
+                RegistryKey key = Registry.CurrentUser.CreateSubKey(@$"Software\Classes\{ext}");
+
+                if (key is null)
+                {
+                    return false;
+                }
+
+                key!.CreateSubKey(@"shell\open\command")!.SetValue("", $"\"{Environment.ProcessPath}\" \"%1\"");
+                key.Close();
+
+                // Notify Explorer the file association has been changed.
+                SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_FLUSH, IntPtr.Zero, IntPtr.Zero);
+
+                return true;
+            }
+
+            bool registered = false;
+
+            foreach (string ext in new string[] { ".nca", ".nro", ".nso", ".nsp", ".xci" })
+            {
+                registered |= RegisterExtension(ext);
+            }
+
+            return registered;
+        }
+
         public static bool RegisterTypeAssociations()
         {
             if (OperatingSystem.IsLinux())
@@ -62,7 +97,12 @@ namespace Ryujinx.Ui.Common.Helper
                 return RegisterLinuxMimeTypes();
             }
 
-            // TODO: Add Windows and macOS support
+            if (OperatingSystem.IsWindows())
+            {
+                return RegisterWindowsMimeTypes();
+            }
+
+            // TODO: Add macOS support.
 
             return false;
         }
