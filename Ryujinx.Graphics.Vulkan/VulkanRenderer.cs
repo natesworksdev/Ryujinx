e767778691a57a22b3bedfde4dfc6808f1500953
gdk <gab.dark.100@gmail.com>
2023-01-23 19:52:57 -0300

    Change query capability check to check for pipeline statistics query rather than geometry shader support

--- Ryujinx.Graphics.Vulkan/VulkanRenderer.cs
+++ Ryujinx.Graphics.Vulkan/VulkanRenderer.cs
@@ -271,6 +271,7 @@ namespace Ryujinx.Graphics.Vulkan
                 supportsTransformFeedback,
                 propertiesTransformFeedback.TransformFeedbackQueries,
                 features2.Features.OcclusionQueryPrecise,
+                supportedFeatures.PipelineStatisticsQuery,
                 supportedFeatures.GeometryShader,
                 propertiesSubgroupSizeControl.MinSubgroupSize,
                 propertiesSubgroupSizeControl.MaxSubgroupSize,
@@ -589,8 +590,8 @@ namespace Ryujinx.Graphics.Vulkan
 
             Vendor = VendorUtils.FromId(properties.VendorID);
 
-            IsAmdWindows = Vendor == Vendor.Amd && RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-            IsIntelWindows = Vendor == Vendor.Intel && RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            IsAmdWindows = Vendor == Vendor.Amd && OperatingSystem.IsWindows();
+            IsIntelWindows = Vendor == Vendor.Intel && OperatingSystem.IsWindows();
             IsTBDR = IsMoltenVk ||
                 Vendor == Vendor.Qualcomm ||
                 Vendor == Vendor.ARM ||
