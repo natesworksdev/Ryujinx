5674378536b782e76f1d6382e256ac7578c85a2b
Andrey Sukharev <SukharevAndrey@users.noreply.github.com>
2023-03-19 15:25:47 +0300

    Remove overridden json property names as they are handled in the options

--- Ryujinx.Ui.Common/Models/Github/GithubReleaseAssetJsonResponse.cs
+++ Ryujinx.Ui.Common/Models/Github/GithubReleaseAssetJsonResponse.cs
@@ -1,14 +1,9 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Ryujinx.Ui.Common.Models.Github
+﻿namespace Ryujinx.Ui.Common.Models.Github
 {
     public class GithubReleaseAssetJsonResponse
     {
-        [JsonPropertyName("name")]
         public string Name { get; set; }
-        [JsonPropertyName("state")]
         public string State { get; set; }
-        [JsonPropertyName("browser_download_url")]
         public string BrowserDownloadUrl { get; set; }
     }
 }
--- Ryujinx.Ui.Common/Models/Github/GithubReleasesJsonResponse.cs
+++ Ryujinx.Ui.Common/Models/Github/GithubReleasesJsonResponse.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace Ryujinx.Ui.Common.Models.Github
 {
     public class GithubReleasesJsonResponse
     {
-        [JsonPropertyName("name")]
         public string Name { get; set; }
-        [JsonPropertyName("assets")]
         public List<GithubReleaseAssetJsonResponse> Assets { get; set; }
     }
 }
