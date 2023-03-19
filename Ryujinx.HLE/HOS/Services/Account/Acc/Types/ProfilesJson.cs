7282c305d4e010df038d814c018be789ac4e3328
Andrey Sukharev <SukharevAndrey@users.noreply.github.com>
2023-03-19 15:25:47 +0300

    Remove overridden json property names as they are handled in the options

--- Ryujinx.HLE/HOS/Services/Account/Acc/Types/ProfilesJson.cs
+++ Ryujinx.HLE/HOS/Services/Account/Acc/Types/ProfilesJson.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace Ryujinx.HLE.HOS.Services.Account.Acc.Types
 {
     internal struct ProfilesJson
     {
-        [JsonPropertyName("profiles")]
         public List<UserProfileJson> Profiles { get; set; }
-        [JsonPropertyName("last_opened")]
         public string LastOpened { get; set; }
     }
 }
