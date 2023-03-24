c6d972f315ae5b9f597c073d57c9906cf0660dfc
Luke Kellett <lkellett2@outlook.com>
2023-03-24 23:38:08 +1000

    changed exception type to OutOfRangeException

--- Ryujinx.Ava/UI/ViewModels/MainWindowViewModel.cs
+++ Ryujinx.Ava/UI/ViewModels/MainWindowViewModel.cs
@@ -1346,7 +1346,7 @@ namespace Ryujinx.Ava.UI.ViewModels
                 "NCA"  => ConfigurationState.Instance.Ui.ShownFileTypes.NCA.Value  = !ConfigurationState.Instance.Ui.ShownFileTypes.NCA,
                 "NRO"  => ConfigurationState.Instance.Ui.ShownFileTypes.NRO.Value  = !ConfigurationState.Instance.Ui.ShownFileTypes.NRO,
                 "NSO"  => ConfigurationState.Instance.Ui.ShownFileTypes.NSO.Value  = !ConfigurationState.Instance.Ui.ShownFileTypes.NSO,
-                    _  => throw new NotImplementedException(),
+                    _  => throw new ArgumentOutOfRangeException(fileType),
             };
 
             ConfigurationState.Instance.ToFileFormat().SaveConfig(Program.ConfigurationPath);
