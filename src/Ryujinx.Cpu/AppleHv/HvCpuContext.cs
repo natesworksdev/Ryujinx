3a049dc257b3e61f4d58a3f8a029aa73f43a3380
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-09 16:25:15 +0200

    Silence dotnet format IDE0060 warnings

--- src/Ryujinx.Cpu/AppleHv/HvCpuContext.cs
+++ src/Ryujinx.Cpu/AppleHv/HvCpuContext.cs
@@ -8,16 +8,20 @@ namespace Ryujinx.Cpu.AppleHv
         private readonly ITickSource _tickSource;
         private readonly HvMemoryManager _memoryManager;
 
+#pragma warning disable IDE0060
         public HvCpuContext(ITickSource tickSource, IMemoryManager memory, bool for64Bit)
         {
             _tickSource = tickSource;
             _memoryManager = (HvMemoryManager)memory;
         }
+#pragma warning restore IDE0060
 
 #pragma warning disable IDE0051
+#pragma warning disable IDE0060
         private static void UnmapHandler(ulong address, ulong size)
         {
         }
+#pragma warning restore IDE0060
 #pragma warning restore IDE0051
 
         /// <inheritdoc/>
