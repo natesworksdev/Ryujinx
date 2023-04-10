d0a6750334a4532e1fb8c926aa42feba0c456c5c
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-11 00:16:05 +0200

    Make dotnet format succeed in style mode

--- src/Ryujinx/Ui/MainWindow.cs
+++ src/Ryujinx/Ui/MainWindow.cs
@@ -1626,11 +1626,11 @@ namespace Ryujinx.Ui
         private void ManageCheats_Pressed(object sender, EventArgs args)
         {
             var window = new CheatWindow(
-                _virtualFileSystem,
-                _emulationContext.Processes.ActiveApplication.ProgramId,
-                _emulationContext.Processes.ActiveApplication.ApplicationControlProperties
-                    .Title[(int)_emulationContext.System.State.DesiredTitleLanguage].NameString.ToString(), 
-                _currentEmulatedGamePath);
+                            _virtualFileSystem,
+                            _emulationContext.Processes.ActiveApplication.ProgramId,
+                            _emulationContext.Processes.ActiveApplication.ApplicationControlProperties
+                                .Title[(int)_emulationContext.System.State.DesiredTitleLanguage].NameString.ToString(), 
+                            _currentEmulatedGamePath);
 
             window.Destroyed += CheatWindow_Destroyed;
             window.Show();
