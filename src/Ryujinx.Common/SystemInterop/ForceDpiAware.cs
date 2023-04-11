07887563a4c296f21a165b25ef37fd2ae62a60bd
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-11 22:59:41 +0200

    Silence CA1806 and CA1834 issues

--- src/Ryujinx.Common/SystemInterop/ForceDpiAware.cs
+++ src/Ryujinx.Common/SystemInterop/ForceDpiAware.cs
@@ -65,7 +65,7 @@ namespace Ryujinx.Common.SystemInterop
                         {
                             userDpiScale = (double)XDisplayWidth(display, 0) * 25.4 / (double)XDisplayWidthMM(display, 0);
                         }
-                        XCloseDisplay(display);
+                        _ = XCloseDisplay(display);
                     }
                     else if (xdgSessionType == "wayland")
                     {
