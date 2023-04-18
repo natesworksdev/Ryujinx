8ff0508acd02262c80d6facae6083ea51b6d9086
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-18 23:27:17 +0200

    Apply dotnet format whitespace formatting
    A few of them have been manually reverted and the corresponding warning was silenced

--- src/Ryujinx.Audio/Renderer/Dsp/UpsamplerHelper.cs
+++ src/Ryujinx.Audio/Renderer/Dsp/UpsamplerHelper.cs
@@ -58,10 +58,10 @@ namespace Ryujinx.Audio.Renderer.Dsp
             {
                 state.Scale = inputSampleCount switch
                 {
-                    40  => 6.0f,
-                    80  => 3.0f,
+                    40 => 6.0f,
+                    80 => 3.0f,
                     160 => 1.5f,
-                    _   => throw new ArgumentOutOfRangeException(nameof(inputSampleCount), inputSampleCount, null)
+                    _ => throw new ArgumentOutOfRangeException(nameof(inputSampleCount), inputSampleCount, null)
                 };
                 state.Initialized = true;
             }
