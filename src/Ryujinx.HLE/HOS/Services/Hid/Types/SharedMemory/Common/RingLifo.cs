cfb46e2f35e21c674763813b976d5e9d6048fb59
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-19 02:05:25 +0200

    Add previously silenced warnings back
    I have no clue how these disappeared

--- src/Ryujinx.HLE/HOS/Services/Hid/Types/SharedMemory/Common/RingLifo.cs
+++ src/Ryujinx.HLE/HOS/Services/Hid/Types/SharedMemory/Common/RingLifo.cs
@@ -9,9 +9,9 @@ namespace Ryujinx.HLE.HOS.Services.Hid.Types.SharedMemory.Common
     {
         private const ulong MaxEntries = 17;
 
-#pragma warning disable IDE0051
+#pragma warning disable IDE0051, CS0169
         private readonly ulong _unused;
-#pragma warning restore IDE0051
+#pragma warning restore IDE0051, CS0169
 #pragma warning disable CS0414, IDE0052
         private ulong _bufferCount;
 #pragma warning restore CS0414, IDE0052
