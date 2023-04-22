bfd3bd78a923ed23df9576a197a4e034b8f39409
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-22 02:54:37 +0200

    Revert formatting changes for while and for-loops

--- src/Ryujinx.Graphics.GAL/Multithreading/ThreadedRenderer.cs
+++ src/Ryujinx.Graphics.GAL/Multithreading/ThreadedRenderer.cs
@@ -468,9 +468,9 @@ namespace Ryujinx.Graphics.GAL.Multithreading
             {
                 lock (_interruptLock)
                 {
-#pragma warning disable IDE0055 // Disable formatting
-                    while (Interlocked.CompareExchange(ref _interruptAction, action, null) != null) { }
-#pragma warning restore IDE0055
+                    while (Interlocked.CompareExchange(ref _interruptAction, action, null) != null)
+                    {
+                    }
 
                     _galWorkAvailable.Set();
 
