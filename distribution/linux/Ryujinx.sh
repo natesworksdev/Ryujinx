dd6f7e4cfc44455a6212be0749e5f12654e7d4d8
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-25 19:53:39 +0200

    Revert increasing vm.max_map_count via script

--- distribution/linux/Ryujinx.sh
+++ distribution/linux/Ryujinx.sh
@@ -11,13 +11,6 @@ if [ -f "$SCRIPT_DIR/Ryujinx.Headless.SDL2" ]; then
     RYUJINX_BIN="Ryujinx.Headless.SDL2"
 fi
 
-ORIG_MAX_MAP_COUNT=$(sysctl vm.max_map_count | awk -F' ' '{print $3}')
-
-if [ $ORIG_MAX_MAP_COUNT -le 65530 ]; then
-  echo "Increasing max amount of memory map areas..."
-  pkexec sh -c 'sysctl -w vm.max_map_count=262144'
-fi
-
 COMMAND="env DOTNET_EnableAlternateStackCheck=1"
 
 if command -v gamemoderun > /dev/null 2>&1; then
@@ -25,8 +18,3 @@ if command -v gamemoderun > /dev/null 2>&1; then
 fi
 
 $COMMAND "$SCRIPT_DIR/$RYUJINX_BIN" "$@"
-
-if [ $ORIG_MAX_MAP_COUNT -le 65530 ]; then
-  echo "Restoring original max amount of memory map areas..."
-  pkexec sh -c "sysctl -w vm.max_map_count=$ORIG_MAX_MAP_COUNT"
-fi
