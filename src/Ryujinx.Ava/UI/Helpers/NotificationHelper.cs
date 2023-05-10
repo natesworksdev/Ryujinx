28182f05f378a25c5fcc285bff2db286e905c388
jhorv <38920027+jhorv@users.noreply.github.com>
2023-05-09 20:02:43 -0400

    Revert "use AsyncWorkQueue to do everything I added in SetNotificationManager()"
    This reverts commit f0e78366b8776ec8e2fef8ab023c0db1833155d3.

--- src/Ryujinx.Ava/UI/Helpers/NotificationHelper.cs
+++ src/Ryujinx.Ava/UI/Helpers/NotificationHelper.cs
@@ -3,7 +3,6 @@ using Avalonia.Controls;
 using Avalonia.Controls.Notifications;
 using Avalonia.Threading;
 using Ryujinx.Ava.Common.Locale;
-using Ryujinx.Common;
 using System;
 using System.Collections.Concurrent;
 using System.Threading;
@@ -34,18 +33,45 @@ namespace Ryujinx.Ava.UI.Helpers
                 _templateAppliedEvent.Set();
             };
 
-            AsyncWorkQueue<Notification> asyncWorkQueue = new AsyncWorkQueue<Notification>(notification =>
-            {
-                Dispatcher.UIThread.Post(() =>
-                {
-                    _notificationManager.Show(notification);
-                });
-            }, "UI.NotificationThread", _notifications);
+            // Ordinarily you'd want to Dispose() these, but we're using this one until the application start shutting down.
+            // The process will be ending soon (right?!) so we're not too concerned about proper disposal.
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
             host.Closing += (sender, args) =>
             {
-                asyncWorkQueue.Dispose();
+                cancellationTokenSource.Cancel();
+                _notifications.CompleteAdding();
             };
+
+            var notificationThread = new Thread(() =>
+            {
+                _templateAppliedEvent.Wait(cancellationTokenSource.Token);
+
+                try
+                {
+                    foreach (var notification in _notifications.GetConsumingEnumerable(cancellationTokenSource.Token))
+                    {
+                        Dispatcher.UIThread.Post(() =>
+                        {
+                            _notificationManager.Show(notification);
+                        });
+
+                        bool isCancelled = cancellationTokenSource.Token.WaitHandle.WaitOne(NotificationDelayInMs / MaxNotifications);
+                        if (isCancelled)
+                            break;
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Do nothing.
+                }
+            })
+            {
+                Name = "UI.NotificationThread",
+                IsBackground = true,
+            };
+
+            notificationThread.Start();
         }
 
         public static void Show(string title, string text, NotificationType type, bool waitingExit = false, Action onClick = null, Action onClose = null)
