e5bdda423e0647c53b326e84c2f68e12492309f3
jhorv <38920027+jhorv@users.noreply.github.com>
2023-05-09 20:26:26 -0400

    use AsyncWorkQueue to handle the Thread-related changes previously made to NotificationHelper.SetNotificationHelper(). Wrap it in Lazy<T> and force instantiation in the TemplateApplied event handler to accomodate for the fact that AsyncWorkQueue starts immediately, and the notification dispatch loop was being delayed by _templateAppliedEvent.

--- src/Ryujinx.Ava/UI/Helpers/NotificationHelper.cs
+++ src/Ryujinx.Ava/UI/Helpers/NotificationHelper.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.Notifications;
 using Avalonia.Threading;
 using Ryujinx.Ava.Common.Locale;
+using Ryujinx.Common;
 using System;
 using System.Collections.Concurrent;
 using System.Threading;
@@ -16,7 +17,6 @@ namespace Ryujinx.Ava.UI.Helpers
 
         private static WindowNotificationManager _notificationManager;
 
-        private static readonly ManualResetEventSlim             _templateAppliedEvent = new(false);
         private static readonly BlockingCollection<Notification> _notifications        = new();
 
         public static void SetNotificationManager(Window host)
@@ -28,50 +28,29 @@ namespace Ryujinx.Ava.UI.Helpers
                 Margin   = new Thickness(0, 0, 15, 40)
             };
 
+            Lazy<AsyncWorkQueue<Notification>> maybeAsyncWorkQueue = new Lazy<AsyncWorkQueue<Notification>>(
+                () => new AsyncWorkQueue<Notification>(notification =>
+                {
+                    Dispatcher.UIThread.Post(() =>
+                    {
+                        _notificationManager.Show(notification);
+                    });
+                }, "UI.NotificationThread", _notifications)
+                , LazyThreadSafetyMode.ExecutionAndPublication);
+
             _notificationManager.TemplateApplied += (sender, args) =>
             {
-                _templateAppliedEvent.Set();
+                // Force creation of the AsyncWorkQueue
+                _ = maybeAsyncWorkQueue.Value;
             };
-
-            // Ordinarily you'd want to Dispose() these, but we're using this one until the application start shutting down.
-            // The process will be ending soon (right?!) so we're not too concerned about proper disposal.
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
             host.Closing += (sender, args) =>
             {
-                cancellationTokenSource.Cancel();
-                _notifications.CompleteAdding();
-            };
-
-            var notificationThread = new Thread(() =>
-            {
-                _templateAppliedEvent.Wait(cancellationTokenSource.Token);
-
-                try
+                if (maybeAsyncWorkQueue.IsValueCreated)
                 {
-                    foreach (var notification in _notifications.GetConsumingEnumerable(cancellationTokenSource.Token))
-                    {
-                        Dispatcher.UIThread.Post(() =>
-                        {
-                            _notificationManager.Show(notification);
-                        });
-
-                        bool isCancelled = cancellationTokenSource.Token.WaitHandle.WaitOne(NotificationDelayInMs / MaxNotifications);
-                        if (isCancelled)
-                            break;
-                    }
+                    maybeAsyncWorkQueue.Value.Dispose();
                 }
-                catch (OperationCanceledException)
-                {
-                    // Do nothing.
-                }
-            })
-            {
-                Name = "UI.NotificationThread",
-                IsBackground = true,
             };
-
-            notificationThread.Start();
         }
 
         public static void Show(string title, string text, NotificationType type, bool waitingExit = false, Action onClick = null, Action onClose = null)
