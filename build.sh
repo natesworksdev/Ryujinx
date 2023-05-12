8cb7afde79247e4b06fac7f4631f5a58e8ca18b6
beginor <beginor@qq.com>
2023-05-12 10:56:40 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-./distribution/macos/create_macos_release.sh . ./build/temp ./build/output ./distribution/macos/entitlements.xml 1.1.0 $(date +%Y%m%d)
+./distribution/macos/create_macos_release.sh . ./build/temp ./build/output ./distribution/macos/entitlements.xml 1.1.0 $(date +%Y%m%d) Release
