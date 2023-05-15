b2c1832e32c8d6ab99055202fe30548d0d2f213e
subanz <subanz@protonmail.com>
2023-05-14 20:25:15 -0500

    Fix due to wrong assumption about the output of which.
    Checks if the which output contains a no match response, otherwise use gamemoderun.
    
    Using a case statement because it makes substring matching possible in sh and also it turns out that adding an empty string after env throws an error because env attempts to parse it as a paramater.

--- distribution/linux/Ryujinx.sh
+++ distribution/linux/Ryujinx.sh
@@ -11,4 +11,9 @@ if [ -f "$SCRIPT_DIR/Ryujinx.Headless.SDL2" ]; then
     RYUJINX_BIN="Ryujinx.Headless.SDL2"
 fi
 
-env DOTNET_EnableAlternateStackCheck=1 "$(which gamemoderun)" "$SCRIPT_DIR/$RYUJINX_BIN" "$@"
+gamemode="$(which gamemoderun)"
+
+case "which: no gamemoderun in" in
+*"$gamemode"*) env DOTNET_EnableAlternateStackCheck=1 "$SCRIPT_DIR/$RYUJINX_BIN" "$@";;
+*   ) env DOTNET_EnableAlternateStackCheck=1 "$gamemode" "$SCRIPT_DIR/$RYUJINX_BIN" "$@"
+esac
