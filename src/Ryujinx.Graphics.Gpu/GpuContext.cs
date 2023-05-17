cfa6654b492eddc6c32b8c66a2fbd1c8557cc36d
paftree <1337paf92@gmail.com>
2023-05-17 00:59:45 -0500

    init _firstTimestamp in constructer per feedback

--- src/Ryujinx.Graphics.Gpu/GpuContext.cs
+++ src/Ryujinx.Graphics.Gpu/GpuContext.cs
@@ -124,6 +124,8 @@ namespace Ryujinx.Graphics.Gpu
             DeferredActions = new Queue<Action>();
 
             PhysicalMemoryRegistry = new ConcurrentDictionary<ulong, PhysicalMemory>();
+
+            _firstTimestamp = ConvertNanosecondsToTicks((ulong)PerformanceCounter.ElapsedNanoseconds);
         }
 
         /// <summary>
@@ -221,10 +223,6 @@ namespace Ryujinx.Graphics.Gpu
             ulong ticks = ConvertNanosecondsToTicks((ulong)PerformanceCounter.ElapsedNanoseconds);
 
             // Initialize ticks counter to start at 0, instead of the host GPU counter
-            if (_firstTimestamp == 0)
-            {
-                _firstTimestamp = ticks;
-            }
             ticks -= _firstTimestamp;
 
             if (GraphicsConfig.FastGpuTime)
