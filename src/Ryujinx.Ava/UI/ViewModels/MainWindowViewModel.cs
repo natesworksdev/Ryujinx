bd9efa2fcbbf02883f42c2aea94795034d2eaa25
Ac_K <Acoustik666@gmail.com>
2023-06-01 18:01:40 +0200

    Remove unused using

--- src/Ryujinx.Ava/UI/ViewModels/MainWindowViewModel.cs
+++ src/Ryujinx.Ava/UI/ViewModels/MainWindowViewModel.cs
@@ -32,7 +32,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Path = System.IO.Path;
