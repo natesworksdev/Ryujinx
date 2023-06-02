f66a9f17584210540933658f564c78c41b983fe2
Mary <mary@mary.zone>
2023-06-02 19:42:42 +0200

    Armeilleure: Fix support for Windows on ARM64
    Tested on Windows DevKit 2023.

--- src/ARMeilleure/Translation/Cache/JitCache.cs
+++ src/ARMeilleure/Translation/Cache/JitCache.cs
@@ -6,10 +6,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 namespace ARMeilleure.Translation.Cache
 {
-    static class JitCache
+    static partial class JitCache
     {
         private const int PageSize = 4 * 1024;
         private const int PageMask = PageSize - 1;
@@ -26,6 +27,10 @@ namespace ARMeilleure.Translation.Cache
 
         private static readonly object _lock = new object();
         private static bool _initialized;
+
+        [SupportedOSPlatform("windows")]
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        public static partial IntPtr FlushInstructionCache(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize);
 
         public static void Initialize(IJitMemoryAllocator allocator)
         {
@@ -77,7 +82,14 @@ namespace ARMeilleure.Translation.Cache
                     Marshal.Copy(code, 0, funcPtr, code.Length);
                     ReprotectAsExecutable(funcOffset, code.Length);
 
-                    _jitCacheInvalidator.Invalidate(funcPtr, (ulong)code.Length);
+                    if (OperatingSystem.IsWindows() && RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
+                    {
+                        FlushInstructionCache(Process.GetCurrentProcess().Handle, funcPtr, (UIntPtr)code.Length);
+                    }
+                    else
+                    {
+                        _jitCacheInvalidator.Invalidate(funcPtr, (ulong)code.Length);
+                    }
                 }
 
                 Add(funcOffset, code.Length, func.UnwindInfo);
