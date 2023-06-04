ff2c8fdd22aeaf30578432b6cd98b28642413d42
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-06-04 18:37:44 +0200

    Remove a few unused parameters

--- src/Ryujinx.Cpu/AppleHv/HvCpuContext.cs
+++ src/Ryujinx.Cpu/AppleHv/HvCpuContext.cs
@@ -13,15 +13,11 @@ namespace Ryujinx.Cpu.AppleHv
             _tickSource = tickSource;
             _memoryManager = (HvMemoryManager)memory;
         }
-#pragma warning restore IDE0060
 
-#pragma warning disable IDE0051 // Remove unused private member
-#pragma warning disable IDE0060 // Remove unused parameter
         private static void UnmapHandler(ulong address, ulong size)
         {
         }
 #pragma warning restore IDE0060
-#pragma warning restore IDE0051
 
         /// <inheritdoc/>
         public IExecutionContext CreateExecutionContext(ExceptionCallbacks exceptionCallbacks)
