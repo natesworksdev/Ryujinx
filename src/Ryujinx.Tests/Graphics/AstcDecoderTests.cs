18231cc9da1fddb4dc4986c79b9863bf5d3b8ce7
Michael Durrant <mmdurrant@gmail.com>
2023-06-11 11:27:53 -0600

    wip

---
+++ src/Ryujinx.Tests/Graphics/AstcDecoderTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Ryujinx.Graphics.Gpu;
+using Ryujinx.Graphics.Texture.Astc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryujinx.Tests.Graphics
+{
+    public class AstcDecoderTests
+    {
+        private const int ASTCBlockSize;
+
+        [SetUp]
+        public void SetupFixture()
+        {
+
+        } 
+
+        [Test]
+        public void _Test()
+        {
+            var inputData = GetTestData();
+            var outputBuffer = new Memory<byte>();
+                        GraphicsConfig.EnableTextureRecompression = false;
+
+
+            AstcDecoder.TryDecodeToRgba8P()
+
+            AstcDecoder target = new AstcDecoder();
+        }
+
+        private ReadOnlyMemory<byte> GetTestData();
+    }
+}
