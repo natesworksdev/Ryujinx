7f580078606db4f75cefaaeb03efa302fea2c93c
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-06-14 20:19:31 +0200

    Address IDE0251 warnings

--- src/Ryujinx.Graphics.Vic/Image/InputSurface.cs
+++ src/Ryujinx.Graphics.Vic/Image/InputSurface.cs
@@ -15,7 +15,7 @@ namespace Ryujinx.Graphics.Vic.Image
             Index = index;
         }
 
-        public void Return(BufferPool<byte> pool)
+        public readonly void Return(BufferPool<byte> pool)
         {
             if (Index != -1)
             {
@@ -65,7 +65,7 @@ namespace Ryujinx.Graphics.Vic.Image
             Buffer2Index = buffer.Index;
         }
 
-        public void Return(BufferPool<byte> pool)
+        public readonly void Return(BufferPool<byte> pool)
         {
             if (Buffer0Index != -1)
             {
