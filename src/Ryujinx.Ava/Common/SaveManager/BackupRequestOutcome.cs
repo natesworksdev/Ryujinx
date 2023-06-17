091bc2be536ea8f1500e8c33e56287262127cbb6
Patrick Hovsepian <hov@Patricks-MacBook-Pro.local>
2023-06-17 15:15:46 -0700

    own file

---
+++ src/Ryujinx.Ava/Common/SaveManager/BackupRequestOutcome.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ryujinx.Ava.Common.SaveManager
+{
+    public readonly record struct BackupRequestOutcome
+    {
+        public bool DidFail { get; init; }
+        public string Message { get; init; }
+    }
+}
+
--- src/Ryujinx.Ava/Common/SaveManager/SaveManager.cs
+++ src/Ryujinx.Ava/Common/SaveManager/SaveManager.cs
@@ -23,12 +23,6 @@ using Path = System.IO.Path;
 
 namespace Ryujinx.Ava.Common.SaveManager
 {
-    public readonly record struct BackupRequestOutcome
-    {
-        public bool DidFail { get; init; }
-        public string Message { get; init; }
-    }
-
     public class SaveManager : ISaveManager
     {
         // UI Metadata
