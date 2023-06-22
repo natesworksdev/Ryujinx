19ac0b207cd224ff5d9d9748841e9942c8d4b689
Ac_K <Acoustik666@gmail.com>
2023-06-22 22:58:22 +0200

    cpu: Implement VCVT (between floating-point and fixed-point) instruction
    Rebase, fix and superseed of https://github.com/Ryujinx/Ryujinx/pull/2915
    
    (Since I only have little CPU knowledge, I hope I have done everything good)

---
+++ src/ARMeilleure/Decoders/OpCode32SimdCvtFFixed.cs
@@ -0,0 +1,28 @@
+﻿namespace ARMeilleure.Decoders
+{
+    class OpCode32SimdCvtFFixed : OpCode32Simd
+    {
+        public int Fbits { get; protected set; }
+
+        public new static OpCode Create(InstDescriptor inst, ulong address, int opCode) => new OpCode32SimdCvtFFixed(inst, address, opCode, false);
+        public new static OpCode CreateT32(InstDescriptor inst, ulong address, int opCode) => new OpCode32SimdCvtFFixed(inst, address, opCode, true);
+
+        public OpCode32SimdCvtFFixed(InstDescriptor inst, ulong address, int opCode, bool isThumb) : base(inst, address, opCode, isThumb)
+        {
+            Opc = (opCode >> 8) & 0x1;
+
+            Size = Opc == 1 ? 0 : 2;
+            Fbits = 64 - ((opCode >> 16) & 0x3f);
+
+            if (((opCode >> 21) & 0x1) == 0)
+            {
+                Instruction = InstDescriptor.Undefined;
+            }
+
+            if (DecoderHelper.VectorArgumentsInvalid(Q, Vd, Vm))
+            {
+                Instruction = InstDescriptor.Undefined;
+            }
+        }
+    }
+}
