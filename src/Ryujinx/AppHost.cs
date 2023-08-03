b61b703c74041c9670913e4671c4cb60ae46e7c2
Isaac Marovitz <isaacryu@icloud.com>
2023-08-03 14:50:49 -0400

    Formatting

--- src/Ryujinx/AppHost.cs
+++ src/Ryujinx/AppHost.cs
@@ -822,6 +822,10 @@ namespace Ryujinx.Ava
                     VulkanHelper.GetRequiredInstanceExtensions,
                     ConfigurationState.Instance.Graphics.PreferredGpu.Value);
             }
+            else if (ConfigurationState.Instance.Graphics.GraphicsBackend.Value == GraphicsBackend.Metal && OperatingSystem.IsMacOS())
+            {
+                renderer = new MetalRenderer();
+            }
             else
             {
                 renderer = new OpenGLRenderer();
