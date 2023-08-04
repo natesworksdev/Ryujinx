c614238fc61386138033b3a1c4209e8cfd27ffdf
Isaac Marovitz <isaacryu@icloud.com>
2023-08-04 17:15:38 -0400

    Isn’t that conveniant?

--- src/Ryujinx.Graphics.Shader/CodeGen/Msl/HelperFunctions/HelperFunctionNames.cs
+++ src/Ryujinx.Graphics.Shader/CodeGen/Msl/HelperFunctions/HelperFunctionNames.cs
@@ -2,9 +2,6 @@ namespace Ryujinx.Graphics.Shader.CodeGen.Msl
 {
     static class HelperFunctionNames
     {
-        public static string MultiplyHighS32 = "helperMultiplyHighS32";
-        public static string MultiplyHighU32 = "helperMultiplyHighU32";
-
         public static string Shuffle = "helperShuffle";
         public static string ShuffleDown = "helperShuffleDown";
         public static string ShuffleUp = "helperShuffleUp";
