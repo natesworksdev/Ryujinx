0480e9d8aeb8748b24589fe44a5b33567ab04303
riperiperi <rhy3756547@hotmail.com>
2023-08-06 23:27:35 +0100

    Remove mystery BufferCache change
    Probably macos related?

--- src/Ryujinx.Graphics.Gpu/Memory/BufferCache.cs
+++ src/Ryujinx.Graphics.Gpu/Memory/BufferCache.cs
@@ -372,18 +372,6 @@ namespace Ryujinx.Graphics.Gpu.Memory
         }
 
         /// <summary>
-        /// Gets a buffer sub-range for a given GPU memory range.
-        /// </summary>
-        /// <param name="memoryManager">GPU memory manager where the buffer is mapped</param>
-        /// <param name="gpuVa">Start GPU virtual address of the buffer</param>
-        /// <param name="size">Size in bytes of the buffer</param>
-        /// <returns>The buffer sub-range for the given range</returns>
-        public BufferRange GetGpuBufferRange(MemoryManager memoryManager, ulong gpuVa, ulong size)
-        {
-            return GetBufferRange(TranslateAndCreateBuffer(memoryManager, gpuVa, size), size);
-        }
-
-        /// <summary>
         /// Gets a buffer sub-range from a start address til a page boundary after the given size.
         /// </summary>
         /// <param name="address">Start address of the memory range</param>
