3f2c2a1465ef1a55327748005e6d325fc4e4bf13
Isaac Marovitz <isaacryu@icloud.com>
2023-08-14 14:39:54 +0100

    Remove unnecessary using

--- src/Ryujinx.Ava/UI/Windows/ModManagerWindow.axaml.cs
+++ src/Ryujinx.Ava/UI/Windows/ModManagerWindow.axaml.cs
@@ -6,7 +6,6 @@ using Ryujinx.Ava.Common.Locale;
 using Ryujinx.Ava.UI.Helpers;
 using Ryujinx.Ava.UI.Models;
 using Ryujinx.Ava.UI.ViewModels;
-using Ryujinx.HLE.FileSystem;
 using Ryujinx.Ui.Common.Helper;
 using System.Threading.Tasks;
 using Button = Avalonia.Controls.Button;
