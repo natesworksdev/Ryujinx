f35d06f81019eaed5ad6baf66c70651240cb07de
Isaac Marovitz <isaacryu@icloud.com>
2023-08-31 00:43:01 +0100

    Add macOS

--- src/Ryujinx.Common/SystemInterop/DisplaySleep.cs
+++ src/Ryujinx.Common/SystemInterop/DisplaySleep.cs
@@ -16,11 +16,22 @@ namespace Ryujinx.Common.SystemInterop
         [LibraryImport("kernel32.dll", SetLastError = true)]
         private static partial EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
 
+        [LibraryImport("/System/Library/Frameworks/IOKit.framework/IOKit", StringMarshalling = StringMarshalling.Utf8)]
+        private static partial int IOPMAssertionCreateWithName(string assertionType, UInt32 assertionLevel, string assertionName, UInt32 assertionId);
+
+        [LibraryImport("/System/Library/Frameworks/IOKit.framework/IOKit", StringMarshalling = StringMarshalling.Utf8)]
+        private static partial int IOPMAssertionRelease(int assertionId);
+
         static public void Prevent()
         {
             if (OperatingSystem.IsWindows())
             {
                 SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+            }
+            else if (OperatingSystem.IsMacOS())
+            {
+                // TODO: Assertion ID needs to be a PTR to an int
+                IOPMAssertionCreateWithName("NoDisplaySleepAssertion", 255, "Ryujinx", 0);
             }
         }
 
@@ -29,6 +40,10 @@ namespace Ryujinx.Common.SystemInterop
             if (OperatingSystem.IsWindows())
             {
                 SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+            }
+            else if (OperatingSystem.IsMacOS())
+            {
+                IOPMAssertionRelease(0);
             }
         }
     }
