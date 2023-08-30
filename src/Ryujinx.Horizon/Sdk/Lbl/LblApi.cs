b5a6db24ea39bff22c3e729254591c01badd943b
Ac_K <Acoustik666@gmail.com>
2023-08-30 23:19:11 +0200

    lbl: Migrate service to Horizon

---
+++ src/Ryujinx.Horizon/Sdk/Lbl/LblApi.cs
@@ -0,0 +1,41 @@
+﻿using Ryujinx.Horizon.Common;
+using Ryujinx.Horizon.Sdk.Sm;
+using System;
+
+namespace Ryujinx.Horizon.Sdk.Lbl
+{
+    public class LblApi : IDisposable
+    {
+        private const string LblName = "lbl";
+
+        private readonly int _sessionHandle;
+
+        public LblApi()
+        {
+            using var smApi = new SmApi();
+
+            smApi.Initialize();
+            smApi.GetServiceHandle(out _sessionHandle, ServiceName.Encode(LblName)).AbortOnFailure();
+        }
+
+        public Result EnableVrMode()
+        {
+            return ServiceUtil.SendRequest(out _, _sessionHandle, 26, sendPid: false, ReadOnlySpan<byte>.Empty);
+        }
+
+        public Result DisableVrMode()
+        {
+            return ServiceUtil.SendRequest(out _, _sessionHandle, 27, sendPid: false, ReadOnlySpan<byte>.Empty);
+        }
+
+        public void Dispose()
+        {
+            if (_sessionHandle != 0)
+            {
+                HorizonStatic.Syscall.CloseHandle(_sessionHandle);
+            }
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
