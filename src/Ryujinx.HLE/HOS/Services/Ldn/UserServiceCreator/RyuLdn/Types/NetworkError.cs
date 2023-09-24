b9a1165746b0bca5d2d384b5852b57d1e4259ef1
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-09-24 14:08:00 +0200

    Extract NetworkErrorMessage from NetworkError

--- src/Ryujinx.HLE/HOS/Services/Ldn/UserServiceCreator/RyuLdn/Types/NetworkError.cs
+++ src/Ryujinx.HLE/HOS/Services/Ldn/UserServiceCreator/RyuLdn/Types/NetworkError.cs
@@ -1,5 +1,3 @@
-using System.Runtime.InteropServices;
-
 namespace Ryujinx.HLE.HOS.Services.Ldn.UserServiceCreator.RyuLdn.Types
 {
     enum NetworkError : int
@@ -20,11 +18,5 @@ namespace Ryujinx.HLE.HOS.Services.Ldn.UserServiceCreator.RyuLdn.Types
         RejectFailed,
 
         Unknown = -1,
-    }
-
-    [StructLayout(LayoutKind.Sequential, Size = 0x4)]
-    struct NetworkErrorMessage
-    {
-        public NetworkError Error;
     }
 }
