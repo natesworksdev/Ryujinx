63d6ba3c948c1065ec1d37abdac149be842dd762
Isaac Marovitz <isaacryu@icloud.com>
2023-10-21 01:21:47 -0400

    ICommonStateGetter

---
+++ src/Ryujinx.Horizon/Sdk/Am/ICommonStateGetter.cs
@@ -0,0 +1,61 @@
+using Ryujinx.Horizon.Common;
+
+namespace Ryujinx.Horizon.Sdk.Am
+{
+    public interface ICommonStateGetter
+    {
+        Result GetEventHandle();
+        Result ReceiveMessage();
+        Result GetThisAppletKind();
+        Result AllowToEnterSleep();
+        Result DisallowToEnterSleep();
+        Result GetOperationMode();
+        Result GetPerformanceMode();
+        Result GetCradleStatus();
+        Result GetBootMode();
+        Result GetCurrentFocusState();
+        Result RequestToAcquireSleepLock();
+        Result ReleaseSleepLock();
+        Result ReleaseSleepLockTransiently();
+        Result GetAcquiredSleepLockEvent();
+        Result GetWakeupCount();
+        Result PushToGeneralChannel();
+        Result GetHomeButtonReaderLockAccessor();
+        Result GetReaderLockAccessorEx();
+        Result GetWriterLockAccessorEx();
+        Result GetCradleFwVersion();
+        Result IsVrModeEnabled();
+        Result SetVrModeEnabled();
+        Result SetLcdBacklightOffEnabled();
+        Result BeginVrModeEx();
+        Result EndVrModeEx();
+        Result IsInControllerFirmwareUpdateSection();
+        Result SetVrPositionForDebug();
+        Result GetDefaultDisplayResolution();
+        Result GetDefaultDisplayResolutionChangeEvent();
+        Result GetHdcpAuthenticationState();
+        Result GetHdcpAuthenticationStateChangeEvent();
+        Result SetTvPowerStateMatchingMode();
+        Result GetApplicationIdByContentActionName();
+        Result SetCpuBoostMode();
+        Result CancelCpuBoostMode();
+        Result GetBuiltInDisplayType();
+        Result PerformSystemButtonPressingIfInFocus();
+        Result SetPerformanceConfigurationChangedNotification();
+        Result GetCurrentPerformanceConfiguration();
+        Result SetHandlingHomeButtonShortPressedEnabled();
+        Result OpenMyGpuErrorHandler();
+        Result GetAppletLaunchedHistory();
+        Result GetOperationModeSystemInfo();
+        Result GetSettingsPlatformRegion();
+        Result ActivateMigrationService();
+        Result DeactivateMigrationService();
+        Result DisableSleepTillShutdown();
+        Result SuppressDisablingSleepTemporarily();
+        Result IsSleepEnabled();
+        Result IsDisablingSleepSuppressed();
+        Result OpenNamedChannelAsChild();
+        Result SetRequestExitToLibraryAppletAtExecuteNextProgramEnabled();
+        Result GetLaunchRequiredTick();
+    }
+}
