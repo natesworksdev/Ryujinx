47f0fe68373b72a69eec5b6e67f8d6372765559d
Isaac Marovitz <isaacryu@icloud.com>
2023-10-21 01:27:30 -0400

    Commands

---
+++ src/Ryujinx.Horizon/Am/Ipc/Proxies/OverlayAppletProxy.cs
@@ -0,0 +1,75 @@
+using Ryujinx.Horizon.Common;
+using Ryujinx.Horizon.Sdk.Am;
+using Ryujinx.Horizon.Sdk.Sf;
+
+namespace Ryujinx.Horizon.Am.Ipc.Proxies
+{
+    partial class OverlayAppletProxy : IOverlayAppletProxy
+    {
+        [CmifCommand(0)]
+        public Result GetCommonStateGetter(out ICommonStateGetter commonStateGetter, ulong pid)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(1)]
+        public Result GetSelfController()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(2)]
+        public Result GetWindowController()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(3)]
+        public Result GetAudioController()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(4)]
+        public Result GetDisplayController()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(10)]
+        public Result GetProcessWindingController()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(11)]
+        public Result GetLibraryAppletCreator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(20)]
+        public Result GetOverlayFunctions()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(21)]
+        public Result GetAppletCommonFunctions()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(23)]
+        public Result GetGlobalStateController()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [CmifCommand(1000)]
+        public Result GetDebugFunctions()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
