0f50273d4fa2244b2525af8feac0d909e801ee04
svc64 <svcx80@icloud.com>
2023-10-21 11:06:41 +0300

    Fix GDB stub settings

--- src/Ryujinx.Gtk3/UI/Windows/SettingsWindow.cs
+++ src/Ryujinx.Gtk3/UI/Windows/SettingsWindow.cs
@@ -117,8 +117,8 @@ namespace Ryujinx.UI.Windows
         [GUI] ToggleButton _configureController7;
         [GUI] ToggleButton _configureController8;
         [GUI] ToggleButton _configureControllerH;
-        [GUI] ToggleButton    _gdbStubToggle;
-        [GUI] Adjustment      _gdbStubPortSpinAdjustment;
+        [GUI] ToggleButton _gdbStubToggle;
+        [GUI] Adjustment _gdbStubPortSpinAdjustment;
 #pragma warning restore CS0649, IDE0044
 
         public SettingsWindow(MainWindow parent, VirtualFileSystem virtualFileSystem, ContentManager contentManager) : this(parent, new Builder("Ryujinx.Gtk3.UI.Windows.SettingsWindow.glade"), virtualFileSystem, contentManager) { }
@@ -667,6 +667,7 @@ namespace Ryujinx.UI.Windows
             ConfigurationState.Instance.Graphics.ScalingFilter.Value = Enum.Parse<ScalingFilter>(_scalingFilter.ActiveId);
             ConfigurationState.Instance.Graphics.ScalingFilterLevel.Value = (int)_scalingFilterLevel.Value;
             ConfigurationState.Instance.Multiplayer.LanInterfaceId.Value = _multiLanSelect.ActiveId;
+            ConfigurationState.Instance.Debug.EnableGdbStub.Value = _gdbStubToggle.Active;
 
             _previousVolumeLevel = ConfigurationState.Instance.System.AudioVolume.Value;
 
