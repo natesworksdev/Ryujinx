1e8f229b7f8de97850529acfdaeae1919e5696fb
Gabriel A <gab.dark.100@gmail.com>
2023-11-16 16:41:43 -0300

    Enable copy dependency between RGBA8 and RGBA32 formats

--- src/Ryujinx.Graphics.Gpu/Image/TextureCompatibility.cs
+++ src/Ryujinx.Graphics.Gpu/Image/TextureCompatibility.cs
@@ -718,7 +718,8 @@ namespace Ryujinx.Graphics.Gpu.Image
                 (lhsFormat, rhsFormat) = (rhsFormat, lhsFormat);
             }
 
-            return lhsFormat.Format == Format.R8Unorm && rhsFormat.Format == Format.R8G8B8A8Unorm;
+            return (lhsFormat.Format == Format.R8G8B8A8Unorm && rhsFormat.Format == Format.R32G32B32A32Float) ||
+                   (lhsFormat.Format == Format.R8Unorm && rhsFormat.Format == Format.R8G8B8A8Unorm);
         }
 
         /// <summary>
