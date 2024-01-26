be59e9bda0c87d8643c6abc43f2e0a89d492b7c7
Gabriel A <gab.dark.100@gmail.com>
2024-01-26 16:29:36 -0300

    Missed that one

--- src/Ryujinx.Horizon/Sdk/Settings/System/ButtonConfigRegisteredSettings.cs
+++ src/Ryujinx.Horizon/Sdk/Settings/System/ButtonConfigRegisteredSettings.cs
@@ -2,6 +2,7 @@ using System.Runtime.InteropServices;
 
 namespace Ryujinx.Horizon.Sdk.Settings.System
 {
+    [StructLayout(LayoutKind.Sequential, Size = 0x5C8)]
     struct ButtonConfigRegisteredSettings
     {
     }
