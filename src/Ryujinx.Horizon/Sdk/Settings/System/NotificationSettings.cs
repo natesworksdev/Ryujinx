01104469916853dee7a3b1e40869285df70df42b
Gabriel A <gab.dark.100@gmail.com>
2024-01-26 22:38:18 -0300

    Fill in enums from PR review

--- src/Ryujinx.Horizon/Sdk/Settings/System/NotificationSettings.cs
+++ src/Ryujinx.Horizon/Sdk/Settings/System/NotificationSettings.cs
@@ -1,12 +1,29 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace Ryujinx.Horizon.Sdk.Settings.System
 {
+    [Flags]
+    enum NotificationFlag : uint
+    {
+        RingtoneFlag = 1 << 0,
+        DownloadCompletionFlag = 1 << 1,
+        EnablesNews = 1 << 8,
+        IncomingLampFlag = 1 << 9,
+    }
+
+    enum NotificationVolume : uint
+    {
+        Mute,
+        Low,
+        High,
+    }
+
     [StructLayout(LayoutKind.Sequential, Size = 0x18, Pack = 0x4)]
     struct NotificationSettings
     {
-        public uint Flag;
-        public float Volume;
+        public NotificationFlag Flag;
+        public NotificationVolume Volume;
         public ulong HeadTime;
         public ulong TailTime;
     }
--- src/Ryujinx.Horizon/Sdk/Settings/System/TvSettings.cs
+++ src/Ryujinx.Horizon/Sdk/Settings/System/TvSettings.cs
@@ -1,17 +1,59 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace Ryujinx.Horizon.Sdk.Settings.System
 {
+    [Flags]
+    enum TvFlag : uint
+    {
+        Allows4k = 1 << 0,
+        Allows3d = 1 << 1,
+        AllowsCec = 1 << 2,
+        PreventsScreenBurnIn = 1 << 3,
+    }
+
+    enum TvResolution : uint
+    {
+        Auto,
+        At1080p,
+        At720p,
+        At480p,
+    }
+
+    enum HdmiContentType : uint
+    {
+        None,
+        Graphics,
+        Cinema,
+        Photo,
+        Game,
+    }
+
+    enum RgbRange : uint
+    {
+        Auto,
+        Full,
+        Limited,
+    }
+
+    enum CmuMode : uint
+    {
+        None,
+        ColorInvert,
+        HighContrast,
+        GrayScale,
+    }
+
     [StructLayout(LayoutKind.Sequential, Size = 0x20, Pack = 0x4)]
     struct TvSettings
     {
-        public uint Flags;
-        public uint TvResolution;
-        public uint HdmiContentType;
-        public uint RgbRange;
-        public uint CmuMode;
-        public uint TvUnderscan;
-        public uint TvGamma;
-        public uint ContrastRatio;
+        public TvFlag Flags;
+        public TvResolution TvResolution;
+        public HdmiContentType HdmiContentType;
+        public RgbRange RgbRange;
+        public CmuMode CmuMode;
+        public float TvUnderscan;
+        public float TvGamma;
+        public float ContrastRatio;
     }
 }
