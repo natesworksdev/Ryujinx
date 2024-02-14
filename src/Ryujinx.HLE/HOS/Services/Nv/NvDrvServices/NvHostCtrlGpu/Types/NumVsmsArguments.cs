f8638d4865e689695da104b4e95c025b479b8960
Gabriel A <gab.dark.100@gmail.com>
2024-02-14 20:38:57 -0300

    Stub VSMS related ioctls

---
+++ src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostCtrlGpu/Types/NumVsmsArguments.cs
@@ -0,0 +1,12 @@
+using Ryujinx.Common.Memory;
+using System.Runtime.InteropServices;
+
+namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostCtrlGpu.Types
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct NumVsmsArguments
+    {
+        public uint NumVsms;
+        public uint Reserved;
+    }
+}
