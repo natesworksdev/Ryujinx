efabbda08896fe6f0e223690b6e718f155eeb6a0
Gabriel A <gab.dark.100@gmail.com>
2024-02-24 21:42:25 -0300

    IsRmwMemory -> IsPartialRegisterUpdateMemory

--- src/Ryujinx.Cpu/LightningJit/Arm64/InstName.cs
+++ src/Ryujinx.Cpu/LightningJit/Arm64/InstName.cs
@@ -1132,7 +1132,7 @@ namespace Ryujinx.Cpu.LightningJit.Arm64
             return false;
         }
 
-        public static bool IsRmwMemory(this InstName name)
+        public static bool IsPartialRegisterUpdateMemory(this InstName name)
         {
             switch (name)
             {
