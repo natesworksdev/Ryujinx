a8eb25441cb4288e5b77031e94233ec2b5b8c067
Isaac Marovitz <isaacryu@icloud.com>
2024-02-28 22:18:01 -0500

    Stringify Time Zones like Horizon

--- src/Ryujinx.Ava/UI/Models/TimeZone.cs
+++ src/Ryujinx.Ava/UI/Models/TimeZone.cs
@@ -1,12 +1,27 @@
 namespace Ryujinx.Ava.UI.Models
 {
-    internal class TimeZone
+    public class TimeZone
     {
         public TimeZone(string utcDifference, string location, string abbreviation)
         {
             UtcDifference = utcDifference;
             Location = location;
             Abbreviation = abbreviation;
+        }
+
+        public string ToString()
+        {
+            // Prettifies location strings eg:
+            // "America/Costa_Rica" -> "Costa Rica"
+            var location = Location.Replace("_", " ");
+
+            if (location.Contains("/"))
+            {
+                var parts = location.Split("/");
+                location = parts[1];
+            }
+
+            return $"{UtcDifference} - {location}";
         }
 
         public string UtcDifference { get; set; }
