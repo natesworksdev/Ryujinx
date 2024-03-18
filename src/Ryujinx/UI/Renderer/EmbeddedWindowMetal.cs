b80a904cf2c49fcf6d874b2b0e0ce3fd915d8c5a
Isaac Marovitz <isaacryu@icloud.com>
2024-03-18 14:57:23 -0400

    Resolve warning

--- src/Ryujinx/UI/Renderer/EmbeddedWindowMetal.cs
+++ src/Ryujinx/UI/Renderer/EmbeddedWindowMetal.cs
@@ -1,7 +1,9 @@
 using SharpMetal.QuartzCore;
+using System.Runtime.Versioning;
 
 namespace Ryujinx.Ava.UI.Renderer
 {
+    [SupportedOSPlatform("macos")]
     public class EmbeddedWindowMetal : EmbeddedWindow
     {
         public CAMetalLayer CreateSurface()
