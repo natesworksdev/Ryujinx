047418ca1fce95851cf6046fbdce4bb44c0f8314
Yohoki <yohoki3@aol.com>
2024-05-06 14:45:11 -0500

    Update `RequestSyncDeliveryCacheWithDirectoryName` to accept two parameters.

--- src/Ryujinx.Horizon/Sdk/Bcat/IBcatService.cs
+++ src/Ryujinx.Horizon/Sdk/Bcat/IBcatService.cs
@@ -6,6 +6,6 @@ namespace Ryujinx.Horizon.Sdk.Bcat
     internal interface IBcatService : IServiceObject
     {
         Result RequestSyncDeliveryCache(out IDeliveryCacheProgressService deliveryCacheProgressService);
-        Result RequestSyncDeliveryCacheWithDirectoryName(out IDeliveryCacheProgressService deliveryCacheProgressService);
+        Result RequestSyncDeliveryCacheWithDirectoryName(out IDeliveryCacheProgressService deliveryCacheProgressService, LibHac.Bcat.DirectoryName arg1);
     }
 }
