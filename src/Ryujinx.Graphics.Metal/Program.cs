05a44ac930a54199e2ab6d4de65e4a77cc08ba2d
Isaac Marovitz <isaacryu@icloud.com>
2024-05-18 18:54:55 -0400

    Break everything :D

--- src/Ryujinx.Graphics.Metal/Program.cs
+++ src/Ryujinx.Graphics.Metal/Program.cs
@@ -28,8 +28,6 @@ namespace Ryujinx.Graphics.Metal
                 {
                     Logger.Warning?.Print(LogClass.Gpu, $"Shader linking failed: \n{StringHelper.String(libraryError.LocalizedDescription)}");
                     _status = ProgramLinkStatus.Failure;
-                    //Console.WriteLine($"SHADER {index}: {shader.Code}");
-                    //throw new NotImplementedException();
                     return;
                 }
 
