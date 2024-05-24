56674f30efc8331f6e575c6ef38a020ba4cde7b1
Gabriel A <gab.dark.100@gmail.com>
2024-05-24 19:18:40 -0300

    Key textures using set and binding (rather than just binding)

--- src/Ryujinx.Graphics.Shader/CodeGen/Spirv/Instructions.cs
+++ src/Ryujinx.Graphics.Shader/CodeGen/Spirv/Instructions.cs
@@ -1,4 +1,4 @@
-using Ryujinx.Graphics.Shader.IntermediateRepresentation;
+﻿using Ryujinx.Graphics.Shader.IntermediateRepresentation;
 using Ryujinx.Graphics.Shader.StructuredIr;
 using Ryujinx.Graphics.Shader.Translation;
 using System;
