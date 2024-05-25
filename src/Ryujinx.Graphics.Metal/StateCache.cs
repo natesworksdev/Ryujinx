8b7ac4e87f1e220aaacf11899106c097caa2360b
Isaac Marovitz <isaacryu@icloud.com>
2024-05-25 12:30:06 -0400

    Suppress GC Finalize on StateCache

--- src/Ryujinx.Graphics.Metal/StateCache.cs
+++ src/Ryujinx.Graphics.Metal/StateCache.cs
@@ -19,6 +19,8 @@ namespace Ryujinx.Graphics.Metal
             {
                 value.Dispose();
             }
+
+            GC.SuppressFinalize(this);
         }
 
         public T GetOrCreate(TDescriptor descriptor)
