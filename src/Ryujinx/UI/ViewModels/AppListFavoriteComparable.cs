f39247d810beb5a66fd7a606e43e12243949207e
Logan Stromberg <lostromb@microsoft.com>
2024-08-07 17:18:02 -0700

    Address code style warnings. Add null checking. Make title name comparison case insensitive.

--- src/Ryujinx/UI/ViewModels/AppListFavoriteComparable.cs
+++ src/Ryujinx/UI/ViewModels/AppListFavoriteComparable.cs
@@ -7,7 +7,7 @@ namespace Ryujinx.Ava.UI.ViewModels
     /// Implements a custom comparer which is used for sorting titles by favorite on a UI.
     /// Returns a sorted list of favorites in alphabetical order, followed by all non-favorites sorted alphabetical.
     /// </summary>
-    public struct AppListFavoriteComparable : IComparable
+    public readonly struct AppListFavoriteComparable : IComparable
     {
         /// <summary>
         /// The application data being compared.
@@ -20,23 +20,28 @@ namespace Ryujinx.Ava.UI.ViewModels
         /// <param name="app">The app data being compared.</param>
         public AppListFavoriteComparable(ApplicationData app)
         {
+            if (app is null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
             this.app = app;
         }
 
         /// <inheritdoc/>
-        public int CompareTo(object o)
+        public readonly int CompareTo(object o)
         {
             if (o is AppListFavoriteComparable other)
             {
                 if (app.Favorite == other.app.Favorite)
                 {
-                    return app.Name.CompareTo(other.app.Name);
+                    return string.Compare(app.Name, other.app.Name, StringComparison.OrdinalIgnoreCase);
                 }
 
                 return app.Favorite ? -1 : 1;
             }
 
-            throw new InvalidCastException($"Cannot cast {o.GetType()} to {nameof(ApplicationData)}");
+            throw new InvalidCastException($"Cannot cast {o.GetType()} to {nameof(AppListFavoriteComparable)}");
         }
     }
 }
