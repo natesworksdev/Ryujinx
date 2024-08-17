6a555ee9f78caec50aa598fbb5435ea68c4f5f8b
Barış Hamil <barishamil90@gmail.com>
2024-08-17 23:04:01 +0300

    Ability to assign hotkeys to cycle controllers for players (WIP)

--- src/Ryujinx/Common/KeyboardHotkeyState.cs
+++ src/Ryujinx/Common/KeyboardHotkeyState.cs
@@ -12,5 +12,13 @@ namespace Ryujinx.Ava.Common
         ResScaleDown,
         VolumeUp,
         VolumeDown,
+        CycleControllersPlayer1,
+        CycleControllersPlayer2,
+        CycleControllersPlayer3,
+        CycleControllersPlayer4,
+        CycleControllersPlayer5,
+        CycleControllersPlayer6,
+        CycleControllersPlayer7,
+        CycleControllersPlayer8
     }
 }
--- src/Ryujinx/UI/Views/Settings/SettingsHotkeysView.axaml.cs
+++ src/Ryujinx/UI/Views/Settings/SettingsHotkeysView.axaml.cs
@@ -3,11 +3,17 @@ using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.LogicalTree;
+using Avalonia.VisualTree;
+using DynamicData.Kernel;
+using FluentAvalonia.Core;
 using Ryujinx.Ava.Input;
 using Ryujinx.Ava.UI.Helpers;
 using Ryujinx.Ava.UI.ViewModels;
 using Ryujinx.Input;
 using Ryujinx.Input.Assigner;
+using System;
+using System.Collections.Specialized;
+using System.Linq;
 using Key = Ryujinx.Common.Configuration.Hid.Key;
 
 namespace Ryujinx.Ava.UI.Views.Settings
@@ -20,16 +26,21 @@ namespace Ryujinx.Ava.UI.Views.Settings
         public SettingsHotkeysView()
         {
             InitializeComponent();
+            RegisterEvents();
+            _avaloniaKeyboardDriver = new AvaloniaKeyboardDriver(this);
+            CycleControllers.LayoutUpdated += (_, _1) => RegisterEvents();
+        }
 
+        private void RegisterEvents()
+        {
             foreach (ILogical visual in SettingButtons.GetLogicalDescendants())
             {
                 if (visual is ToggleButton button and not CheckBox)
                 {
+                    button.IsCheckedChanged -= Button_IsCheckedChanged;
                     button.IsCheckedChanged += Button_IsCheckedChanged;
                 }
             }
-
-            _avaloniaKeyboardDriver = new AvaloniaKeyboardDriver(this);
         }
 
         protected override void OnPointerReleased(PointerReleasedEventArgs e)
@@ -50,87 +61,89 @@ namespace Ryujinx.Ava.UI.Views.Settings
 
             PointerPressed -= MouseClick;
         }
-
         private void Button_IsCheckedChanged(object sender, RoutedEventArgs e)
         {
-            if (sender is ToggleButton button)
+            if (sender is not ToggleButton button)
             {
-                if ((bool)button.IsChecked)
-                {
-                    if (_currentAssigner != null && button == _currentAssigner.ToggledButton)
-                    {
-                        return;
-                    }
-
-                    if (_currentAssigner == null)
-                    {
-                        _currentAssigner = new ButtonKeyAssigner(button);
-
-                        this.Focus(NavigationMethod.Pointer);
-
-                        PointerPressed += MouseClick;
-
-                        var keyboard = (IKeyboard)_avaloniaKeyboardDriver.GetGamepad("0");
-                        IButtonAssigner assigner = new KeyboardKeyAssigner(keyboard);
-
-                        _currentAssigner.ButtonAssigned += (sender, e) =>
-                        {
-                            if (e.ButtonValue.HasValue)
-                            {
-                                var viewModel = (DataContext) as SettingsViewModel;
-                                var buttonValue = e.ButtonValue.Value;
-
-                                switch (button.Name)
-                                {
-                                    case "ToggleVsync":
-                                        viewModel.KeyboardHotkey.ToggleVsync = buttonValue.AsHidType<Key>();
-                                        break;
-                                    case "Screenshot":
-                                        viewModel.KeyboardHotkey.Screenshot = buttonValue.AsHidType<Key>();
-                                        break;
-                                    case "ShowUI":
-                                        viewModel.KeyboardHotkey.ShowUI = buttonValue.AsHidType<Key>();
-                                        break;
-                                    case "Pause":
-                                        viewModel.KeyboardHotkey.Pause = buttonValue.AsHidType<Key>();
-                                        break;
-                                    case "ToggleMute":
-                                        viewModel.KeyboardHotkey.ToggleMute = buttonValue.AsHidType<Key>();
-                                        break;
-                                    case "ResScaleUp":
-                                        viewModel.KeyboardHotkey.ResScaleUp = buttonValue.AsHidType<Key>();
-                                        break;
-                                    case "ResScaleDown":
-                                        viewModel.KeyboardHotkey.ResScaleDown = buttonValue.AsHidType<Key>();
-                                        break;
-                                    case "VolumeUp":
-                                        viewModel.KeyboardHotkey.VolumeUp = buttonValue.AsHidType<Key>();
-                                        break;
-                                    case "VolumeDown":
-                                        viewModel.KeyboardHotkey.VolumeDown = buttonValue.AsHidType<Key>();
-                                        break;
-                                }
-                            }
-                        };
-
-                        _currentAssigner.GetInputAndAssign(assigner, keyboard);
-                    }
-                    else
-                    {
-                        if (_currentAssigner != null)
-                        {
-                            _currentAssigner.Cancel();
-                            _currentAssigner = null;
-                            button.IsChecked = false;
-                        }
-                    }
-                }
-                else
-                {
-                    _currentAssigner?.Cancel();
-                    _currentAssigner = null;
-                }
+                return;
             }
+
+            if (!button.IsChecked.ValueOr(default))
+            {
+                _currentAssigner?.Cancel();
+                _currentAssigner = null;
+                return;
+            }
+
+            if (_currentAssigner != null && button == _currentAssigner.ToggledButton)
+            {
+                return;
+            }
+
+            if (_currentAssigner != null)
+            {
+                _currentAssigner.Cancel();
+                _currentAssigner = null;
+                button.IsChecked = false;
+                return;
+            }
+
+            _currentAssigner = new ButtonKeyAssigner(button);
+
+            this.Focus(NavigationMethod.Pointer);
+
+            PointerPressed += MouseClick;
+
+            var keyboard = (IKeyboard)_avaloniaKeyboardDriver.GetGamepad("0");
+            IButtonAssigner assigner = new KeyboardKeyAssigner(keyboard);
+
+            _currentAssigner.ButtonAssigned += (sender, e) =>
+            {
+                if (!e.ButtonValue.HasValue)
+                {
+                    return;
+                }
+
+                var viewModel = (DataContext) as SettingsViewModel;
+                var buttonValue = e.ButtonValue.Value;
+
+                switch (button.Name)
+                {
+                    case "ToggleVsync":
+                        viewModel.KeyboardHotkey.ToggleVsync = buttonValue.AsHidType<Key>();
+                        break;
+                    case "Screenshot":
+                        viewModel.KeyboardHotkey.Screenshot = buttonValue.AsHidType<Key>();
+                        break;
+                    case "ShowUI":
+                        viewModel.KeyboardHotkey.ShowUI = buttonValue.AsHidType<Key>();
+                        break;
+                    case "Pause":
+                        viewModel.KeyboardHotkey.Pause = buttonValue.AsHidType<Key>();
+                        break;
+                    case "ToggleMute":
+                        viewModel.KeyboardHotkey.ToggleMute = buttonValue.AsHidType<Key>();
+                        break;
+                    case "ResScaleUp":
+                        viewModel.KeyboardHotkey.ResScaleUp = buttonValue.AsHidType<Key>();
+                        break;
+                    case "ResScaleDown":
+                        viewModel.KeyboardHotkey.ResScaleDown = buttonValue.AsHidType<Key>();
+                        break;
+                    case "VolumeUp":
+                        viewModel.KeyboardHotkey.VolumeUp = buttonValue.AsHidType<Key>();
+                        break;
+                    case "VolumeDown":
+                        viewModel.KeyboardHotkey.VolumeDown = buttonValue.AsHidType<Key>();
+                        break;
+                    default:
+                        var index = button.FindAncestorOfType<ItemsControl>().GetLogicalDescendants().OfType<ToggleButton>().IndexOf(button);
+                        viewModel.KeyboardHotkey.CycleControllers[index].Hotkey = buttonValue.AsHidType<Key>();
+                        break;
+                }
+            };
+
+            _currentAssigner.GetInputAndAssign(assigner, keyboard);
         }
 
         public void Dispose()
