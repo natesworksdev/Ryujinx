8a82388d007ea699ea5092c138a7d16c3ef67585
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2024-09-09 23:11:17 +0200

    Remove Avalonia GUI migration code

--- src/Ryujinx/Modules/Updater/Updater.cs
+++ src/Ryujinx/Modules/Updater/Updater.cs
@@ -300,13 +300,6 @@ namespace Ryujinx.Modules
                         // Find the process name.
                         string ryuName = Path.GetFileName(Environment.ProcessPath) ?? string.Empty;
 
-                        // Migration: Start the updated binary.
-                        // TODO: Remove this in a future update.
-                        if (ryuName.StartsWith("Ryujinx.Ava"))
-                        {
-                            ryuName = ryuName.Replace(".Ava", "");
-                        }
-
                         // Some operating systems can see the renamed executable, so strip off the .ryuold if found.
                         if (ryuName.EndsWith(".ryuold"))
                         {
@@ -765,43 +758,6 @@ namespace Ryujinx.Modules
             foreach (string file in Directory.GetFiles(_homeDir, "*.ryuold", SearchOption.AllDirectories))
             {
                 File.Delete(file);
-            }
-
-            // Migration: Delete old Ryujinx binary.
-            // TODO: Remove this in a future update.
-            if (!OperatingSystem.IsMacOS())
-            {
-                string[] oldRyuFiles = Directory.GetFiles(_homeDir, "Ryujinx.Ava*", SearchOption.TopDirectoryOnly);
-                // Assume we are running the new one if the process path is not available.
-                // This helps to prevent an infinite loop of restarts.
-                string currentRyuName = Path.GetFileName(Environment.ProcessPath) ?? (OperatingSystem.IsWindows() ? "Ryujinx.exe" : "Ryujinx");
-
-                string newRyuName = Path.Combine(_homeDir, currentRyuName.Replace(".Ava", ""));
-                if (!currentRyuName.Contains("Ryujinx.Ava"))
-                {
-                    foreach (string oldRyuFile in oldRyuFiles)
-                    {
-                        File.Delete(oldRyuFile);
-                    }
-                }
-                // Should we be running the old binary, start the new one if possible.
-                else if (File.Exists(newRyuName))
-                {
-                    ProcessStartInfo processStart = new(newRyuName)
-                    {
-                        UseShellExecute = true,
-                        WorkingDirectory = _homeDir,
-                    };
-
-                    foreach (string argument in CommandLineState.Arguments)
-                    {
-                        processStart.ArgumentList.Add(argument);
-                    }
-
-                    Process.Start(processStart);
-
-                    Environment.Exit(0);
-                }
             }
         }
     }
